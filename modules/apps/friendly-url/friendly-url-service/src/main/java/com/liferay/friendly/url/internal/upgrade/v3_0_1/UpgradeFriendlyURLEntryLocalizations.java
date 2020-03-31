b859fa9df6e75eeac0883bd9ca32682e9d6f580f
ricardocousolr <ricardo.couso@liferay.com>
2020-03-31 10:06:09 +0200

    LPS-107018 For each of the journal article most recent versions, find journal article localizations in languages without a friendly url entry localization

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/upgrade/v3_0_1/UpgradeFriendlyURLEntryLocalizations.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/upgrade/v3_0_1/UpgradeFriendlyURLEntryLocalizations.java
@@ -14,15 +14,91 @@
 
 package com.liferay.friendly.url.internal.upgrade.v3_0_1;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Ricardo Couso
  */
 public class UpgradeFriendlyURLEntryLocalizations extends UpgradeProcess {
 
+	protected void addMissingFriendlyURLEntryLocalizations() {
+		StringBundler sb1 = new StringBundler(8);
+
+		sb1.append("select JournalArticle.id_, ");
+		sb1.append("JournalArticle.resourcePrimKey, JournalArticle.groupId, ");
+		sb1.append("JournalArticle.companyId from (select resourcePrimKey, ");
+		sb1.append("max(version) as latestVersion from JournalArticle group ");
+		sb1.append("by resourcePrimKey) LatestVersion inner join ");
+		sb1.append("JournalArticle on JournalArticle.resourcePrimKey = ");
+		sb1.append("LatestVersion.resourcePrimKey and JournalArticle.version ");
+		sb1.append("= LatestVersion.latestVersion");
+
+		try (Statement statement1 = connection.createStatement();
+			ResultSet rs1 = statement1.executeQuery(sb1.toString())) {
+
+			while (rs1.next()) {
+				long id = rs1.getLong(1);
+				long resourcePrimKey = rs1.getLong(2);
+
+				StringBundler sb2 = new StringBundler(13);
+
+				sb2.append("select JAL.title, JAL.languageId from (select ");
+				sb2.append("title, languageId from ");
+				sb2.append("JournalArticleLocalization where articlePK = ");
+				sb2.append(id);
+				sb2.append(") JAL left join (select ");
+				sb2.append("friendlyURLEntryLocalizationId, languageId from ");
+				sb2.append("FriendlyURLEntryLocalization where classPK = ");
+				sb2.append(resourcePrimKey);
+				sb2.append(" and classNameId = ");
+				sb2.append(_classNameIdJournalArticle);
+				sb2.append(") FURLEL on JAL.languageId = FURLEL.languageId ");
+				sb2.append("where FURLEL.friendlyURLEntryLocalizationId is ");
+				sb2.append("null");
+
+				Map<String, String> urlTitleMap = new HashMap<>();
+
+				try (Statement statement2 = connection.createStatement();
+					ResultSet rs2 = statement2.executeQuery(sb2.toString())) {
+
+					while (rs2.next()) {
+						String title = rs2.getString(1);
+						String languageId = rs2.getString(2);
+
+						urlTitleMap.put(languageId, title);
+					}
+				}
+			}
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+		}
+	}
+
 	@Override
 	protected void doUpgrade() throws Exception {
-
+		setUpClassNameIds();
+		addMissingFriendlyURLEntryLocalizations();
 	}
+
+	protected void setUpClassNameIds() {
+		_classNameIdJournalArticle = PortalUtil.getClassNameId(
+			"com.liferay.journal.model.JournalArticle");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeFriendlyURLEntryLocalizations.class);
+
+	private long _classNameIdJournalArticle;
+
 }
