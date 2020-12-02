35940285baf8fe66f11b07d3f1693953d3afc8d7
Jorge Díaz <jorge.diaz@liferay.com>
2020-12-02 14:02:18 +0100

    LPS-123694 portal-search: Create the new NestedFacetImpl class
    https://issues.liferay.com/browse/LPS-123694

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/NestedFacetImpl.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.facet;
+
+import com.liferay.portal.kernel.search.BooleanClause;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.search.filter.QueryFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.search.generic.BooleanClauseImpl;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.search.generic.NestedQuery;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.search.facet.nested.NestedFacet;
+
+/**
+ * @author Jorge Díaz
+ */
+public class NestedFacetImpl extends FacetImpl implements NestedFacet {
+
+	public NestedFacetImpl(String fieldName, SearchContext searchContext) {
+		super(fieldName, searchContext);
+	}
+
+	public String getFilterField() {
+		return _filterField;
+	}
+
+	public String getFilterValue() {
+		return _filterValue;
+	}
+
+	@Override
+	public String getPath() {
+		return _path;
+	}
+
+	public void setFilterField(String filterField) {
+		_filterField = filterField;
+	}
+
+	public void setFilterValue(String filterValue) {
+		_filterValue = filterValue;
+	}
+
+	public void setPath(String path) {
+		_path = path;
+	}
+
+	@Override
+	protected BooleanClause<Filter> doGetFacetFilterBooleanClause() {
+		String[] selections = getSelections();
+
+		if (ArrayUtil.isEmpty(selections)) {
+			return null;
+		}
+
+		TermsFilter nestedFilter = new TermsFilter(_filterField);
+
+		nestedFilter.addValue(_filterValue);
+
+		TermsFilter nestedAggregationFilter = new TermsFilter(getFieldName());
+
+		nestedAggregationFilter.addValues(selections);
+
+		BooleanFilter booleanFilter = new BooleanFilter();
+
+		booleanFilter.add(nestedFilter, BooleanClauseOccur.MUST);
+		booleanFilter.add(nestedAggregationFilter, BooleanClauseOccur.MUST);
+
+		BooleanQuery booleanQuery = new BooleanQueryImpl();
+
+		booleanQuery.setPreBooleanFilter(booleanFilter);
+
+		NestedQuery nestedQuery = new NestedQuery(_path, booleanQuery);
+
+		QueryFilter queryFilter = new QueryFilter(nestedQuery);
+
+		return new BooleanClauseImpl<>(queryFilter, BooleanClauseOccur.MUST);
+	}
+
+	private String _filterField;
+	private String _filterValue;
+	private String _path;
+
+}
