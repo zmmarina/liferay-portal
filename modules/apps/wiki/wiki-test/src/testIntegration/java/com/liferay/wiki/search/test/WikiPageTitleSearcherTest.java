6393ee730016f26372a30f81a9460234355d675f
André de Oliveira <andre.oliveira@liferay.com>
2021-01-06 15:45:29 -0800

    LPS-128441 wiki-test: Assert full results; count never helpful
    https://issues.liferay.com/browse/LPS-128441

--- modules/apps/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/search/test/WikiPageTitleSearcherTest.java
+++ modules/apps/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/search/test/WikiPageTitleSearcherTest.java
@@ -18,7 +18,6 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.BaseSearcher;
-import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.SearchContext;
@@ -34,14 +33,18 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.legacy.searcher.SearchResponseBuilderFactory;
+import com.liferay.portal.search.test.util.DocumentsAssert;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.wiki.model.WikiNode;
 import com.liferay.wiki.model.WikiPage;
-import com.liferay.wiki.service.WikiPageLocalServiceUtil;
+import com.liferay.wiki.service.WikiPageLocalService;
 import com.liferay.wiki.test.util.WikiTestUtil;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.Assert;
@@ -83,12 +86,9 @@ public class WikiPageTitleSearcherTest {
 		addPage("Gijon (Asturias)", RandomTestUtil.randomString());
 		addPage("Madrid", RandomTestUtil.randomString());
 
-		assertSearch("Asturias", 1);
-		assertSearch("Gijon", 2);
-		assertSearch("Madrid", 1);
-
-		assertSearchTitle("Asturias", "Gijon (Asturias)");
-		assertSearchTitle("Madrid", "Madrid");
+		assertSearch("Asturias", Arrays.asList("Gijon (Asturias)"));
+		assertSearch("Gijon", Arrays.asList("Gijon", "Gijon (Asturias)"));
+		assertSearch("Madrid", Arrays.asList("Madrid"));
 	}
 
 	@Test
@@ -96,7 +96,7 @@ public class WikiPageTitleSearcherTest {
 		addPage("Barcelona", "Spanish city");
 		addPage("Madrid", "Spanish city");
 
-		assertSearch("city", 0);
+		assertSearch("city", Arrays.asList());
 	}
 
 	@Test
@@ -108,7 +108,7 @@ public class WikiPageTitleSearcherTest {
 
 		addPage(node.getNodeId(), "Barcelona", RandomTestUtil.randomString());
 
-		assertSearch("Barcelona", 1);
+		assertSearch("Barcelona", Arrays.asList("Barcelona"));
 		assertSearchNode("Barcelona", _node.getNodeId());
 	}
 
@@ -120,12 +120,12 @@ public class WikiPageTitleSearcherTest {
 		addPage("Gijun", RandomTestUtil.randomString());
 		addPage("Gikon", RandomTestUtil.randomString());
 
-		assertSearch("G", 5);
-		assertSearch("Gi", 4);
-		assertSearch("Gij", 3);
-		assertSearch("Gijo", 2);
-		assertSearch("Gijon", 1);
-		assertSearchTitle("Gijon", "Gijon");
+		assertSearch(
+			"G", Arrays.asList("Gejon", "Gijom", "Gijon", "Gijun", "Gikon"));
+		assertSearch("Gi", Arrays.asList("Gijom", "Gijon", "Gijun", "Gikon"));
+		assertSearch("Gij", Arrays.asList("Gijom", "Gijon", "Gijun"));
+		assertSearch("Gijo", Arrays.asList("Gijom", "Gijon"));
+		assertSearch("Gijon", Arrays.asList("Gijon"));
 	}
 
 	protected void addPage(long nodeId, String title, String content)
@@ -143,47 +143,54 @@ public class WikiPageTitleSearcherTest {
 		addPage(_node.getNodeId(), title, content);
 	}
 
-	protected void assertSearch(String keywords, int length) throws Exception {
-		_searchContext.setKeywords(StringUtil.toLowerCase(keywords));
+	protected void assertSearch(
+			String keywords, Collection<String> expectedValues)
+		throws Exception {
 
-		Hits hits = _wikiPageSearcher.search(_searchContext);
+		Hits hits = search(keywords);
 
-		Assert.assertEquals(hits.toString(), length, hits.getLength());
+		DocumentsAssert.assertValuesIgnoreRelevance(
+			getRequestString(), hits.getDocs(), Field.TITLE, expectedValues);
 	}
 
 	protected void assertSearchNode(String keywords, long nodeId)
 		throws Exception {
 
-		_searchContext.setKeywords(StringUtil.toLowerCase(keywords));
-
-		Hits hits = _wikiPageSearcher.search(_searchContext);
+		Hits hits = search(keywords);
 
 		List<SearchResult> searchResults = SearchResultUtil.getSearchResults(
 			hits, LocaleUtil.getDefault());
 
 		for (SearchResult searchResult : searchResults) {
-			WikiPage page = WikiPageLocalServiceUtil.getPage(
+			WikiPage page = wikiPageLocalService.getPage(
 				searchResult.getClassPK());
 
 			Assert.assertEquals(nodeId, page.getNodeId());
 		}
 	}
 
-	protected void assertSearchTitle(String keywords, String title)
-		throws Exception {
-
-		_searchContext.setKeywords(StringUtil.toLowerCase(keywords));
-
-		Hits hits = _wikiPageSearcher.search(_searchContext);
-
-		for (Document document : hits.getDocs()) {
-			Assert.assertEquals(title, document.get(Field.TITLE));
-		}
+	protected String getRequestString() {
+		return searchResponseBuilderFactory.builder(
+			_searchContext
+		).build(
+		).getRequestString();
 	}
 
 	protected SearchContext getSearchContext(Group group) throws Exception {
 		return SearchContextTestUtil.getSearchContext(group.getGroupId());
 	}
+
+	protected Hits search(String keywords) throws Exception {
+		_searchContext.setKeywords(StringUtil.toLowerCase(keywords));
+
+		return _wikiPageSearcher.search(_searchContext);
+	}
+
+	@Inject
+	protected static SearchResponseBuilderFactory searchResponseBuilderFactory;
+
+	@Inject
+	protected static WikiPageLocalService wikiPageLocalService;
 
 	@Inject(filter = "model.class.name=com.liferay.wiki.model.WikiPage")
 	private static BaseSearcher _wikiPageSearcher;
