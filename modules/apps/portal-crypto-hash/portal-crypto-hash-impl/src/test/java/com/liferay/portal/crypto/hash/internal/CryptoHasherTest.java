f3c4119b852b27528a5c186efe617b4525f39890
arthurchan35 <arthurchan35@gmail.com>
2021-01-11 13:37:45 +0800

    LPS-124770 Crypto Hash Test
    Co-authored-by: Carlos Sierra Andrés <carlos.sierra@liferay.com>
    
    LPS-124770 Turn test into unit test

---
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-impl/src/test/java/com/liferay/portal/crypto/hash/internal/CryptoHasherTest.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.crypto.hash.internal;
+
+import com.liferay.portal.crypto.hash.CryptoHasher;
+import com.liferay.portal.crypto.hash.generation.CryptoHashGenerationResponse;
+import com.liferay.portal.kernel.util.UnicodeFormatter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Arthur Chan
+ * @author Carlos Sierra Andrés
+ */
+public class CryptoHasherTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_cryptoHasher = new CryptoHasherImpl();
+	}
+
+	@Test
+	public void testGenerationAndVerification() throws Exception {
+		final CryptoHashGenerationResponse cryptoHashGenerationResponse =
+			_cryptoHasher.generate(_PASSWORD.getBytes());
+
+		Assert.assertTrue(
+			_cryptoHasher.verify(
+				_PASSWORD.getBytes(), cryptoHashGenerationResponse.getHash(),
+				cryptoHashGenerationResponse.getSalt()));
+
+		Assert.assertFalse(
+			_cryptoHasher.verify(
+				_WRONG_PASSWORD.getBytes(),
+				cryptoHashGenerationResponse.getHash(),
+				cryptoHashGenerationResponse.getSalt()));
+	}
+
+	@Test
+	public void testVerificationWithFixedHashAndSalt() throws Exception {
+		_cryptoHasher.verify(
+			_PASSWORD.getBytes(), _PASSWORD_HASH_WITH_SALT, _SALT_1.getBytes());
+	}
+
+	private static final String _PASSWORD = "password";
+
+	private static final byte[] _PASSWORD_HASH_WITH_SALT =
+		UnicodeFormatter.hexToBytes(
+			"ee765094649dcc6b5e89a91663cbeb80ecceed035e13201da471a97d30534f57" +
+				"1dd8974729feb4e1696485b1e054672d91c9e774514921c067028a46bcb6" +
+					"f1c5");
+
+	private static final String _SALT_1 = "salt1";
+
+	private static final String _WRONG_PASSWORD = "wrongPassword";
+
+	private CryptoHasher _cryptoHasher;
+
+}
