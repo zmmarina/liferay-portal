9c4421dea8cc3cc396cd6011ef3f58ee0bb038c9
drewbrokke <drew.brokke@liferay.com>
2021-01-29 17:42:46 -0600

    LPS-125701 commerce-account-service: CommerceAccountImpl: adds static transform methods

--- modules/apps/commerce/commerce-account-service/src/main/java/com/liferay/commerce/account/model/impl/CommerceAccountImpl.java
+++ modules/apps/commerce/commerce-account-service/src/main/java/com/liferay/commerce/account/model/impl/CommerceAccountImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.commerce.account.model.impl;
 
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.model.AccountEntry;
 import com.liferay.commerce.account.constants.CommerceAccountConstants;
 import com.liferay.commerce.account.model.CommerceAccount;
 import com.liferay.commerce.account.model.CommerceAccountOrganizationRel;
@@ -23,14 +25,139 @@ import com.liferay.commerce.account.service.CommerceAccountOrganizationRelLocalS
 import com.liferay.commerce.account.service.CommerceAccountUserRelLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 /**
  * @author Marco Leo
  * @author Alessio Antonio Rendina
  */
 public class CommerceAccountImpl extends CommerceAccountBaseImpl {
+
+	public static CommerceAccount fromAccountEntry(AccountEntry accountEntry) {
+		if (accountEntry == null) {
+			return null;
+		}
+
+		CommerceAccount commerceAccount = new CommerceAccountImpl();
+
+		Map<String, Object> modelAttributes = accountEntry.getModelAttributes();
+
+		Map<String, BiConsumer<CommerceAccount, Object>>
+			attributeSetterBiConsumers =
+				commerceAccount.getAttributeSetterBiConsumers();
+
+		for (Map.Entry<String, Object> entry : modelAttributes.entrySet()) {
+			String key = entry.getKey();
+
+			BiConsumer<CommerceAccount, Object>
+				commerceAccountObjectBiConsumer =
+					attributeSetterBiConsumers.get(key);
+
+			if (commerceAccountObjectBiConsumer != null) {
+				Object value = entry.getValue();
+
+				if (key.equals("type")) {
+					value = toCommerceAccountType((String)value);
+				}
+
+				commerceAccountObjectBiConsumer.accept(commerceAccount, value);
+			}
+		}
+
+		commerceAccount.setCommerceAccountId(accountEntry.getAccountEntryId());
+		commerceAccount.setParentCommerceAccountId(
+			accountEntry.getParentAccountEntryId());
+		commerceAccount.setEmail(accountEntry.getEmailAddress());
+		commerceAccount.setTaxId(accountEntry.getTaxIdNumber());
+		commerceAccount.setActive(
+			toCommerceAccountActive(accountEntry.getStatus()));
+		commerceAccount.setDisplayDate(null);
+		commerceAccount.setExpirationDate(null);
+		commerceAccount.setUserUuid(accountEntry.getUserUuid());
+
+		return commerceAccount;
+	}
+
+	public static Integer toAccountEntryStatus(Boolean commerceAccountActive) {
+		if (commerceAccountActive == null) {
+			return null;
+		}
+
+		if (commerceAccountActive) {
+			return WorkflowConstants.STATUS_APPROVED;
+		}
+
+		return WorkflowConstants.STATUS_INACTIVE;
+	}
+
+	public static String toAccountEntryType(int commerceAccountType) {
+		if (commerceAccountType ==
+				CommerceAccountConstants.ACCOUNT_TYPE_BUSINESS) {
+
+			return AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS;
+		}
+		else if (commerceAccountType ==
+					CommerceAccountConstants.ACCOUNT_TYPE_GUEST) {
+
+			return AccountConstants.ACCOUNT_ENTRY_TYPE_GUEST;
+		}
+		else if (commerceAccountType ==
+					CommerceAccountConstants.ACCOUNT_TYPE_PERSONAL) {
+
+			return AccountConstants.ACCOUNT_ENTRY_TYPE_PERSON;
+		}
+
+		return null;
+	}
+
+	public static String[] toAccountEntryTypes(int commerceSiteType) {
+		if (commerceSiteType == CommerceAccountConstants.SITE_TYPE_B2X) {
+			return new String[] {
+				AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS,
+				AccountConstants.ACCOUNT_ENTRY_TYPE_PERSON
+			};
+		}
+		else if (commerceSiteType == CommerceAccountConstants.SITE_TYPE_B2B) {
+			return new String[] {AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS};
+		}
+		else if (commerceSiteType == CommerceAccountConstants.SITE_TYPE_B2C) {
+			return new String[] {AccountConstants.ACCOUNT_ENTRY_TYPE_PERSON};
+		}
+
+		return null;
+	}
+
+	public static boolean toCommerceAccountActive(int accountEntryStatus) {
+		if (accountEntryStatus == WorkflowConstants.STATUS_APPROVED) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static Integer toCommerceAccountType(String accountEntryType) {
+		if (accountEntryType.equals(
+				AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS)) {
+
+			return CommerceAccountConstants.ACCOUNT_TYPE_BUSINESS;
+		}
+		else if (accountEntryType.equals(
+					AccountConstants.ACCOUNT_ENTRY_TYPE_GUEST)) {
+
+			return CommerceAccountConstants.ACCOUNT_TYPE_GUEST;
+		}
+		else if (accountEntryType.equals(
+					AccountConstants.ACCOUNT_ENTRY_TYPE_PERSON)) {
+
+			return CommerceAccountConstants.ACCOUNT_TYPE_PERSONAL;
+		}
+
+		return CommerceAccountConstants.ACCOUNT_TYPE_GUEST;
+	}
 
 	public CommerceAccountImpl() {
 	}
