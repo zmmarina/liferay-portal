d468682920d0d210be4d08ada2a8411b3e179e43
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-02-01 19:15:47 -0300

    LPS-125938 Pass DDMForm related to DDMFormLayout instead of serialize DataDefinition, which not always is related to that DDMFormLayout

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
@@ -216,7 +216,7 @@ public class DataDefinitionResourceImpl
 						deDataDefinitionFieldLink.getFieldName())));
 
 			_removeFieldsFromDataLayoutsAndDataListViews(
-				dataDefinition, deDataDefinitionFieldLink.getClassPK(),
+				deDataDefinitionFieldLink.getClassPK(),
 				_getRemovedFieldNames(dataDefinition, dataDefinition.getId()));
 
 			_updateDataDefinition(
@@ -618,7 +618,7 @@ public class DataDefinitionResourceImpl
 		}
 
 		_removeFieldsFromDataLayoutsAndDataListViews(
-			dataDefinition, dataDefinitionId,
+			dataDefinitionId,
 			_getRemovedFieldNames(dataDefinition, dataDefinitionId));
 
 		_deDataDefinitionFieldLinkLocalService.deleteDEDataDefinitionFieldLinks(
@@ -1069,8 +1069,7 @@ public class DataDefinitionResourceImpl
 	}
 
 	private void _removeFieldsFromDataLayouts(
-			DataDefinition dataDefinition, Set<Long> ddmStructureLayoutIds,
-			String[] fieldNames)
+			Set<Long> ddmStructureLayoutIds, String[] fieldNames)
 		throws Exception {
 
 		for (Long ddmStructureLayoutId : ddmStructureLayoutIds) {
@@ -1090,10 +1089,10 @@ public class DataDefinitionResourceImpl
 			String definitionSchemaVersion =
 				ddmFormLayout.getDefinitionSchemaVersion();
 
+			DDMStructure ddmStructure = ddmStructureLayout.getDDMStructure();
+
 			ddmFormLayout = DataLayoutUtil.toDDMFormLayout(
-				dataLayout,
-				DataDefinitionDDMFormUtil.toDDMForm(
-					dataDefinition, _ddmFormFieldTypeServicesTracker),
+				dataLayout, ddmStructure.getDDMForm(),
 				_ddmFormFieldTypeServicesTracker, _ddmFormRuleDeserializer);
 
 			ddmFormLayout.setDefinitionSchemaVersion(definitionSchemaVersion);
@@ -1115,8 +1114,7 @@ public class DataDefinitionResourceImpl
 	}
 
 	private void _removeFieldsFromDataLayoutsAndDataListViews(
-			DataDefinition dataDefinition, long dataDefinitionId,
-			String[] fieldNames)
+			long dataDefinitionId, String[] fieldNames)
 		throws Exception {
 
 		Set<Long> ddmStructureLayoutIds = new HashSet<>();
@@ -1144,8 +1142,7 @@ public class DataDefinitionResourceImpl
 			_portal.getClassNameId(DEDataListView.class), dataDefinitionId,
 			fieldNames);
 
-		_removeFieldsFromDataLayouts(
-			dataDefinition, ddmStructureLayoutIds, fieldNames);
+		_removeFieldsFromDataLayouts(ddmStructureLayoutIds, fieldNames);
 		_removeFieldsFromDataListViews(deDataListViewIds, fieldNames);
 	}
 
