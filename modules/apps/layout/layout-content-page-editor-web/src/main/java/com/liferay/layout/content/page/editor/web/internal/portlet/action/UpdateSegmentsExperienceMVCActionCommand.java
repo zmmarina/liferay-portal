a76ef0ebd8aef9d715169c3f07442cd1fedc0e6f
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-02-01 19:10:25 +0100

    LPS-124170 Read languageIds parameter and save it in DB when the entity is updated.

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateSegmentsExperienceMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateSegmentsExperienceMVCActionCommand.java
@@ -20,6 +20,9 @@ import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.segments.service.SegmentsExperienceService;
 
 import java.util.Collections;
@@ -55,12 +58,21 @@ public class UpdateSegmentsExperienceMVCActionCommand
 		long segmentsEntryId = ParamUtil.getLong(
 			actionRequest, "segmentsEntryId");
 		String name = ParamUtil.getString(actionRequest, "name");
+		String[] languageIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "languageIds", ""));
+
+		UnicodeProperties typeSettingsUnicodeProperties = new UnicodeProperties(
+			true);
+
+		typeSettingsUnicodeProperties.setProperty(
+			PropsKeys.LOCALES, StringUtil.merge(languageIds));
 
 		return JSONUtil.put(
 			"segmentsExperience",
 			_segmentsExperienceService.updateSegmentsExperience(
 				segmentsExperienceId, segmentsEntryId,
-				Collections.singletonMap(LocaleUtil.getDefault(), name), true));
+				Collections.singletonMap(LocaleUtil.getDefault(), name), true,
+				typeSettingsUnicodeProperties));
 	}
 
 	@Reference
