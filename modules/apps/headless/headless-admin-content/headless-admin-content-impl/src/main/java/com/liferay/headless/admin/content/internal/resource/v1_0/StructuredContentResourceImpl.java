3033d0a30325b8c30b2d72ccb876bfb3d7102778
Luismi Barcos <luismiguel.barcos@liferay.com>
2021-02-02 19:20:43 +0100

    LPS-128276 Add implementation to retrieve the list of structured contents with their last versions
    Co-authored-by: Javier de Arcos <javier.dearcos@liferay.com>

--- modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/StructuredContentResourceImpl.java
+++ modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/StructuredContentResourceImpl.java
@@ -14,9 +14,41 @@
 
 package com.liferay.headless.admin.content.internal.resource.v1_0;
 
+import com.liferay.dynamic.data.mapping.util.DDMIndexer;
+import com.liferay.headless.admin.content.internal.dto.v1_0.extension.ExtendedStructuredContent;
+import com.liferay.headless.admin.content.internal.dto.v1_0.util.VersionUtil;
 import com.liferay.headless.admin.content.resource.v1_0.StructuredContentResource;
+import com.liferay.headless.delivery.dto.v1_0.StructuredContent;
+import com.liferay.headless.delivery.search.aggregation.AggregationUtil;
+import com.liferay.headless.delivery.search.sort.SortUtil;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleService;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.aggregation.Aggregations;
+import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.sort.Sorts;
+import com.liferay.portal.vulcan.aggregation.Aggregation;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.util.ContentLanguageUtil;
+import com.liferay.portal.vulcan.util.EntityExtensionUtil;
+import com.liferay.portal.vulcan.util.SearchUtil;
+
+import java.util.Collections;
+
+import javax.validation.constraints.NotNull;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +60,110 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class StructuredContentResourceImpl
 	extends BaseStructuredContentResourceImpl {
+
+	@Override
+	public Page<StructuredContent> getSiteStructuredContentsPage(
+			@NotNull Long siteId, Boolean flatten, String search,
+			Aggregation aggregation, Filter filter, Pagination pagination,
+			Sort[] sorts)
+		throws Exception {
+
+		return SearchUtil.search(
+			null,
+			booleanQuery -> {
+			},
+			filter, JournalArticle.class, search, pagination,
+			queryConfig -> queryConfig.setSelectedFieldNames(
+				Field.ARTICLE_ID, Field.SCOPE_GROUP_ID),
+			searchContext -> {
+				searchContext.addVulcanAggregation(aggregation);
+				searchContext.setAttribute(
+					Field.STATUS, WorkflowConstants.STATUS_APPROVED);
+				searchContext.setAttribute("head", Boolean.TRUE);
+				searchContext.setCompanyId(contextCompany.getCompanyId());
+
+				if (siteId != null) {
+					searchContext.setGroupIds(new long[] {siteId});
+				}
+
+				SearchRequestBuilder searchRequestBuilder =
+					_searchRequestBuilderFactory.builder(searchContext);
+
+				AggregationUtil.processVulcanAggregation(
+					_aggregations, _ddmIndexer, _queries, searchRequestBuilder,
+					aggregation);
+
+				SortUtil.processSorts(
+					_ddmIndexer, searchRequestBuilder, searchContext.getSorts(),
+					_queries, _sorts);
+			},
+			sorts,
+			document -> {
+				JournalArticle journalArticle =
+					_journalArticleService.getLatestArticle(
+						GetterUtil.getLong(document.get(Field.SCOPE_GROUP_ID)),
+						document.get(Field.ARTICLE_ID),
+						WorkflowConstants.STATUS_ANY);
+
+				return _toExtendedStructuredContent(journalArticle);
+			});
+	}
+
+	private StructuredContent _toExtendedStructuredContent(
+			JournalArticle journalArticle)
+		throws Exception {
+
+		ContentLanguageUtil.addContentLanguageHeader(
+			journalArticle.getAvailableLanguageIds(),
+			journalArticle.getDefaultLanguageId(), contextHttpServletResponse,
+			contextAcceptLanguage.getPreferredLocale());
+
+		DTOConverter<JournalArticle, StructuredContent> dtoConverter =
+			(DTOConverter<JournalArticle, StructuredContent>)
+				_dtoConverterRegistry.getDTOConverter(
+					JournalArticle.class.getName());
+
+		if (dtoConverter == null) {
+			return null;
+		}
+
+		StructuredContent structuredContent = dtoConverter.toDTO(
+			new DefaultDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(),
+				Collections.emptyMap(), _dtoConverterRegistry,
+				contextHttpServletRequest, journalArticle.getResourcePrimKey(),
+				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+				contextUser),
+			journalArticle);
+
+		return EntityExtensionUtil.extend(
+			structuredContent, StructuredContent.class,
+			ExtendedStructuredContent.class,
+			extendedStructuredContent -> extendedStructuredContent.setVersion(
+				VersionUtil.toVersion(
+					journalArticle.getGroupId(), journalArticle.getStatus(),
+					journalArticle.getVersion())));
+	}
+
+	@Reference
+	private Aggregations _aggregations;
+
+	@Reference
+	private DDMIndexer _ddmIndexer;
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
+	private JournalArticleService _journalArticleService;
+
+	@Reference
+	private Queries _queries;
+
+	@Reference
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+	@Reference
+	private Sorts _sorts;
+
 }
