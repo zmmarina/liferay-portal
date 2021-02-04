1cf82b333091c6ba5a16493b341ba1b8bebffdf9
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-02-03 23:35:57 -0300

    LPS-127127 Move Table component to data engine js components web

---
+++ modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/components/table/DropDown.es.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {ClayButtonWithIcon} from '@clayui/button';
+import ClayDropDown, {Align} from '@clayui/drop-down';
+import {ClayTooltipProvider} from '@clayui/tooltip';
+import React, {cloneElement, useState} from 'react';
+
+import DropDownAction from './DropDownAction.es';
+
+const {ItemList} = ClayDropDown;
+
+export default ({actions, item, noActionsMessage}) => {
+	const [active, setActive] = useState(false);
+
+	const DropdownButton = (
+		<ClayButtonWithIcon
+			className="page-link"
+			displayType="unstyled"
+			symbol="ellipsis-v"
+		/>
+	);
+
+	actions = actions.filter((action) =>
+		action.show ? action.show(item) : true
+	);
+
+	if (actions.length === 0) {
+		return (
+			<ClayTooltipProvider>
+				{cloneElement(DropdownButton, {
+					'data-tooltip-align': 'bottom',
+					'data-tooltip-delay': '200',
+					disabled: true,
+					title: noActionsMessage,
+				})}
+			</ClayTooltipProvider>
+		);
+	}
+
+	return (
+		<ClayDropDown
+			active={active}
+			alignmentPosition={Align.RightCenter}
+			className="dropdown-action"
+			onActiveChange={(newVal) => setActive(newVal)}
+			trigger={DropdownButton}
+		>
+			<ItemList>
+				{actions.map((action, index) => (
+					<DropDownAction
+						action={action}
+						item={item}
+						key={index}
+						setActive={setActive}
+					/>
+				))}
+			</ItemList>
+		</ClayDropDown>
+	);
+};
---
+++ modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/components/table/TableWithPagination.es.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayLayout from '@clayui/layout';
+import {ClayPaginationBarWithBasicItems} from '@clayui/pagination-bar';
+import React, {useContext} from 'react';
+
+import {withLoading} from '../loading/Loading.es';
+import SearchContext from '../management-toolbar/SearchContext.es';
+import {withEmpty} from './EmptyState.es';
+import Table from './Table.es';
+
+const TableWithPagination = ({
+	actions,
+	columns,
+	deltaValues = [4, 8, 20, 40, 60],
+	editMode,
+	items,
+	noActionsMessage,
+	totalCount,
+}) => {
+	const [{page, pageSize}, dispatch] = useContext(SearchContext);
+
+	const deltas = deltaValues.map((label) => ({label}));
+
+	return (
+		<ClayLayout.ContainerFluid>
+			<Table
+				actions={actions}
+				columns={columns}
+				editMode={editMode}
+				items={items}
+				noActionsMessage={noActionsMessage}
+			/>
+
+			{totalCount > deltaValues[0] && (
+				<div className="taglib-search-iterator-page-iterator-bottom">
+					<ClayPaginationBarWithBasicItems
+						activeDelta={Number(pageSize)}
+						activePage={Number(page)}
+						deltas={deltas}
+						ellipsisBuffer={3}
+						onDeltaChange={(pageSize) =>
+							dispatch({pageSize, type: 'CHANGE_PAGE_SIZE'})
+						}
+						onPageChange={(page) =>
+							dispatch({page, type: 'CHANGE_PAGE'})
+						}
+						totalItems={totalCount}
+					/>
+				</div>
+			)}
+		</ClayLayout.ContainerFluid>
+	);
+};
+
+export default withLoading(withEmpty(TableWithPagination));
