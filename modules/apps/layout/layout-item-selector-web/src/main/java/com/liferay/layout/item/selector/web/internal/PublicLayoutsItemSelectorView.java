ec86113840147967297ed6c6be8f42d1021cb1ea
Victor Galan <victor.galan@liferay.com>
2021-02-08 15:56:38 +0100

    LPS-127359 Show public pages view always, when there isn't any public page an empty state will be displayed

--- modules/apps/layout/layout-item-selector-web/src/main/java/com/liferay/layout/item/selector/web/internal/PublicLayoutsItemSelectorView.java
+++ modules/apps/layout/layout-item-selector-web/src/main/java/com/liferay/layout/item/selector/web/internal/PublicLayoutsItemSelectorView.java
@@ -15,9 +15,6 @@
 package com.liferay.layout.item.selector.web.internal;
 
 import com.liferay.item.selector.ItemSelectorView;
-import com.liferay.layout.item.selector.criterion.LayoutItemSelectorCriterion;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
@@ -51,20 +48,6 @@ public class PublicLayoutsItemSelectorView extends BaseLayoutsItemSelectorView {
 	@Override
 	public boolean isPrivateLayout() {
 		return false;
-	}
-
-	@Override
-	public boolean isVisible(
-		LayoutItemSelectorCriterion layoutItemSelectorCriterion,
-		ThemeDisplay themeDisplay) {
-
-		Group group = themeDisplay.getScopeGroup();
-
-		if (group.getPublicLayoutsPageCount() <= 0) {
-			return false;
-		}
-
-		return true;
 	}
 
 	@Reference(
