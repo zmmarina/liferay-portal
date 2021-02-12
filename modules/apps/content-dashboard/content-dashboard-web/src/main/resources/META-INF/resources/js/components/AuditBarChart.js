3dd742d988a6b54f5dc162efc3c46f486b956fc1
Sarai Díaz García <dgarciasarai@gmail.com>
2021-02-12 12:00:54 +0100

    LPS-120676 Add onClick event for bars: applied category filter

--- modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/AuditBarChart.js
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/AuditBarChart.js
@@ -15,8 +15,9 @@
 import ClayEmptyState from '@clayui/empty-state';
 import {ClayCheckbox} from '@clayui/form';
 import ClayLayout from '@clayui/layout';
+import {useEventListener} from 'frontend-js-react-web';
 import PropTypes from 'prop-types';
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {
 	Bar,
 	BarChart,
@@ -31,6 +32,11 @@ import {
 
 import {BAR_CHART, COLORS, DEFAULT_COLOR} from '../utils/constants';
 import {shortenNumber} from '../utils/shortenNumber';
+
+const ESCAPE_KEYS = [
+	'Escape', // Most browsers.
+	'Esc', // IE and Edge.
+];
 
 export default function AuditBarChart({namespace, rtl, vocabularies}) {
 	const auditBarChartData = useMemo(() => {
@@ -86,7 +92,7 @@ export default function AuditBarChart({namespace, rtl, vocabularies}) {
 						[key]: value,
 					};
 				},
-				{name: category.name}
+				{key: category.key, name: category.name}
 			);
 		});
 
@@ -206,6 +212,60 @@ export default function AuditBarChart({namespace, rtl, vocabularies}) {
 
 	const [tooltip, setTooltip] = useState(null);
 
+	const onBarClick = (assetCategoryIds) => {
+		if (assetCategoryIds.length) {
+			let uri = window.location.href;
+
+			if (!uri.includes('resetBarsCategoryFiltersURL')) {
+				uri = Liferay.Util.addParams(
+					'resetBarsCategoryFiltersURL=' + encodeURIComponent(uri),
+					uri
+				);
+			}
+
+			const params = new URLSearchParams(window.location.search);
+
+			const categories = params.getAll(namespace + 'assetCategoryId');
+
+			if (categories.length) {
+				categories.map((category) => {
+					uri = uri.replace(
+						namespace + 'assetCategoryId=' + category,
+						''
+					);
+				});
+			}
+
+			assetCategoryIds.map((assetCategoryId) => {
+				if (assetCategoryId !== 'none') {
+					uri = Liferay.Util.addParams(
+						namespace + 'assetCategoryId=' + assetCategoryId,
+						uri
+					);
+				}
+			});
+
+			Liferay.Util.navigate(uri);
+		}
+	};
+
+	const handleKeydown = useCallback((event) => {
+		const uri = window.location.href;
+
+		if (
+			ESCAPE_KEYS.includes(event.key) &&
+			uri.includes('resetBarsCategoryFiltersURL')
+		) {
+			const params = new URLSearchParams(uri);
+
+			Liferay.Util.navigate(
+				decodeURIComponent(params.get('resetBarsCategoryFiltersURL'))
+			);
+		}
+	}, []);
+
+	useEventListener('keydown', handleKeydown, true, document);
+
 	return (
 		<>
 			{Object.keys(checkboxes).length > 0 && noCheckboxesChecked && (
@@ -284,6 +344,12 @@ export default function AuditBarChart({namespace, rtl, vocabularies}) {
 									key={index}
 									legendType="square"
 									name={bar.name}
+									onClick={(props) =>
+										onBarClick([
+											props.payload.key,
+											bar.dataKey,
+										])
+									}
 									onMouseOut={() => {
 										setTooltip(null);
 									}}
@@ -293,6 +359,7 @@ export default function AuditBarChart({namespace, rtl, vocabularies}) {
 											name: props.name,
 										});
 									}}
+									style={{cursor: 'pointer'}}
 								>
 									{data.map((entry, index) => (
 										<Cell
@@ -317,6 +384,7 @@ export default function AuditBarChart({namespace, rtl, vocabularies}) {
 						<Bar
 							barSize={BAR_CHART.barHeight}
 							dataKey="value"
+							onClick={(props) => onBarClick([props.payload.key])}
 							onMouseOut={() => {
 								setTooltip(null);
 							}}
@@ -326,6 +394,7 @@ export default function AuditBarChart({namespace, rtl, vocabularies}) {
 									name: props.name,
 								});
 							}}
+							style={{cursor: 'pointer'}}
 						>
 							{data.map((entry, index) => (
 								<Cell
@@ -416,6 +485,7 @@ function CustomYAxisTick(props) {
 }
 
 AuditBarChart.propTypes = {
+	namespace: PropTypes.string.isRequired,
 	rtl: PropTypes.bool.isRequired,
 	vocabularies: PropTypes.array.isRequired,
 };
