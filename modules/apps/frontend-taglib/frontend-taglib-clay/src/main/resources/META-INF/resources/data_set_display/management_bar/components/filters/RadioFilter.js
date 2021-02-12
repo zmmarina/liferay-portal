2fb2682dfd99aa90871689633b9a5c7dd46fcd9d
garofalog <giulio.garofalo@liferay.com>
2021-02-12 16:26:13 +0100

    COMMERCE-5551 changes alignment with pull/584

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/data_set_display/management_bar/components/filters/RadioFilter.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/data_set_display/management_bar/components/filters/RadioFilter.js
@@ -71,7 +71,7 @@ function RadioFilter({id, items, updateFilterState, value: valueProp}) {
 				<div className="inline-scroller mb-n2 mx-n2 px-2">
 					<ClayRadioGroup
 						onSelectedValueChange={setItemValue}
-						selectedValue={itemValue || ''}
+						selectedValue={itemValue ?? ''}
 					>
 						{items.map((item) => (
 							<ClayRadio
