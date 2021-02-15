3b7db67bb1f2fca44e06889a8aa1bf4f13b0e528
Pablo Molina <contact@pablomolina.me>
2021-02-15 12:07:11 +0100

    LPS-127107 Add LayoutSelector

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/LayoutSelector.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import {config} from '../../app/config/index';
+import ItemSelector from './ItemSelector';
+
+export const LayoutSelector = ({mappedLayout, onLayoutSelect}) => {
+	return (
+		<div className="mb-3">
+			<ItemSelector
+				eventName={`${config.portletNamespace}selectLayout`}
+				itemSelectorURL={config.layoutItemSelectorURL}
+				onItemSelect={(layout) => onLayoutSelect(layout)}
+				selectedItemTitle={mappedLayout?.name || ''}
+			/>
+		</div>
+	);
+};
+
+LayoutSelector.propTypes = {
+	mappedLayout: PropTypes.shape({
+		groupId: PropTypes.string.isRequired,
+		layoutId: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		privateLayout: PropTypes.bool.isRequired,
+	}),
+	onLayoutSelect: PropTypes.func.isRequired,
+};
