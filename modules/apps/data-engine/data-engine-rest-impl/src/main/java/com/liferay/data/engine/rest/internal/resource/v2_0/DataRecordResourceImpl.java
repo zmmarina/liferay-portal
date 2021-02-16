76a7ca76d77f6a98c1c0a2516f883856f79ac408
Jorge Díaz <jorge.diaz@liferay.com>
2021-02-16 08:03:53 +0100

    LPS-124433 data-engine: Move the data engine sort logic and the query filter code to the DDMIndexer class

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataRecordResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataRecordResourceImpl.java
@@ -32,8 +32,6 @@ import com.liferay.dynamic.data.lists.model.DDLRecordSetVersion;
 import com.liferay.dynamic.data.lists.service.DDLRecordLocalService;
 import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalService;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeServicesTracker;
-import com.liferay.dynamic.data.mapping.model.DDMFormField;
-import com.liferay.dynamic.data.mapping.model.DDMFormFieldType;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
 import com.liferay.dynamic.data.mapping.service.DDMStorageLinkLocalService;
@@ -62,7 +60,6 @@ import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.odata.entity.EntityField;
@@ -71,7 +68,6 @@ import com.liferay.portal.odata.entity.StringEntityField;
 import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.query.Queries;
 import com.liferay.portal.search.sort.FieldSort;
-import com.liferay.portal.search.sort.NestedSort;
 import com.liferay.portal.search.sort.SortOrder;
 import com.liferay.portal.search.sort.Sorts;
 import com.liferay.portal.vulcan.pagination.Page;
@@ -229,7 +225,7 @@ public class DataRecordResourceImpl
 					_searchRequestBuilderFactory.builder(
 						searchContext
 					).sorts(
-						_getFieldSorts(ddlRecordSet.getDDMStructure(), sorts)
+						_getSearchSorts(ddlRecordSet.getDDMStructure(), sorts)
 					);
 				}
 
@@ -441,10 +437,8 @@ public class DataRecordResourceImpl
 
 				fieldBooleanFilter.add(
 					_ddmIndexer.createFieldValueQueryFilter(
-						_getIndexFieldName(
-							ddmStructure.getStructureId(), fieldName,
-							contextAcceptLanguage.getPreferredLocale()),
-						value, contextAcceptLanguage.getPreferredLocale()),
+						ddmStructure, fieldName, value,
+						contextAcceptLanguage.getPreferredLocale()),
 					BooleanClauseOccur.SHOULD);
 			}
 
@@ -482,76 +476,31 @@ public class DataRecordResourceImpl
 		return ddlRecordSet.getRecordSetId();
 	}
 
-	private FieldSort[] _getFieldSorts(DDMStructure ddmStructure, Sort[] sorts)
+	private com.liferay.portal.search.sort.Sort[] _getSearchSorts(
+			DDMStructure ddmStructure, Sort[] sorts)
 		throws PortalException {
 
-		List<FieldSort> fieldSorts = new ArrayList<>();
+		List<com.liferay.portal.search.sort.Sort> searchSorts =
+			new ArrayList<>();
 
 		for (Sort sort : sorts) {
 			String fieldName = sort.getFieldName();
 
-			FieldSort fieldSort = _sorts.field(
-				_getSortableFieldName(ddmStructure, fieldName));
+			SortOrder sortOrder = SortOrder.ASC;
 
 			if (sort.isReverse()) {
-				fieldSort.setSortOrder(SortOrder.DESC);
+				sortOrder = SortOrder.DESC;
 			}
 
-			NestedSort nestedSort = _sorts.nested(DDMIndexer.DDM_FIELD_ARRAY);
+			com.liferay.portal.search.sort.Sort searchSort =
+				_ddmIndexer.createDDMStructureFieldSort(
+					ddmStructure, fieldName,
+					contextAcceptLanguage.getPreferredLocale(), sortOrder);
 
-			nestedSort.setFilterQuery(
-				_queries.term(
-					StringBundler.concat(
-						DDMIndexer.DDM_FIELD_ARRAY, StringPool.PERIOD,
-						DDMIndexer.DDM_FIELD_NAME),
-					_getIndexFieldName(
-						ddmStructure.getStructureId(), fieldName,
-						contextAcceptLanguage.getPreferredLocale())));
-
-			fieldSort.setNestedSort(nestedSort);
-
-			fieldSorts.add(fieldSort);
+			searchSorts.add(searchSort);
 		}
 
-		return fieldSorts.toArray(new FieldSort[0]);
-	}
-
-	private String _getIndexFieldName(
-		long ddmStructureId, String fieldName, Locale locale) {
-
-		return _ddmIndexer.encodeName(ddmStructureId, fieldName, locale);
-	}
-
-	private String _getSortableFieldName(
-			DDMStructure ddmStructure, String fieldName)
-		throws PortalException {
-
-		StringBundler sb = new StringBundler(5);
-
-		sb.append(DDMIndexer.DDM_FIELD_ARRAY);
-		sb.append(StringPool.PERIOD);
-		sb.append(
-			_ddmIndexer.getValueFieldName(
-				ddmStructure.getFieldProperty(fieldName, "indexType"),
-				contextAcceptLanguage.getPreferredLocale()));
-		sb.append(StringPool.UNDERLINE);
-
-		DDMFormField ddmFormField = ddmStructure.getDDMFormField(fieldName);
-
-		String ddmFormFieldType = ddmFormField.getType();
-
-		if (StringUtil.equals(ddmFormFieldType, DDMFormFieldType.DECIMAL) ||
-			StringUtil.equals(ddmFormFieldType, DDMFormFieldType.INTEGER) ||
-			StringUtil.equals(ddmFormFieldType, DDMFormFieldType.NUMBER) ||
-			StringUtil.equals(ddmFormFieldType, DDMFormFieldType.NUMERIC)) {
-
-			sb.append("Number");
-		}
-		else {
-			sb.append("String");
-		}
-
-		return Field.getSortableFieldName(sb.toString());
+		return searchSorts.toArray(new FieldSort[0]);
 	}
 
 	private DataRecord _toDataRecord(DDLRecord ddlRecord) throws Exception {
