dd98017abdb5fb5c53e69cc525eefd0d1784e57a
Javier de Arcos <javier.dearcos@liferay.com>
2021-02-16 07:42:51 +0100

    LPS-127827 Create DataLimitException in Portal Kernel

---
+++ portal-kernel/src/com/liferay/portal/kernel/exception/DataLimitException.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.exception;
+
+/**
+ * @author Javier de Arcos
+ */
+public class DataLimitException extends PortalException {
+
+	public DataLimitException() {
+	}
+
+	public DataLimitException(String msg) {
+		super(msg);
+	}
+
+	public DataLimitException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public DataLimitException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
