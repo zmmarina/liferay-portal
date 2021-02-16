42f6a696d98731183a848bede85f712096399260
tina.tian <tina.tian@liferay.com>
2021-02-16 11:34:18 -0800

    LPS-125549 Extract logic into Log4jConfigUtil#configureLog4J

--- modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
+++ modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
@@ -15,9 +15,9 @@
 package com.liferay.petra.log4j;
 
 import com.liferay.petra.io.StreamUtil;
+import com.liferay.petra.log4j.internal.Log4jConfigUtil;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactory;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -35,18 +35,12 @@ import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggerRepository;
-import org.apache.log4j.xml.DOMConfigurator;
-
-import org.dom4j.Document;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
 
 /**
  * @author Brian Wing Shun Chan
@@ -91,45 +85,15 @@ public class Log4JUtil {
 		urlContent = StringUtil.replace(
 			urlContent, "@liferay.home@", _getLiferayHome());
 
-		Document document = null;
+		Map<String, String> priorities = null;
 
-		try {
-			SAXReader saxReader = new SAXReader();
-
-			document = saxReader.read(new UnsyncStringReader(urlContent));
+		if (ServerDetector.getServerId() != null) {
+			priorities = Log4jConfigUtil.configureLog4J(urlContent);
 		}
-		catch (Exception exception) {
-			_log.error(exception, exception);
-
-			return;
+		else {
+			priorities = Log4jConfigUtil.configureLog4J(
+				urlContent, "TEXT_FILE", "XML_FILE");
 		}
-
-		Map<String, String> priorities = new HashMap<>();
-
-		Element rootElement = document.getRootElement();
-
-		for (Element element : rootElement.elements()) {
-			if (ServerDetector.getServerId() == null) {
-				_removeAppender(rootElement, element, "TEXT_FILE");
-				_removeAppender(rootElement, element, "XML_FILE");
-			}
-
-			if (Objects.equals("category", element.getName())) {
-				Element priorityElement = element.element("priority");
-
-				priorities.put(
-					element.attributeValue("name"),
-					priorityElement.attributeValue("value"));
-			}
-		}
-
-		// See LPS-6029, LPS-8865, and LPS-24280
-
-		DOMConfigurator domConfigurator = new DOMConfigurator();
-
-		domConfigurator.doConfigure(
-			new UnsyncStringReader(document.asXML()),
-			LogManager.getLoggerRepository());
 
 		for (Map.Entry<String, String> entry : priorities.entrySet()) {
 			java.util.logging.Logger jdkLogger =
@@ -255,25 +219,6 @@ public class Log4JUtil {
 		}
 
 		return _liferayHome;
-	}
-
-	private static void _removeAppender(
-		Element rootElement, Element element, String appenderName) {
-
-		if (Objects.equals("appender", element.getName()) &&
-			Objects.equals(appenderName, element.attributeValue("name"))) {
-
-			rootElement.remove(element);
-		}
-
-		for (Element childElement : element.elements()) {
-			if (Objects.equals("appender-ref", childElement.getName()) &&
-				Objects.equals(
-					appenderName, childElement.attributeValue("ref"))) {
-
-				element.remove(childElement);
-			}
-		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(Log4JUtil.class);
---
+++ modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/internal/Log4jConfigUtil.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.petra.log4j.internal;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+/**
+ * @author Tina Tian
+ */
+public class Log4jConfigUtil {
+
+	public static Map<String, String> configureLog4J(
+		String xmlContent, String... removedAppenderNames) {
+
+		Document document = null;
+
+		try {
+			SAXReader saxReader = new SAXReader();
+
+			document = saxReader.read(new UnsyncStringReader(xmlContent));
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			return Collections.emptyMap();
+		}
+
+		Map<String, String> priorities = new HashMap<>();
+
+		Element rootElement = document.getRootElement();
+
+		for (Element element : rootElement.elements()) {
+			for (String appenderName : removedAppenderNames) {
+				_removeAppender(rootElement, element, appenderName);
+			}
+
+			if (Objects.equals("category", element.getName())) {
+				Element priorityElement = element.element("priority");
+
+				priorities.put(
+					element.attributeValue("name"),
+					priorityElement.attributeValue("value"));
+			}
+		}
+
+		DOMConfigurator domConfigurator = new DOMConfigurator();
+
+		domConfigurator.doConfigure(
+			new UnsyncStringReader(document.asXML()),
+			LogManager.getLoggerRepository());
+
+		return priorities;
+	}
+
+	private static void _removeAppender(
+		Element rootElement, Element element, String appenderName) {
+
+		if (Objects.equals("appender", element.getName()) &&
+			Objects.equals(appenderName, element.attributeValue("name"))) {
+
+			rootElement.remove(element);
+		}
+
+		for (Element childElement : element.elements()) {
+			if (Objects.equals("appender-ref", childElement.getName()) &&
+				Objects.equals(
+					appenderName, childElement.attributeValue("ref"))) {
+
+				element.remove(childElement);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		Log4jConfigUtil.class);
+
+}
