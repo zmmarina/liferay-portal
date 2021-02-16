a61bbe7e56737213f0b3aa5ad504f5a39cbdd0d2
Lino Alves <lino.alves@liferay.com>
2021-02-16 11:27:48 -0300

    LPS-127929 extracting method

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/fieldset/FieldSetDDMFormFieldTemplateContextContributor.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/fieldset/FieldSetDDMFormFieldTemplateContextContributor.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -108,7 +109,8 @@ public class FieldSetDDMFormFieldTemplateContextContributor
 			"upgradedStructure",
 			GetterUtil.getBoolean(ddmFormField.getProperty("upgradedStructure"))
 		).put(
-			"visible", _isFieldSetVisible(nestedFields)
+			"visible",
+			ListUtil.isNotEmpty(_getVisibleNestedFields(nestedFields))
 		).build();
 	}
 
@@ -163,13 +165,8 @@ public class FieldSetDDMFormFieldTemplateContextContributor
 	protected JSONArray getRowsJSONArray(List<Object> nestedFields) {
 		JSONArray rowsJSONArray = jsonFactory.createJSONArray();
 
-		Stream<Object> visibleNestedFieldsStream = nestedFields.stream();
-
-		List<Object> visibleNestedFields = visibleNestedFieldsStream.filter(
-			this::isNestedFieldVisible
-		).collect(
-			Collectors.toList()
-		);
+		List<Object> visibleNestedFields = _getVisibleNestedFields(
+			nestedFields);
 
 		if (!visibleNestedFields.isEmpty()) {
 			rowsJSONArray.put(createRowJSONObject(visibleNestedFields));
@@ -221,20 +218,14 @@ public class FieldSetDDMFormFieldTemplateContextContributor
 	@Reference
 	protected JSONFactory jsonFactory;
 
-	private boolean _isFieldSetVisible(List<Object> nestedFields) {
+	private List<Object> _getVisibleNestedFields(List<Object> nestedFields) {
 		Stream<Object> visibleNestedFieldsStream = nestedFields.stream();
 
-		List<Object> visibleNestedFields = visibleNestedFieldsStream.filter(
+		return visibleNestedFieldsStream.filter(
 			this::isNestedFieldVisible
 		).collect(
 			Collectors.toList()
 		);
-
-		if (visibleNestedFields.isEmpty()) {
-			return false;
-		}
-
-		return true;
 	}
 
 	private boolean _needsLoadLayout(DDMFormField ddmFormField) {
