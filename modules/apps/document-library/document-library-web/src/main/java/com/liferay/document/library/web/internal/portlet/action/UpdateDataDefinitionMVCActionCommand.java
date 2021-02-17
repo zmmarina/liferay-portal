e4f606604ab9882875282c3167c4f56792697d1f
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-02-17 10:04:04 +0100

    LPS-127583 Fixing same issue on UpdateDataDefinitionMVCActionCommand

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/UpdateDataDefinitionMVCActionCommand.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/UpdateDataDefinitionMVCActionCommand.java
@@ -19,18 +19,22 @@ import com.liferay.data.engine.rest.dto.v2_0.DataLayout;
 import com.liferay.data.engine.rest.resource.exception.DataDefinitionValidationException;
 import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
 import com.liferay.document.library.constants.DLPortletKeys;
-import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseTransactionalMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -46,10 +50,45 @@ import org.osgi.service.component.annotations.Component;
 	},
 	service = MVCActionCommand.class
 )
-public class UpdateDataDefinitionMVCActionCommand extends BaseMVCActionCommand {
+public class UpdateDataDefinitionMVCActionCommand
+	extends BaseTransactionalMVCActionCommand {
 
 	@Override
-	protected void doProcessAction(
+	public boolean processAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws PortletException {
+
+		try {
+			return super.processAction(actionRequest, actionResponse);
+		}
+		catch (PortletException portletException) {
+			if (portletException.getCause() instanceof
+					DataDefinitionValidationException) {
+
+				DataDefinitionValidationException
+					dataDefinitionValidationException =
+						(DataDefinitionValidationException)
+							portletException.getCause();
+
+				SessionMessages.add(
+					actionRequest,
+					_portal.getPortletId(actionRequest) +
+						SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_ERROR_MESSAGE);
+
+				SessionErrors.add(
+					actionRequest, dataDefinitionValidationException.getClass(),
+					dataDefinitionValidationException);
+			}
+			else {
+				throw portletException;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	protected void doTransactionalCommand(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
@@ -68,24 +107,16 @@ public class UpdateDataDefinitionMVCActionCommand extends BaseMVCActionCommand {
 		dataDefinition.setDefaultDataLayout(
 			DataLayout.toDTO(ParamUtil.getString(actionRequest, "dataLayout")));
 
-		try {
-			if (ArrayUtil.isEmpty(dataDefinition.getDataDefinitionFields())) {
-				throw new DataDefinitionValidationException.MustSetFields();
-			}
-
-			dataDefinitionResource.putDataDefinition(
-				ParamUtil.getLong(actionRequest, "dataDefinitionId"),
-				dataDefinition);
+		if (ArrayUtil.isEmpty(dataDefinition.getDataDefinitionFields())) {
+			throw new DataDefinitionValidationException.MustSetFields();
 		}
-		catch (DataDefinitionValidationException
-					dataDefinitionValidationException) {
 
-			hideDefaultErrorMessage(actionRequest);
-
-			SessionErrors.add(
-				actionRequest, dataDefinitionValidationException.getClass(),
-				dataDefinitionValidationException);
-		}
+		dataDefinitionResource.putDataDefinition(
+			ParamUtil.getLong(actionRequest, "dataDefinitionId"),
+			dataDefinition);
 	}
+
+	@Reference
+	private Portal _portal;
 
 }
