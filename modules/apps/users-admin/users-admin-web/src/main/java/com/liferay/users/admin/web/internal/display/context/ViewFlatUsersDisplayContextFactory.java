fd32c409dcd5cf2cd33de2371d8ec5f9a1168357
drewbrokke <drew.brokke@liferay.com>
2021-02-17 17:58:52 -0600

    LPS-127686 users-admin-web: ViewFlatUsersDisplayContextFactory: uses static util

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/display/context/ViewFlatUsersDisplayContextFactory.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/display/context/ViewFlatUsersDisplayContextFactory.java
@@ -20,22 +20,19 @@ import com.liferay.portal.kernel.dao.search.RowChecker;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
-import com.liferay.portal.kernel.portlet.PortalPreferences;
-import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.usersadmin.search.UserSearch;
 import com.liferay.portlet.usersadmin.search.UserSearchTerms;
-import com.liferay.users.admin.constants.UsersAdminPortletKeys;
 import com.liferay.users.admin.management.toolbar.FilterContributor;
 import com.liferay.users.admin.web.internal.constants.UsersAdminWebKeys;
+import com.liferay.users.admin.web.internal.util.DisplayStyleUtil;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -58,44 +55,17 @@ public class ViewFlatUsersDisplayContextFactory {
 		ViewFlatUsersDisplayContext viewFlatUsersDisplayContext =
 			new ViewFlatUsersDisplayContext();
 
-		LiferayPortletRequest liferayPortletRequest =
-			PortalUtil.getLiferayPortletRequest(renderRequest);
-
-		String displayStyle = ParamUtil.getString(
-			liferayPortletRequest, "displayStyle");
-
-		HttpServletRequest httpServletRequest =
-			liferayPortletRequest.getOriginalHttpServletRequest();
-
-		if (Validator.isNull(displayStyle)) {
-			displayStyle = ParamUtil.getString(
-				httpServletRequest, "displayStyle");
-		}
-
-		PortalPreferences portalPreferences =
-			PortletPreferencesFactoryUtil.getPortalPreferences(
-				liferayPortletRequest);
-
-		if (Validator.isNull(displayStyle)) {
-			displayStyle = portalPreferences.getValue(
-				UsersAdminPortletKeys.USERS_ADMIN, "display-style", "list");
-		}
-		else {
-			portalPreferences.setValue(
-				UsersAdminPortletKeys.USERS_ADMIN, "display-style",
-				displayStyle);
-
-			httpServletRequest.setAttribute(
-				WebKeys.SINGLE_PAGE_APPLICATION_CLEAR_CACHE, Boolean.TRUE);
-		}
-
-		viewFlatUsersDisplayContext.setDisplayStyle(displayStyle);
+		viewFlatUsersDisplayContext.setDisplayStyle(
+			DisplayStyleUtil.getDisplayStyle(renderRequest, "list"));
 
 		UserSearch searchContainer = _createSearchContainer(
 			renderRequest, renderResponse);
 
 		UserSearchTerms userSearchTerms =
 			(UserSearchTerms)searchContainer.getSearchTerms();
+
+		LiferayPortletRequest liferayPortletRequest =
+			PortalUtil.getLiferayPortletRequest(renderRequest);
 
 		LiferayPortletResponse liferayPortletResponse =
 			PortalUtil.getLiferayPortletResponse(renderResponse);
@@ -105,6 +75,9 @@ public class ViewFlatUsersDisplayContextFactory {
 				liferayPortletRequest, liferayPortletResponse, searchContainer,
 				isShowDeleteButton(userSearchTerms),
 				isShowRestoreButton(userSearchTerms));
+
+		HttpServletRequest httpServletRequest =
+			liferayPortletRequest.getOriginalHttpServletRequest();
 
 		Optional<FilterContributor[]> filterContributorsOptional =
 			getFilterContributorsOptional(httpServletRequest);
