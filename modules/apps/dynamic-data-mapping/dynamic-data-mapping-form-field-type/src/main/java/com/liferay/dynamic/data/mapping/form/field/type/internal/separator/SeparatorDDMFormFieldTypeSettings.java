3a1328763cf4d2349d46f0a0674429df321a469f
Marcela Cunha <marcela.cunha@liferay.com>
2021-02-17 14:55:47 -0300

    LPS-128013 Separator must not contain the localizable property

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/separator/SeparatorDDMFormFieldTypeSettings.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/separator/SeparatorDDMFormFieldTypeSettings.java
@@ -51,9 +51,8 @@ import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 						@DDMFormLayoutColumn(
 							size = 12,
 							value = {
-								"name", "fieldReference", "style",
-								"localizable", "dataType", "type", "showLabel",
-								"repeatable"
+								"name", "fieldReference", "style", "dataType",
+								"type", "showLabel", "repeatable"
 							}
 						)
 					}
