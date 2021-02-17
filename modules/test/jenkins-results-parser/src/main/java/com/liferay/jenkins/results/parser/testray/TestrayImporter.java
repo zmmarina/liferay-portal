a94c26119b8b1f5c09767e37cf55c58b4ab84b25
Michael Hashimoto <michael.hashimoto@liferay.com>
2021-02-17 09:37:54 -0800

    LRCI-1988 Add ability to replace variables at runtime to get more dynamic testray values

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/TestrayImporter.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/TestrayImporter.java
@@ -15,9 +15,28 @@
 package com.liferay.jenkins.results.parser.testray;
 
 import com.liferay.jenkins.results.parser.Build;
+import com.liferay.jenkins.results.parser.GitWorkingDirectoryFactory;
+import com.liferay.jenkins.results.parser.JenkinsMaster;
 import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil;
 import com.liferay.jenkins.results.parser.Job;
+import com.liferay.jenkins.results.parser.PluginsTopLevelBuild;
+import com.liferay.jenkins.results.parser.PortalAppReleaseTopLevelBuild;
+import com.liferay.jenkins.results.parser.PortalBranchInformationBuild;
+import com.liferay.jenkins.results.parser.PortalFixpackRelease;
+import com.liferay.jenkins.results.parser.PortalFixpackReleaseBuild;
+import com.liferay.jenkins.results.parser.PortalGitWorkingDirectory;
+import com.liferay.jenkins.results.parser.PortalHotfixRelease;
+import com.liferay.jenkins.results.parser.PortalHotfixReleaseBuild;
+import com.liferay.jenkins.results.parser.PortalRelease;
+import com.liferay.jenkins.results.parser.PortalReleaseBuild;
+import com.liferay.jenkins.results.parser.PullRequest;
+import com.liferay.jenkins.results.parser.PullRequestBuild;
+import com.liferay.jenkins.results.parser.QAWebsitesTopLevelBuild;
 import com.liferay.jenkins.results.parser.TopLevelBuild;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Michael Hashimoto
@@ -47,6 +66,57 @@ public class TestrayImporter {
 		return _job;
 	}
 
+	public PortalFixpackRelease getPortalFixpackRelease() {
+		TopLevelBuild topLevelBuild = getTopLevelBuild();
+
+		if (!(topLevelBuild instanceof PortalFixpackReleaseBuild)) {
+			return null;
+		}
+
+		PortalFixpackReleaseBuild portalFixpackReleaseBuild =
+			(PortalFixpackReleaseBuild)topLevelBuild;
+
+		return portalFixpackReleaseBuild.getPortalFixpackRelease();
+	}
+
+	public PortalHotfixRelease getPortalHotfixRelease() {
+		TopLevelBuild topLevelBuild = getTopLevelBuild();
+
+		if (!(topLevelBuild instanceof PortalHotfixReleaseBuild)) {
+			return null;
+		}
+
+		PortalHotfixReleaseBuild portalHotfixReleaseBuild =
+			(PortalHotfixReleaseBuild)topLevelBuild;
+
+		return portalHotfixReleaseBuild.getPortalHotfixRelease();
+	}
+
+	public PortalRelease getPortalRelease() {
+		TopLevelBuild topLevelBuild = getTopLevelBuild();
+
+		if (!(topLevelBuild instanceof PortalReleaseBuild)) {
+			return null;
+		}
+
+		PortalReleaseBuild portalReleaseBuild =
+			(PortalReleaseBuild)topLevelBuild;
+
+		return portalReleaseBuild.getPortalRelease();
+	}
+
+	public PullRequest getPullRequest() {
+		TopLevelBuild topLevelBuild = getTopLevelBuild();
+
+		if (!(topLevelBuild instanceof PullRequestBuild)) {
+			return null;
+		}
+
+		PullRequestBuild pullRequestBuild = (PullRequestBuild)topLevelBuild;
+
+		return pullRequestBuild.getPullRequest();
+	}
+
 	public TestrayProject getTestrayProject() {
 		if (_testrayProject != null) {
 			return _testrayProject;
@@ -71,7 +141,7 @@ public class TestrayImporter {
 			!JenkinsResultsParserUtil.isNullOrEmpty(testrayProjectName)) {
 
 			testrayProject = _testrayServer.getTestrayProjectByName(
-				testrayProjectName);
+				_replaceEnvVars(testrayProjectName));
 		}
 
 		testrayProjectID = JenkinsResultsParserUtil.getProperty(
@@ -93,7 +163,7 @@ public class TestrayImporter {
 			!JenkinsResultsParserUtil.isNullOrEmpty(testrayProjectName)) {
 
 			testrayProject = _testrayServer.getTestrayProjectByName(
-				testrayProjectName);
+				_replaceEnvVars(testrayProjectName));
 		}
 
 		if (testrayProject != null) {
@@ -166,6 +236,258 @@ public class TestrayImporter {
 	public TopLevelBuild getTopLevelBuild() {
 		return _topLevelBuild;
 	}
+
+	private PortalGitWorkingDirectory _getPortalGitWorkingDirectory() {
+		return GitWorkingDirectoryFactory.newPortalGitWorkingDirectory(
+			_topLevelBuild.getBranchName());
+	}
+
+	private String _replaceEnvVars(String string) {
+		string = _replaceEnvVarsControllerBuild(string);
+		string = _replaceEnvVarsPluginsTopLevelBuild(string);
+		string = _replaceEnvVarsPortalAppReleaseTopLevelBuild(string);
+		string = _replaceEnvVarsPortalBranchInformationBuild(string);
+		string = _replaceEnvVarsPortalRelease(string);
+		string = _replaceEnvVarsPullRequestBuild(string);
+		string = _replaceEnvVarsQAWebsitesTopLevelBuild(string);
+		string = _replaceEnvVarsTopLevelBuild(string);
+
+		return string;
+	}
+
+	private String _replaceEnvVarsControllerBuild(String string) {
+		Build controllerBuild = _topLevelBuild.getControllerBuild();
+
+		if (controllerBuild == null) {
+			return string;
+		}
+
+		string = string.replace(
+			"$(jenkins.controller.build.url)", controllerBuild.getBuildURL());
+
+		string = string.replace(
+			"$(jenkins.controller.build.number)",
+			String.valueOf(controllerBuild.getBuildNumber()));
+
+		string = string.replace(
+			"$(jenkins.controller.build.start)",
+			JenkinsResultsParserUtil.toDateString(
+				new Date(controllerBuild.getStartTime()),
+				"yyyy-MM-dd[HH:mm:ss]", "America/Los_Angeles"));
+
+		string = string.replace(
+			"$(jenkins.controller.job.name)", controllerBuild.getJobName());
+
+		JenkinsMaster jenkinsMaster = controllerBuild.getJenkinsMaster();
+
+		return string.replace(
+			"$(jenkins.controller.master.hostname)", jenkinsMaster.getName());
+	}
+
+	private String _replaceEnvVarsPluginsTopLevelBuild(String string) {
+		if (!(_topLevelBuild instanceof PluginsTopLevelBuild)) {
+			return string;
+		}
+
+		PluginsTopLevelBuild pluginsTopLevelBuild =
+			(PluginsTopLevelBuild)_topLevelBuild;
+
+		String pluginName = pluginsTopLevelBuild.getPluginName();
+
+		if (!JenkinsResultsParserUtil.isNullOrEmpty(pluginName)) {
+			string = string.replace("$(plugin.name)", pluginName);
+		}
+
+		return string;
+	}
+
+	private String _replaceEnvVarsPortalAppReleaseTopLevelBuild(String string) {
+		if (!(_topLevelBuild instanceof PortalAppReleaseTopLevelBuild)) {
+			return string;
+		}
+
+		PortalAppReleaseTopLevelBuild portalAppReleaseTopLevelBuild =
+			(PortalAppReleaseTopLevelBuild)_topLevelBuild;
+
+		return string.replace(
+			"$(portal.app.name)",
+			portalAppReleaseTopLevelBuild.getPortalAppName());
+	}
+
+	private String _replaceEnvVarsPortalBranchInformationBuild(String string) {
+		Job.BuildProfile buildProfile = _topLevelBuild.getBuildProfile();
+
+		string = string.replace(
+			"$(portal.profile)", buildProfile.toDisplayString());
+
+		PortalGitWorkingDirectory portalGitWorkingDirectory =
+			_getPortalGitWorkingDirectory();
+
+		string = string.replace(
+			"$(portal.version)",
+			portalGitWorkingDirectory.getMajorPortalVersion());
+
+		if (!(_topLevelBuild instanceof PortalBranchInformationBuild)) {
+			return string;
+		}
+
+		PortalBranchInformationBuild portalBranchInformationBuild =
+			(PortalBranchInformationBuild)_topLevelBuild;
+
+		Build.BranchInformation portalBranchInformation =
+			portalBranchInformationBuild.getPortalBranchInformation();
+
+		if (portalBranchInformation == null) {
+			return string;
+		}
+
+		string = string.replace(
+			"$(portal.branch.name)",
+			portalBranchInformation.getUpstreamBranchName());
+
+		string = string.replace(
+			"$(portal.repository)",
+			portalBranchInformation.getRepositoryName());
+
+		return string.replace(
+			"$(portal.sha)", portalBranchInformation.getSenderBranchSHA());
+	}
+
+	private String _replaceEnvVarsPortalRelease(String string) {
+		PortalRelease portalRelease = getPortalRelease();
+
+		if (portalRelease != null) {
+			String tomcatURL = String.valueOf(portalRelease.getTomcatURL());
+
+			string = string.replace("$(portal.release.tomcat.url)", tomcatURL);
+
+			string = string.replace(
+				"$(portal.release.version)", portalRelease.getPortalVersion());
+
+			Matcher matcher = _releaseArtifactURLPattern.matcher(tomcatURL);
+
+			if (matcher.find()) {
+				string = string.replace(
+					"$(portal.release.tomcat.name)",
+					matcher.group("releaseFileName"));
+			}
+		}
+
+		PortalFixpackRelease portalFixpackRelease = getPortalFixpackRelease();
+
+		if (portalFixpackRelease != null) {
+			String portalFixpackURL = String.valueOf(
+				portalFixpackRelease.getPortalFixpackURL());
+
+			string = string.replace(
+				"$(portal.fixpack.release.url)", portalFixpackURL);
+
+			string = string.replace(
+				"$(portal.fixpack.release.version)",
+				portalFixpackRelease.getPortalFixpackVersion());
+
+			Matcher matcher = _releaseArtifactURLPattern.matcher(
+				portalFixpackURL);
+
+			if (matcher.find()) {
+				string = string.replace(
+					"$(portal.fixpack.release.name)",
+					matcher.group("releaseFileName"));
+			}
+		}
+
+		PortalHotfixRelease portalHotfixRelease = getPortalHotfixRelease();
+
+		if (portalHotfixRelease != null) {
+			String portalHotfixURL = String.valueOf(
+				portalHotfixRelease.getPortalHotfixReleaseURL());
+
+			string = string.replace(
+				"$(portal.hotfix.release.url)", portalHotfixURL);
+
+			string = string.replace(
+				"$(portal.hotfix.release.version)",
+				portalHotfixRelease.getPortalHotfixReleaseVersion());
+
+			Matcher matcher = _releaseArtifactURLPattern.matcher(
+				portalHotfixURL);
+
+			if (matcher.find()) {
+				string = string.replace(
+					"$(portal.hotfix.release.name)",
+					matcher.group("releaseFileName"));
+			}
+		}
+
+		return string;
+	}
+
+	private String _replaceEnvVarsPullRequestBuild(String string) {
+		PullRequest pullRequest = getPullRequest();
+
+		if (pullRequest == null) {
+			return string;
+		}
+
+		string = string.replace(
+			"$(pull.request.number)", pullRequest.getNumber());
+
+		string = string.replace(
+			"$(pull.request.url)", pullRequest.getHtmlURL());
+
+		string = string.replace(
+			"$(pull.request.receiver.username)",
+			pullRequest.getReceiverUsername());
+
+		return string.replace(
+			"$(pull.request.sender.username)", pullRequest.getSenderUsername());
+	}
+
+	private String _replaceEnvVarsQAWebsitesTopLevelBuild(String string) {
+		if (!(_topLevelBuild instanceof QAWebsitesTopLevelBuild)) {
+			return string;
+		}
+
+		QAWebsitesTopLevelBuild qaWebsitesTopLevelBuild =
+			(QAWebsitesTopLevelBuild)_topLevelBuild;
+
+		return string.replace(
+			"$(qa.websites.project.name)",
+			JenkinsResultsParserUtil.join(
+				",", qaWebsitesTopLevelBuild.getProjectNames()));
+	}
+
+	private String _replaceEnvVarsTopLevelBuild(String string) {
+		string = string.replace(
+			"$(ci.test.suite)", _topLevelBuild.getTestSuiteName());
+
+		string = string.replace(
+			"$(jenkins.build.number)",
+			String.valueOf(_topLevelBuild.getBuildNumber()));
+
+		string = string.replace(
+			"$(jenkins.build.start)",
+			JenkinsResultsParserUtil.toDateString(
+				new Date(_topLevelBuild.getStartTime()), "yyyy-MM-dd[HH:mm:ss]",
+				"America/Los_Angeles"));
+
+		string = string.replace(
+			"$(jenkins.build.url)", _topLevelBuild.getBuildURL());
+
+		string = string.replace(
+			"$(jenkins.job.name)", _topLevelBuild.getJobName());
+
+		JenkinsMaster jenkinsMaster = _topLevelBuild.getJenkinsMaster();
+
+		string = string.replace(
+			"$(jenkins.master.hostname)", jenkinsMaster.getName());
+
+		return string.replace(
+			"$(jenkins.report.url)", _topLevelBuild.getJenkinsReportURL());
+	}
+
+	private static final Pattern _releaseArtifactURLPattern = Pattern.compile(
+		"https?://.+/(?<releaseFileName>[^/]+)");
 
 	private Job _job;
 	private TestrayProject _testrayProject;
