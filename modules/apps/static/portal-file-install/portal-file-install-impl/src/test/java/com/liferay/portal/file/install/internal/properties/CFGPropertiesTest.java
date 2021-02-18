8a197ffa75eb85edb093112cef8f5bec34eab4a2
Matthew Tambara <matthew.tambara@liferay.com>
2021-02-17 16:49:54 -0800

    LPS-128031 Add test

---
+++ modules/apps/static/portal-file-install/portal-file-install-impl/src/test/java/com/liferay/portal/file/install/internal/properties/CFGPropertiesTest.java
@@ -0,0 +1,251 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.file.install.internal.properties;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
+import com.liferay.portal.kernel.test.rule.NewEnv;
+import com.liferay.portal.kernel.test.rule.NewEnvTestRule;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Matthew Tambara
+ */
+public class CFGPropertiesTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			CodeCoverageAssertor.INSTANCE, NewEnvTestRule.INSTANCE);
+
+	@Test
+	public void testException() {
+		try {
+			CFGProperties cfgProperties = new CFGProperties();
+
+			cfgProperties.load(null);
+
+			Assert.fail();
+		}
+		catch (IOException ioException) {
+			Assert.assertEquals("Reader is null", ioException.getMessage());
+		}
+	}
+
+	@Test
+	public void testKeySet() throws IOException {
+		CFGProperties cfgProperties = _createCFGProperties(
+			"testKey1=testValue1\ntestKey2=testValue2");
+
+		Set<String> keys = cfgProperties.keySet();
+
+		Assert.assertEquals(keys.toString(), 2, keys.size());
+
+		List<String> expectedKeys = new ArrayList<String>() {
+			{
+				add("testKey1");
+				add("testKey2");
+			}
+		};
+
+		for (String key : keys) {
+			Assert.assertNotEquals(null, cfgProperties.get(key));
+			Assert.assertTrue(expectedKeys.remove(key));
+		}
+
+		Assert.assertTrue(expectedKeys.toString(), expectedKeys.isEmpty());
+	}
+
+	@Test
+	public void testLoadAndSave() throws IOException {
+		String line = "testKey=testValue";
+
+		CFGProperties cfgProperties = _createCFGProperties(line);
+
+		Assert.assertEquals("testValue", cfgProperties.get("testKey"));
+
+		_assertSave(cfgProperties, line);
+	}
+
+	@Test
+	public void testLoadAndSaveMultiline() throws IOException {
+		String line = "testKey1=testValue11\\\n\ttestValue12";
+
+		CFGProperties cfgProperties = _createCFGProperties(line);
+
+		Assert.assertEquals(
+			"testValue11testValue12", cfgProperties.get("testKey1"));
+
+		_assertSave(cfgProperties, line);
+	}
+
+	@Test
+	public void testLoadAndSaveMultiple() throws IOException {
+		String line = "testKey1=testValue1\ntestKey2=testValue2";
+
+		CFGProperties cfgProperties = _createCFGProperties(line);
+
+		Assert.assertEquals("testValue1", cfgProperties.get("testKey1"));
+		Assert.assertEquals("testValue2", cfgProperties.get("testKey2"));
+
+		_assertSave(cfgProperties, line);
+	}
+
+	@Test
+	public void testLoadAndSaveMultipleWithComment() throws IOException {
+		String line =
+			"#comment1\ntestKey1=testValue1\n!comment2\ntestKey2=testValue2";
+
+		CFGProperties cfgProperties = _createCFGProperties(line);
+
+		Assert.assertEquals("testValue1", cfgProperties.get("testKey1"));
+		Assert.assertEquals("testValue2", cfgProperties.get("testKey2"));
+
+		_assertSave(cfgProperties, line);
+	}
+
+	@Test
+	public void testLoadAndSaveWithBlankLine() throws IOException {
+		String line = "testKey1=testValue1\n\ntestKey2=testValue2";
+
+		CFGProperties cfgProperties = _createCFGProperties(line);
+
+		Assert.assertEquals("testValue1", cfgProperties.get("testKey1"));
+		Assert.assertEquals("testValue2", cfgProperties.get("testKey2"));
+
+		_assertSave(cfgProperties, line);
+	}
+
+	@Test
+	public void testLoadAndSaveWithComment() throws IOException {
+		String line = "#comment\ntestKey=testValue";
+
+		CFGProperties cfgProperties = _createCFGProperties(line);
+
+		Assert.assertEquals("testValue", cfgProperties.get("testKey"));
+
+		_assertSave(cfgProperties, line);
+	}
+
+	@NewEnv(type = NewEnv.Type.JVM)
+	@NewEnv.Environment(variables = "LIFERAY_FOO_ENV_VALUE=ENV_TEST_VALUE")
+	@Test
+	public void testLoadEnvVariable() throws IOException {
+		String line = "testKey=${LIFERAY_FOO_ENV_VALUE}";
+
+		CFGProperties cfgProperties = _createCFGProperties(line);
+
+		Assert.assertEquals("ENV_TEST_VALUE", cfgProperties.get("testKey"));
+
+		_assertSave(cfgProperties, line);
+	}
+
+	@Test
+	public void testLoadPutAndSave() throws IOException {
+		CFGProperties cfgProperties = _createCFGProperties(
+			"testKey1=testValue1");
+
+		cfgProperties.put("testKey2", "testValue2");
+
+		Assert.assertEquals("testValue1", cfgProperties.get("testKey1"));
+		Assert.assertEquals("testValue2", cfgProperties.get("testKey2"));
+
+		_assertSave(cfgProperties, "testKey1=testValue1\ntestKey2=testValue2");
+	}
+
+	@Test
+	public void testLoadRemoveAndSave() throws IOException {
+		CFGProperties cfgProperties = _createCFGProperties(
+			"testKey1=testValue1\ntestKey2=testValue2");
+
+		cfgProperties.remove("testKey1");
+
+		Assert.assertNull(cfgProperties.get("testKey1"));
+		Assert.assertEquals("testValue2", cfgProperties.get("testKey2"));
+
+		_assertSave(cfgProperties, "testKey2=testValue2");
+	}
+
+	@Test
+	public void testPutAndSaveArray() throws IOException {
+		CFGProperties cfgProperties = new CFGProperties();
+
+		String[] array = {"testValue1", "testValue2"};
+
+		cfgProperties.put("testKey1", array);
+
+		String arrayString = "testValue1,testValue2";
+
+		Assert.assertEquals(arrayString, cfgProperties.get("testKey1"));
+
+		_assertSave(cfgProperties, "testKey1=" + arrayString);
+	}
+
+	@Test
+	public void testPutAndSaveCollection() throws IOException {
+		CFGProperties cfgProperties = new CFGProperties();
+
+		List<String> list = new ArrayList<String>() {
+			{
+				add("testValue1");
+				add("testValue2");
+			}
+		};
+
+		cfgProperties.put("testKey1", list);
+
+		String listString = "testValue1,testValue2";
+
+		Assert.assertEquals(listString, cfgProperties.get("testKey1"));
+
+		_assertSave(cfgProperties, "testKey1=" + listString);
+	}
+
+	private void _assertSave(CFGProperties cfgProperties, String expected)
+		throws IOException {
+
+		try (StringWriter stringWriter = new StringWriter()) {
+			cfgProperties.save(stringWriter);
+
+			Assert.assertEquals(expected, stringWriter.toString());
+		}
+	}
+
+	private CFGProperties _createCFGProperties(String string)
+		throws IOException {
+
+		CFGProperties cfgProperties = new CFGProperties();
+
+		try (StringReader stringReader = new StringReader(string)) {
+			cfgProperties.load(stringReader);
+		}
+
+		return cfgProperties;
+	}
+
+}
---
+++ modules/apps/static/portal-file-install/portal-file-install-test/src/testIntegration/java/com/liferay/portal/file/install/deploy/test/FileInstallCfgTest.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.file.install.deploy.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.util.PropsValues;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Dictionary;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.ManagedService;
+
+/**
+ * @author Matthew Tambara
+ */
+@RunWith(Arquillian.class)
+public class FileInstallCfgTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() {
+		Bundle bundle = FrameworkUtil.getBundle(FileInstallCfgTest.class);
+
+		_bundleContext = bundle.getBundleContext();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_deleteConfiguration();
+	}
+
+	@Test
+	public void testConfiguration() throws Exception {
+		String configurationPid = _CONFIGURATION_PID_PREFIX.concat(
+			".testConfiguration");
+
+		_configurationPath = Paths.get(
+			PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
+			configurationPid.concat(".cfg"));
+
+		String testKey = "testKey";
+		String testValue = "testValue";
+
+		_configuration = _createConfiguration(
+			configurationPid, testKey + StringPool.EQUAL + testValue);
+
+		Dictionary<String, Object> dictionary = _configuration.getProperties();
+
+		Assert.assertEquals("testValue", dictionary.get(testKey));
+	}
+
+	private Configuration _createConfiguration(
+			String configurationPid, String content)
+		throws Exception {
+
+		return _createConfiguration(
+			configurationPid, content, Charset.defaultCharset());
+	}
+
+	private Configuration _createConfiguration(
+			String configurationPid, String content, Charset charset)
+		throws Exception {
+
+		CountDownLatch countDownLatch = new CountDownLatch(2);
+
+		ServiceRegistration<ManagedService> serviceRegistration =
+			_bundleContext.registerService(
+				ManagedService.class, props -> countDownLatch.countDown(),
+				MapUtil.singletonDictionary(
+					Constants.SERVICE_PID, configurationPid));
+
+		try {
+			Files.write(_configurationPath, content.getBytes(charset));
+
+			countDownLatch.await();
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+
+		return _configurationAdmin.getConfiguration(
+			configurationPid, StringPool.QUESTION);
+	}
+
+	private void _deleteConfiguration() throws Exception {
+		if (_configurationPath != null) {
+			Files.deleteIfExists(_configurationPath);
+		}
+
+		if (_configuration != null) {
+			ConfigurationTestUtil.deleteConfiguration(_configuration);
+		}
+	}
+
+	private static final String _CONFIGURATION_PID_PREFIX =
+		FileInstallCfgTest.class.getName() + "Configuration";
+
+	@Inject
+	private static ConfigurationAdmin _configurationAdmin;
+
+	private BundleContext _bundleContext;
+	private Configuration _configuration;
+	private Path _configurationPath;
+
+}
