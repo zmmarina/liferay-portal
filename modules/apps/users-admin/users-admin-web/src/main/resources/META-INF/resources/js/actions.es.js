2f4ffc37a9ca8e0fe6e96c38b6288c888e3ea7a9
drewbrokke <drew.brokke@liferay.com>
2021-02-18 14:33:49 -0600

    LPS-121924 users-admin-web: copies select organization users JS to a common file

---
+++ modules/apps/users-admin/users-admin-web/src/main/resources/META-INF/resources/js/actions.es.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {
+	createActionURL,
+	createRenderURL,
+	openSelectionModal,
+} from 'frontend-js-web';
+
+export const ACTIONS = {
+	selectUsers({basePortletURL, organizationId, portletNamespace}) {
+		if (!organizationId) {
+			return;
+		}
+
+		const selectUsersURL = createRenderURL(basePortletURL, {
+			mvcPath: '/select_organization_users.jsp',
+			organizationId,
+			p_p_state: 'pop_up',
+		});
+
+		openSelectionModal({
+			buttonAddLabel: Liferay.Language.get('done'),
+			multiple: true,
+			onSelect: (data) => {
+				if (data) {
+					const assignmentsRedirectURL = createRenderURL(
+						basePortletURL,
+						{
+							mvcRenderCommandName: '/users_admin/view',
+							organizationId,
+							toolbarItem: 'view-all-organizations',
+							usersListView: 'tree',
+						}
+					);
+
+					const editAssignmentURL = createActionURL(basePortletURL, {
+						addUserIds: data.value,
+						assignmentsRedirect: assignmentsRedirectURL.toString(),
+						'javax.portlet.action':
+							'/users_admin/edit_organization_assignments',
+						organizationId,
+					});
+
+					const form = document.getElementById(
+						`${portletNamespace}fm`
+					);
+
+					if (!form) {
+						return;
+					}
+
+					submitForm(form, editAssignmentURL.toString());
+				}
+			},
+			selectEventName: `${portletNamespace}selectUsers`,
+			title: Liferay.Language.get('assign-users'),
+			url: selectUsersURL.toString(),
+		});
+	},
+};
