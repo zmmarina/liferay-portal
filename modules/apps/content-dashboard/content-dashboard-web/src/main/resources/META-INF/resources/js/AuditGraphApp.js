64ccaf095d72dc2a56133197f5abe26ebc620e2d
Brian Chan <brian.chan@liferay.com>
2021-02-19 13:22:28 -0300

    LPS-120676 fix my bad JS

--- modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/AuditGraphApp.js
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/AuditGraphApp.js
@@ -33,7 +33,7 @@ export default function ({context, props}) {
 					<ClayAlert displayType="info">
 						<span>
 							{Liferay.Language.get(
-								'press-escape-to-remove-the-bar's-category-filters'
+								'press-escape-to-remove-the-bar\'s-category-filters'
 							)}
 						</span>
 					</ClayAlert>
