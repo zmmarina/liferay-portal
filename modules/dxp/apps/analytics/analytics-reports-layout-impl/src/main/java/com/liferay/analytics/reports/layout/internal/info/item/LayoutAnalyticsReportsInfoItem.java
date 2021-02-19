59193bd4d7a406fbaac90742894b6e946c1e7838
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-02-19 12:24:52 +0100

    LPS-127220 New Layout Analytics Reports Info Item and Layout Analytics Reports Info Item Object Provider.

---
+++ modules/dxp/apps/analytics/analytics-reports-layout-impl/src/main/java/com/liferay/analytics/reports/layout/internal/info/item/LayoutAnalyticsReportsInfoItem.java
@@ -0,0 +1,214 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.analytics.reports.layout.internal.info.item;
+
+import com.liferay.analytics.reports.info.item.AnalyticsReportsInfoItem;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.info.type.WebImage;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Arques
+ */
+@Component(service = AnalyticsReportsInfoItem.class)
+public class LayoutAnalyticsReportsInfoItem
+	implements AnalyticsReportsInfoItem<Layout> {
+
+	@Override
+	public String getAuthorName(Layout layout) {
+		return _getUserOptional(
+			layout
+		).map(
+			User::getFullName
+		).orElse(
+			StringPool.BLANK
+		);
+	}
+
+	@Override
+	public long getAuthorUserId(Layout layout) {
+		return _getUserOptional(
+			layout
+		).map(
+			User::getUserId
+		).orElse(
+			0L
+		);
+	}
+
+	@Override
+	public WebImage getAuthorWebImage(Layout layout, Locale locale) {
+		ThemeDisplay themeDisplay = _getThemeDisplay();
+
+		if (themeDisplay == null) {
+			return new WebImage(StringPool.BLANK);
+		}
+
+		Optional<User> userOptional = _getUserOptional(layout);
+
+		return userOptional.map(
+			user -> {
+				try {
+					return new WebImage(user.getPortraitURL(themeDisplay));
+				}
+				catch (PortalException portalException) {
+					_log.error(portalException, portalException);
+
+					return new WebImage(StringPool.BLANK);
+				}
+			}
+		).orElse(
+			new WebImage(StringPool.BLANK)
+		);
+	}
+
+	@Override
+	public List<Locale> getAvailableLocales(Layout layout) {
+		return Stream.of(
+			layout.getAvailableLanguageIds()
+		).map(
+			LocaleUtil::fromLanguageId
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	@Override
+	public Locale getDefaultLocale(Layout layout) {
+		return LocaleUtil.fromLanguageId(layout.getDefaultLanguageId());
+	}
+
+	@Override
+	public Date getPublishDate(Layout layout) {
+		return layout.getPublishDate();
+	}
+
+	@Override
+	public String getTitle(Layout layout, Locale locale) {
+		return layout.getTitle(locale);
+	}
+
+	@Override
+	public boolean isShow(Layout layout) {
+		if (!layout.isTypeContent()) {
+			return false;
+		}
+
+		if (_isEmbeddedPersonalApplicationLayout(layout)) {
+			return false;
+		}
+
+		try {
+			if (!_hasEditPermission(
+					layout, PermissionThreadLocal.getPermissionChecker())) {
+
+				return false;
+			}
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException, portalException);
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private ThemeDisplay _getThemeDisplay() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			return serviceContext.getThemeDisplay();
+		}
+
+		return null;
+	}
+
+	private Optional<User> _getUserOptional(Layout layout) {
+		return Optional.ofNullable(
+			_userLocalService.fetchUser(layout.getUserId()));
+	}
+
+	private boolean _hasEditPermission(
+			Layout layout, PermissionChecker permissionChecker)
+		throws PortalException {
+
+		if (!LayoutPermissionUtil.contains(
+				permissionChecker, layout, ActionKeys.UPDATE)) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean _isEmbeddedPersonalApplicationLayout(Layout layout) {
+		if (layout.isTypeControlPanel()) {
+			return false;
+		}
+
+		String layoutFriendlyURL = layout.getFriendlyURL();
+
+		if (layout.isSystem() &&
+			layoutFriendlyURL.equals(
+				PropsUtil.get(PropsKeys.CONTROL_PANEL_LAYOUT_FRIENDLY_URL))) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutAnalyticsReportsInfoItem.class);
+
+	@Reference
+	private InfoItemServiceTracker _infoItemServiceTracker;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
---
+++ modules/dxp/apps/analytics/analytics-reports-layout-impl/src/main/java/com/liferay/analytics/reports/layout/internal/info/item/provider/LayoutAnalyticsReportsInfoItemObjectProvider.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.analytics.reports.layout.internal.info.item.provider;
+
+import com.liferay.analytics.reports.info.item.provider.AnalyticsReportsInfoItemObjectProvider;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.InfoItemReference;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = AnalyticsReportsInfoItemObjectProvider.class)
+public class LayoutAnalyticsReportsInfoItemObjectProvider
+	implements AnalyticsReportsInfoItemObjectProvider<Layout> {
+
+	@Override
+	public Layout getAnalyticsReportsInfoItemObject(
+		InfoItemReference infoItemReference) {
+
+		ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+			(ClassPKInfoItemIdentifier)
+				infoItemReference.getInfoItemIdentifier();
+
+		return _layoutLocalService.fetchLayout(
+			classPKInfoItemIdentifier.getClassPK());
+	}
+
+	@Override
+	public String getClassName() {
+		return Layout.class.getName();
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+}
