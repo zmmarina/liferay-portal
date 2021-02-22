6087eabf6dfe456b331e4f2c9befa4dae578da6d
drewbrokke <drew.brokke@liferay.com>
2021-02-22 13:19:00 -0600

    LPS-125700 commerce-account-service: CommerceAccountGroupImpl: adds static transform methods

--- modules/apps/commerce/commerce-account-service/src/main/java/com/liferay/commerce/account/model/impl/CommerceAccountGroupImpl.java
+++ modules/apps/commerce/commerce-account-service/src/main/java/com/liferay/commerce/account/model/impl/CommerceAccountGroupImpl.java
@@ -14,10 +14,98 @@
 
 package com.liferay.commerce.account.model.impl;
 
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.model.AccountGroup;
+import com.liferay.commerce.account.constants.CommerceAccountConstants;
+import com.liferay.commerce.account.model.CommerceAccountGroup;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+
 /**
  * @author Marco Leo
  */
 public class CommerceAccountGroupImpl extends CommerceAccountGroupBaseImpl {
+
+	public static CommerceAccountGroup fromAccountGroup(
+		AccountGroup accountGroup) {
+
+		if (accountGroup == null) {
+			return null;
+		}
+
+		CommerceAccountGroup commerceAccountGroup =
+			new CommerceAccountGroupImpl();
+
+		Map<String, BiConsumer<CommerceAccountGroup, Object>>
+			attributeSetterBiConsumers =
+				commerceAccountGroup.getAttributeSetterBiConsumers();
+
+		Map<String, Object> modelAttributes = accountGroup.getModelAttributes();
+
+		for (Map.Entry<String, Object> entry : modelAttributes.entrySet()) {
+			String key = entry.getKey();
+
+			BiConsumer<CommerceAccountGroup, Object>
+				commerceAccountGroupObjectBiConsumer =
+					attributeSetterBiConsumers.get(key);
+
+			if (commerceAccountGroupObjectBiConsumer != null) {
+				Object value = entry.getValue();
+
+				if (key.equals("type")) {
+					value = toCommerceAccountGroupType((String)value);
+				}
+
+				commerceAccountGroupObjectBiConsumer.accept(
+					commerceAccountGroup, value);
+			}
+		}
+
+		commerceAccountGroup.setCommerceAccountGroupId(
+			accountGroup.getAccountGroupId());
+		commerceAccountGroup.setType(
+			toCommerceAccountGroupType(accountGroup.getType()));
+		commerceAccountGroup.setSystem(accountGroup.isDefaultAccountGroup());
+
+		return commerceAccountGroup;
+	}
+
+	public static String toAccountGroupType(Integer type) {
+		if (type != null) {
+			if (type == CommerceAccountConstants.ACCOUNT_GROUP_TYPE_GUEST) {
+				return AccountConstants.ACCOUNT_GROUP_TYPE_GUEST;
+			}
+			else if (type ==
+						CommerceAccountConstants.ACCOUNT_GROUP_TYPE_DYNAMIC) {
+
+				return AccountConstants.ACCOUNT_GROUP_TYPE_DYNAMIC;
+			}
+			else if (type ==
+						CommerceAccountConstants.ACCOUNT_GROUP_TYPE_STATIC) {
+
+				return AccountConstants.ACCOUNT_GROUP_TYPE_STATIC;
+			}
+		}
+
+		return AccountConstants.ACCOUNT_GROUP_TYPE_STATIC;
+	}
+
+	public static int toCommerceAccountGroupType(String type) {
+		if (type != null) {
+			if (type.equals(AccountConstants.ACCOUNT_GROUP_TYPE_GUEST)) {
+				return CommerceAccountConstants.ACCOUNT_GROUP_TYPE_GUEST;
+			}
+			else if (type.equals(AccountConstants.ACCOUNT_GROUP_TYPE_DYNAMIC)) {
+				return CommerceAccountConstants.ACCOUNT_GROUP_TYPE_DYNAMIC;
+			}
+			else if (type.equals(AccountConstants.ACCOUNT_GROUP_TYPE_STATIC)) {
+				return CommerceAccountConstants.ACCOUNT_GROUP_TYPE_STATIC;
+			}
+		}
+
+		return CommerceAccountConstants.ACCOUNT_GROUP_TYPE_STATIC;
+	}
 
 	public CommerceAccountGroupImpl() {
 	}
