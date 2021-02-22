80b156ea9f1770ee9e3334ba6cef8b13edd00744
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-02-22 16:54:27 +0100

    LPS-123585 Remove bundler 3 from frontend-js-recharts

--- modules/apps/frontend-js/frontend-js-recharts/npmscripts.config.js
+++ modules/apps/frontend-js/frontend-js-recharts/npmscripts.config.js
@@ -13,20 +13,11 @@
  */
 
 module.exports = {
-	'create-jar': false,
-	exports: {
-		recharts: 'recharts',
-		recharts_lib_index: 'recharts/lib/index.js',
+	federation: {
+		bridges: [
+			'recharts',
+		],
+		runLegacyBuild: false,
 	},
-	imports: {
-		'frontend-js-node-shims': {
-			events: '^1.0.0',
-		},
-		'frontend-js-react-web': {
-			react: '^16.0.0',
-			'react-dom': '^16.0.0',
-		},
-	},
-	output: 'build/node/packageRunBuild/resources',
-	workdir: 'build/node/bundler',
+	preset: '@liferay/npm-scripts/src/presets/standard',
 };
