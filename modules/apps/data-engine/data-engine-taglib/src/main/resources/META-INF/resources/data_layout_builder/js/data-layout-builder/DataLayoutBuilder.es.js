c188643938cf5feb7926fcb68e27f246faabbe41
Milton Castro <milton.castro@liferay.com>
2021-02-24 09:19:07 -0300

    LPS-127934 Remove unused code

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/data-layout-builder/DataLayoutBuilder.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/data-layout-builder/DataLayoutBuilder.es.js
@@ -24,7 +24,6 @@ import {
 	getDataDefinitionField,
 	getFieldSetDDMForm,
 } from '../utils/dataConverter.es';
-import generateDataDefinitionFieldName from '../utils/generateDataDefinitionFieldName.es';
 import EventEmitter from './EventEmitter.es';
 import saveDefinitionAndLayout from './saveDefinitionAndLayout.es';
 
@@ -127,26 +126,6 @@ class DataLayoutBuilder extends React.Component {
 
 	emit(event, payload, error = false) {
 		this.eventEmitter.emit(event, payload, error);
-	}
-
-	fieldNameGenerator(dataDefinitionFields) {
-		const layoutProvider = this.getLayoutProvider();
-
-		layoutProvider.props = {
-			...layoutProvider.props,
-			fieldNameGenerator: (
-				desiredFieldName,
-				currentFieldName,
-				blacklist
-			) =>
-				generateDataDefinitionFieldName(
-					dataDefinitionFields,
-					desiredFieldName,
-					currentFieldName,
-					blacklist
-				),
-			shouldAutoGenerateName: () => false,
-		};
 	}
 
 	getDataDefinitionAndDataLayout(pages, rules) {
