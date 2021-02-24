9c17621c0ced30deaa624c7f47ba15f84384817b
Brian Chan <brian.chan@liferay.com>
2021-02-24 09:31:16 -0300

    LPS-128214 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v4_1_0/UpgradeDDMStructure.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v4_1_0/UpgradeDDMStructure.java
@@ -78,8 +78,8 @@ public class UpgradeDDMStructure extends UpgradeProcess {
 			PreparedStatement ps2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMStructure set definition = ?, " +
-						"parentStructureId = 0 where structureId = ?")) {
+					"update DDMStructure set parentStructureId = 0, " +
+						"definition = ? where structureId = ?")) {
 
 			ps1.setLong(
 				1,
@@ -190,8 +190,8 @@ public class UpgradeDDMStructure extends UpgradeProcess {
 			PreparedStatement ps2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMStructureVersion set definition = ?, " +
-						"parentStructureId = 0 where structureVersionId = ?")) {
+					"update DDMStructureVersion set parentStructureId = 0, " +
+						"definition = ? where structureVersionId = ?")) {
 
 			ps1.setLong(
 				1,
