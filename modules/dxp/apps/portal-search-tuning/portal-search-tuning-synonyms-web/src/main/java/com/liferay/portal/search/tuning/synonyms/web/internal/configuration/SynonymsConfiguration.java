c679eb6769c8eaa904dabc0d1fb4d5e7fe40529c
Tibor Lipusz <tibor.lipusz@liferay.com>
2021-02-25 19:23:33 +0100

    LPS-128388 portal-search-tuning-synonyms-web: Make filter names configurable
    https://issues.liferay.com/browse/LPS-128388
    
    Signed-off-by: Tibor Lipusz <tibor.lipusz@liferay.com>

---
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/configuration/SynonymsConfiguration.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.search.tuning.synonyms.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Tibor Lipusz
+ */
+@ExtendedObjectClassDefinition(category = "search")
+@Meta.OCD(
+	id = "com.liferay.portal.search.tuning.synonyms.web.internal.configuration.SynonymsConfiguration",
+	localization = "content/Language", name = "synonyms"
+)
+public interface SynonymsConfiguration {
+
+	@Meta.AD(
+		deflt = "liferay_filter_synonym_en|liferay_filter_synonym_es",
+		description = "synonym-filter-names-help",
+		name = "synonym-filter-names", required = false
+	)
+	public String[] filterNames();
+
+}
--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/filter/name/SynonymSetFilterNameHolderImpl.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/filter/name/SynonymSetFilterNameHolderImpl.java
@@ -14,22 +14,41 @@
 
 package com.liferay.portal.search.tuning.synonyms.web.internal.filter.name;
 
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.search.tuning.synonyms.web.internal.configuration.SynonymsConfiguration;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Adam Brandizzi
+ * @author Tibor Lipusz
  */
-@Component(service = SynonymSetFilterNameHolder.class)
+@Component(
+	configurationPid = "com.liferay.portal.search.tuning.synonyms.web.internal.configuration.SynonymsConfiguration",
+	service = SynonymSetFilterNameHolder.class
+)
 public class SynonymSetFilterNameHolderImpl
 	implements SynonymSetFilterNameHolder {
 
 	@Override
 	public String[] getFilterNames() {
-		return _FILTER_NAMES;
+		return _filterNames;
 	}
 
-	private static final String[] _FILTER_NAMES = {
-		"liferay_filter_synonym_en", "liferay_filter_synonym_es"
-	};
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		SynonymsConfiguration synonymsConfiguration =
+			ConfigurableUtil.createConfigurable(
+				SynonymsConfiguration.class, properties);
+
+		_filterNames = synonymsConfiguration.filterNames();
+	}
+
+	private volatile String[] _filterNames;
 
 }
