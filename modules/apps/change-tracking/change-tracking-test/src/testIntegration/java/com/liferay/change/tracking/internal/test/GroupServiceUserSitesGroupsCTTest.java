ab2244120990899d5130731ddb42e44019abf6e7
Samuel Tran <samuel.tran@liferay.com>
2021-02-25 18:28:20 -0800

    LPS-120859 Add test for user sites created within a publication

---
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/GroupServiceUserSitesGroupsCTTest.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.internal.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.petra.lang.SafeClosable;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Samuel Trong Tran
+ */
+@RunWith(Arquillian.class)
+public class GroupServiceUserSitesGroupsCTTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_ctCollection = _ctCollectionLocalService.addCTCollection(
+			TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+			GroupServiceUserSitesGroupsCTTest.class.getName(), null);
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testGetUserSitesGroups() throws Exception {
+		Group group = null;
+
+		try (SafeClosable safeClosable =
+				CTCollectionThreadLocal.setCTCollectionId(
+					_ctCollection.getCtCollectionId())) {
+
+			group = GroupTestUtil.addGroup();
+
+			List<Group> groups = _groupService.getUserSitesGroups(
+				TestPropsValues.getUserId(), null, QueryUtil.ALL_POS);
+
+			Assert.assertTrue(groups.toString(), groups.contains(_group));
+			Assert.assertTrue(groups.toString(), groups.contains(group));
+		}
+
+		List<Group> groups = _groupService.getUserSitesGroups(
+			TestPropsValues.getUserId(), null, QueryUtil.ALL_POS);
+
+		Assert.assertTrue(groups.toString(), groups.contains(_group));
+		Assert.assertFalse(groups.toString(), groups.contains(group));
+	}
+
+	@Inject
+	private static CTCollectionLocalService _ctCollectionLocalService;
+
+	@Inject
+	private static GroupService _groupService;
+
+	@DeleteAfterTestRun
+	private CTCollection _ctCollection;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
