ce6e96b6c33a0d143815b5060661b75f5c9019fc
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-03-01 22:35:47 +0100

    LPS-126044 New API LayoutDisplayPageObjectProviderAnalyticsReportsInfoItem that extends AnalyticsReportsInfoItem.

---
+++ modules/dxp/apps/analytics/analytics-reports-layout-display-page-api/src/main/java/com/liferay/analytics/reports/layout/display/page/info/item/LayoutDisplayPageObjectProviderAnalyticsReportsInfoItem.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.analytics.reports.layout.display.page.info.item;
+
+import com.liferay.analytics.reports.info.item.AnalyticsReportsInfoItem;
+import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
+
+/**
+ * @author Cristina González
+ */
+public interface LayoutDisplayPageObjectProviderAnalyticsReportsInfoItem
+	extends AnalyticsReportsInfoItem<LayoutDisplayPageObjectProvider> {
+}
