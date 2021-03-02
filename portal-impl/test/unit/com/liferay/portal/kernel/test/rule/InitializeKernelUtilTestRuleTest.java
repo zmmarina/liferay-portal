b7e90d41ad067c0565f7c66ef666fb3663772d23
Matthew Tambara <matthew.tambara@liferay.com>
2021-03-01 16:25:10 -0800

    LPS-127030 Add test

---
+++ portal-impl/test/unit/com/liferay/portal/kernel/test/rule/InitializeKernelUtilTestRuleTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.test.rule;
+
+import com.liferay.portal.kernel.util.PropsUtil;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Matthew Tambara
+ */
+@InitializeKernelUtil(properties = "testClassKey=testClassValue")
+public class InitializeKernelUtilTestRuleTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testProps() {
+		Assert.assertNotNull(PropsUtil.getProps());
+
+		Assert.assertEquals("testClassValue", PropsUtil.get("testClassKey"));
+	}
+
+	@InitializeKernelUtil(properties = "testMethodKey=testMethodValue")
+	@Test
+	public void testPropsMethodAnnotation() {
+		Assert.assertNotNull(PropsUtil.getProps());
+
+		Assert.assertEquals("testClassValue", PropsUtil.get("testClassKey"));
+
+		Assert.assertEquals("testMethodValue", PropsUtil.get("testMethodKey"));
+	}
+
+}
