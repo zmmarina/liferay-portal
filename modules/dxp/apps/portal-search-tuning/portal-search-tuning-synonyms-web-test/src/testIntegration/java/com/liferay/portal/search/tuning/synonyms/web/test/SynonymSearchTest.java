fb8b830fc403dcbaa81ac186c8fa4f3ac7c9bf04
Tibor Lipusz <tibor.lipusz@liferay.com>
2021-03-03 11:46:30 +0100

    LPS-128388 portal-search-tuning-synonyms-web-test: Add test coverage for synonym search
    https://issues.liferay.com/browse/LPS-128388
    
    Signed-off-by: Tibor Lipusz <tibor.lipusz@liferay.com>

---
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web-test/src/testIntegration/java/com/liferay/portal/search/tuning/synonyms/web/test/SynonymSearchTest.java
@@ -0,0 +1,294 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.search.tuning.synonyms.web.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.portal.search.test.util.DocumentsAssert;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Tibor Lipusz
+ */
+@RunWith(Arquillian.class)
+public class SynonymSearchTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		try (ConfigurationTemporarySwapper
+				elasticSearchConfigurationTemporarySwapper =
+					new ConfigurationTemporarySwapper(
+						"com.liferay.portal.search.elasticsearch7." +
+							"configuration.ElasticsearchConfiguration",
+						new HashMapDictionary<String, Object>() {
+							{
+								put(
+									"additionalIndexConfigurations",
+									loadAdditionalIndexConfigurations());
+								put(
+									"overrideTypeMappings",
+									loadOverrideTypeMappings());
+							}
+				});
+
+			ConfigurationTemporarySwapper synonymConfigurationTemporarySwapper =
+			new ConfigurationTemporarySwapper(
+				"com.liferay.portal.search.tuning.synonyms.web.internal." +
+					"configuration.SynonymsConfiguration",
+				new HashMapDictionary<String, Object>() {
+					{
+						put(
+							"filterNames",
+							new String[] {
+								"liferay_filter_synonym_en",
+								"liferay_filter_synonym_es",
+								"custom-synonym-filter-fr"
+							});
+					}
+					})) {
+
+			_company = CompanyTestUtil.addCompany();
+
+			addSynonymSet("dxp,portal");
+			addSynonymSet("efectivo,productivo");
+			addSynonymSet("hatékony,produktív");
+			addSynonymSet("maison,logement");
+		}
+
+		addJournalArticles();
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		_companyLocalService.deleteCompany(_company);
+	}
+
+	@Test
+	public void testSearchOnLocalesWithDefaultSynonymFilters()
+		throws Exception {
+
+		doAssertSearch("efectivo", Field.TITLE, LocaleUtil.SPAIN, 2);
+		doAssertSearch("dxp", Field.TITLE, LocaleUtil.US, 2);
+	}
+
+	@Test
+	public void testSearchOnLocaleWithCustomSynonymFilter() throws Exception {
+		doAssertSearch("maison", Field.TITLE, LocaleUtil.FRANCE, 2);
+	}
+
+	@Test
+	public void testSearchOnLocaleWithoutSynonymFilter() throws Exception {
+		doAssertSearch("hatékony", Field.TITLE, LocaleUtil.HUNGARY, 1);
+	}
+
+	protected static void addJournalArticle(Map<Locale, String> localeStringMap)
+		throws Exception {
+
+		JournalTestUtil.addArticle(
+			_group.getGroupId(), 0,
+			PortalUtil.getClassNameId(JournalArticle.class), localeStringMap,
+			null, localeStringMap, LocaleUtil.getSiteDefault(), false, true,
+			_serviceContext);
+	}
+
+	protected static void addJournalArticles() throws Exception {
+		_user = UserTestUtil.getAdminUser(_company.getCompanyId());
+
+		_group = GroupTestUtil.addGroup(
+			_company.getCompanyId(), _user.getUserId(),
+			GroupConstants.DEFAULT_PARENT_GROUP_ID);
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId(), _user.getUserId());
+
+		addJournalArticle(
+			HashMapBuilder.put(
+				LocaleUtil.FRANCE, "maison"
+			).put(
+				LocaleUtil.HUNGARY, "hatékony"
+			).put(
+				LocaleUtil.SPAIN, "efectivo"
+			).put(
+				LocaleUtil.US, "dxp"
+			).build());
+		addJournalArticle(
+			HashMapBuilder.put(
+				LocaleUtil.FRANCE, "logement"
+			).put(
+				LocaleUtil.HUNGARY, "produktív"
+			).put(
+				LocaleUtil.SPAIN, "productivo"
+			).put(
+				LocaleUtil.US, "portal"
+			).build());
+	}
+
+	protected static void addSynonymSet(String synonymSet) {
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		mockLiferayPortletActionRequest.setAttribute(
+			WebKeys.COMPANY_ID, _company.getCompanyId());
+		mockLiferayPortletActionRequest.addParameter("synonymSet", synonymSet);
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "updateSynonymSet",
+			new Class<?>[] {ActionRequest.class},
+			mockLiferayPortletActionRequest);
+	}
+
+	protected static String getResourceAsString(
+		Class<?> clazz, String resourceName) {
+
+		try (InputStream inputStream = clazz.getResourceAsStream(
+				resourceName)) {
+
+			return StringUtil.read(inputStream);
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(
+				"Unable to load resource: " + resourceName, ioException);
+		}
+	}
+
+	protected static String loadAdditionalIndexConfigurations() {
+		try {
+			return getResourceAsString(
+				SynonymSearchTest.class,
+				SynonymSearchTest.class.getSimpleName() +
+					"-additionalIndexConfigurations.json");
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	protected static String loadOverrideTypeMappings() {
+		try {
+			return getResourceAsString(
+				SynonymSearchTest.class,
+				SynonymSearchTest.class.getSimpleName() +
+					"-overrideTypeMappings.json");
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	protected void doAssertSearch(
+		String keyword, String fieldName, Locale locale, int expectedCount) {
+
+		String localizedFieldName = Field.getLocalizedName(locale, fieldName);
+
+		SearchRequestBuilder searchRequestBuilder =
+			_searchRequestBuilderFactory.builder(
+			).companyId(
+				_company.getCompanyId()
+			).entryClassNames(
+				JournalArticle.class.getName()
+			).groupIds(
+				_group.getGroupId()
+			).queryString(
+				keyword
+			);
+
+		SearchResponse searchResponse = _searcher.search(
+			searchRequestBuilder.build());
+
+		List<Document> documents = searchResponse.getDocuments71();
+
+		DocumentsAssert.assertCount(
+			searchResponse.getRequestString(),
+			documents.toArray(new Document[0]), localizedFieldName,
+			expectedCount);
+	}
+
+	private static Company _company;
+
+	@Inject
+	private static CompanyLocalService _companyLocalService;
+
+	private static Group _group;
+
+	@Inject(
+		filter = "mvc.command.name=/synonyms/edit_synonym_sets",
+		type = MVCActionCommand.class
+	)
+	private static MVCActionCommand _mvcActionCommand;
+
+	private static ServiceContext _serviceContext;
+	private static User _user;
+
+	@Inject
+	private Queries _queries;
+
+	@Inject
+	private Searcher _searcher;
+
+	@Inject
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+}
