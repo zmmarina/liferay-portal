0b00cdb4bb7eb19566cddc47f7c3c7d6b7fe37ed
Alicia Garcia <alicia.garcia@liferay.com>
2021-03-03 19:08:02 +0100

    LPS-128588 add test for the new method to provide the default title depending on the extension

--- modules/apps/document-library-opener/document-library-opener-test/src/testIntegration/java/com/liferay/document/library/opener/upload/test/UniqueFileEntryTitleProviderTest.java
+++ modules/apps/document-library-opener/document-library-opener-test/src/testIntegration/java/com/liferay/document/library/opener/upload/test/UniqueFileEntryTitleProviderTest.java
@@ -17,6 +17,7 @@ package com.liferay.document.library.opener.upload.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.document.library.opener.constants.DLOpenerMimeTypes;
 import com.liferay.document.library.opener.upload.UniqueFileEntryTitleProvider;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -92,6 +93,77 @@ public class UniqueFileEntryTitleProviderTest {
 			_uniqueFileEntryTitleProvider.provide(
 				_group.getGroupId(), _folder.getFolderId(), StringPool.BLANK,
 				"someTitle"));
+	}
+
+	@Test
+	public void testProvideWithExtensionAndLocale() throws PortalException {
+		Assert.assertEquals(
+			"Untitled",
+			_uniqueFileEntryTitleProvider.provide(
+				_group.getGroupId(), _folder.getFolderId(),
+				DLOpenerMimeTypes.getMimeTypeExtension(
+					DLOpenerMimeTypes.APPLICATION_VND_DOCX),
+				LocaleUtil.US));
+	}
+
+	@Test
+	public void testProvideWithExtensionAndLocaleWithExistingFileName()
+		throws PortalException {
+
+		String mimeTypeExtension = DLOpenerMimeTypes.getMimeTypeExtension(
+			DLOpenerMimeTypes.APPLICATION_VND_DOCX);
+
+		_dlAppLocalService.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			_folder.getFolderId(), "Untitled" + mimeTypeExtension,
+			ContentTypes.IMAGE_JPEG, StringUtil.randomString(),
+			StringUtil.randomString(), StringPool.BLANK, "test".getBytes(),
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		Assert.assertEquals(
+			"Untitled (1)",
+			_uniqueFileEntryTitleProvider.provide(
+				_group.getGroupId(), _folder.getFolderId(), mimeTypeExtension,
+				LocaleUtil.US));
+	}
+
+	@Test
+	public void testProvideWithExtensionAndLocaleWithExistingTitle()
+		throws PortalException {
+
+		_dlAppLocalService.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			_folder.getFolderId(), null, ContentTypes.IMAGE_JPEG, "Untitled",
+			StringUtil.randomString(), StringPool.BLANK, "test".getBytes(),
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		Assert.assertEquals(
+			"Untitled (1)",
+			_uniqueFileEntryTitleProvider.provide(
+				_group.getGroupId(), _folder.getFolderId(),
+				DLOpenerMimeTypes.getMimeTypeExtension(
+					DLOpenerMimeTypes.APPLICATION_VND_DOCX),
+				LocaleUtil.US));
+	}
+
+	@Test
+	public void testProvideWithExtensionAndLocaleWithExistingTitleAndDifferentExtension()
+		throws PortalException {
+
+		_dlAppLocalService.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			_folder.getFolderId(), "Untitled.ppt", ContentTypes.IMAGE_JPEG,
+			"Untitled", StringUtil.randomString(), StringPool.BLANK,
+			"test".getBytes(),
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		Assert.assertEquals(
+			"Untitled (1)",
+			_uniqueFileEntryTitleProvider.provide(
+				_group.getGroupId(), _folder.getFolderId(),
+				DLOpenerMimeTypes.getMimeTypeExtension(
+					DLOpenerMimeTypes.APPLICATION_VND_DOCX),
+				LocaleUtil.US));
 	}
 
 	@Test
