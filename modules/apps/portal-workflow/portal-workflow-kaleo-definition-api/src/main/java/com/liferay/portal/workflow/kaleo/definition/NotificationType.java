1fe73b79c14bfc6fb9fb0a85f86f153e7dcdacff
Rafael Praxedes <rafael.praxedes@liferay.com>
2021-03-04 12:14:17 -0300

    LPS-122558 Deprecate the IM and the Private Message notification type

--- modules/apps/portal-workflow/portal-workflow-kaleo-definition-api/src/main/java/com/liferay/portal/workflow/kaleo/definition/NotificationType.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-definition-api/src/main/java/com/liferay/portal/workflow/kaleo/definition/NotificationType.java
@@ -23,7 +23,17 @@ import java.util.Objects;
  */
 public enum NotificationType {
 
-	EMAIL("email"), IM("im"), PRIVATE_MESSAGE("private-message"),
+	EMAIL("email"),
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), with no direct replacement
+	 */
+	@Deprecated
+	IM("im"),
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), with no direct replacement
+	 */
+	@Deprecated
+	PRIVATE_MESSAGE("private-message"),
 	PUSH_NOTIFICATION("push-notification"),
 	USER_NOTIFICATION("user-notification");
 
