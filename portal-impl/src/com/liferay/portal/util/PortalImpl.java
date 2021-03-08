ce2254c64e18ee48362bdab99d7c7fbd9fa0bbeb
Brian Chan <brian.chan@liferay.com>
2021-03-08 13:23:10 -0300

    LPS-127840 rename

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1437,7 +1437,7 @@ public class PortalImpl implements Portal {
 
 		if (forceLayoutFriendlyURL ||
 			((!layout.isFirstParent() || Validator.isNotNull(parametersURL)) &&
-			 _needLayoutFriendlyURL(
+			 _requiresLayoutFriendlyURL(
 				 siteGroup.getFriendlyURL(),
 				 themeDisplay.getLayoutFriendlyURL(layout),
 				 groupFriendlyURL)) ||
@@ -6667,8 +6667,8 @@ public class PortalImpl implements Portal {
 		}
 
 		if (exception instanceof NoSuchImageException) {
-			if (_logWebServerServlet.isWarnEnabled()) {
-				_logWebServerServlet.warn(exception, exception);
+			if (_webServerServletLog.isWarnEnabled()) {
+				_webServerServletLog.warn(exception, exception);
 			}
 		}
 		else if (exception instanceof PortalException) {
@@ -8755,7 +8755,7 @@ public class PortalImpl implements Portal {
 		return group;
 	}
 
-	private boolean _needLayoutFriendlyURL(
+	private boolean _requiresLayoutFriendlyURL(
 		String siteGroupFriendlyURL, String layoutFriendlyURL,
 		String groupFriendlyURL) {
 
@@ -8802,7 +8802,7 @@ public class PortalImpl implements Portal {
 		new ConcurrentHashMap<>();
 	private static final Map<Long, String> _cdnHostHttpsMap =
 		new ConcurrentHashMap<>();
-	private static final Log _logWebServerServlet = LogFactoryUtil.getLog(
+	private static final Log _webServerServletLog = LogFactoryUtil.getLog(
 		WebServerServlet.class);
 	private static final MethodHandler _resetCDNHostsMethodHandler =
 		new MethodHandler(new MethodKey(PortalUtil.class, "resetCDNHosts"));
