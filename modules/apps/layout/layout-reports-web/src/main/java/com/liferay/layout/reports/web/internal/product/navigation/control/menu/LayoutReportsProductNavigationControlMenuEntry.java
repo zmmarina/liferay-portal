6d6c881b71ca68be7414de28be830d8274b83949
Sarai Díaz García <dgarciasarai@gmail.com>
2021-03-08 18:52:44 +0100

    LPS-119068 Add icon to view the audit panel only for Widget and Content Pages

---
+++ modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/product/navigation/control/menu/LayoutReportsProductNavigationControlMenuEntry.java
@@ -0,0 +1,391 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.reports.web.internal.product.navigation.control.menu;
+
+import com.liferay.layout.reports.web.internal.constants.LayoutReportsPortletKeys;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.portlet.PortletURLFactory;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Html;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.SessionClicks;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.product.navigation.control.menu.BaseProductNavigationControlMenuEntry;
+import com.liferay.product.navigation.control.menu.ProductNavigationControlMenuEntry;
+import com.liferay.product.navigation.control.menu.constants.ProductNavigationControlMenuCategoryKeys;
+import com.liferay.taglib.aui.IconTag;
+import com.liferay.taglib.portletext.RuntimeTag;
+import com.liferay.taglib.util.BodyBottomTag;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.WindowStateException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sarai Díaz
+ */
+@Component(
+	immediate = true,
+	property = {
+		"product.navigation.control.menu.category.key=" + ProductNavigationControlMenuCategoryKeys.USER,
+		"product.navigation.control.menu.entry.order:Integer=550"
+	},
+	service = ProductNavigationControlMenuEntry.class
+)
+public class LayoutReportsProductNavigationControlMenuEntry
+	extends BaseProductNavigationControlMenuEntry {
+
+	@Override
+	public String getLabel(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public String getURL(HttpServletRequest httpServletRequest) {
+		return null;
+	}
+
+	@Override
+	public boolean includeBody(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		BodyBottomTag bodyBottomTag = new BodyBottomTag();
+
+		bodyBottomTag.setOutputKey("layoutReportsPanel");
+
+		try {
+			bodyBottomTag.doBodyTag(
+				httpServletRequest, httpServletResponse,
+				pageContext -> {
+					try {
+						_processBodyBottomTagBody(pageContext);
+					}
+					catch (Exception exception) {
+						throw new ProcessBodyBottomTagBodyException(exception);
+					}
+				});
+		}
+		catch (ProcessBodyBottomTagBodyException
+					processBodyBottomTagBodyException) {
+
+			throw new IOException(processBodyBottomTagBodyException);
+		}
+		catch (JspException jspException) {
+			throw new IOException(jspException);
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean includeIcon(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		Map<String, String> values = new HashMap<>();
+
+		if (_isPanelStateOpen(httpServletRequest)) {
+			values.put("cssClass", "active");
+			values.put("layoutReportsPanelURL", StringPool.BLANK);
+		}
+		else {
+			try {
+				values.put(
+					"layoutReportsPanelURL",
+					_getLayoutReportsPanelURL(
+						httpServletRequest, _portal, _portletURLFactory));
+			}
+			catch (WindowStateException windowStateException) {
+				throw new IOException(windowStateException);
+			}
+
+			values.put("cssClass", StringPool.BLANK);
+		}
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			_portal.getLocale(httpServletRequest), getClass());
+
+		values.put(
+			"title", _html.escape(_language.get(resourceBundle, "page-audit")));
+
+		IconTag iconTag = new IconTag();
+
+		iconTag.setCssClass("icon-monospaced");
+		iconTag.setImage("info-circle");
+		iconTag.setMarkupView("lexicon");
+
+		try {
+			values.put(
+				"iconTag",
+				iconTag.doTagAsString(httpServletRequest, httpServletResponse));
+		}
+		catch (JspException jspException) {
+			throw new IOException(jspException);
+		}
+
+		values.put("portletNamespace", _portletNamespace);
+
+		Writer writer = httpServletResponse.getWriter();
+
+		writer.write(StringUtil.replace(_ICON_TMPL_CONTENT, "${", "}", values));
+
+		return true;
+	}
+
+	@Override
+	public boolean isShow(HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		if (!_isShow(themeDisplay.getPlid()) ||
+			!_isShowPanel(httpServletRequest)) {
+
+			return false;
+		}
+
+		return super.isShow(httpServletRequest);
+	}
+
+	public static class ProcessBodyBottomTagBodyException
+		extends RuntimeException {
+
+		public ProcessBodyBottomTagBodyException(Throwable throwable) {
+			super(throwable);
+		}
+
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		_portletNamespace = _portal.getPortletNamespace(
+			LayoutReportsPortletKeys.LAYOUT_REPORTS);
+	}
+
+	private String _getLayoutReportsPanelURL(
+			HttpServletRequest httpServletRequest, Portal portal,
+			PortletURLFactory portletURLFactory)
+		throws WindowStateException {
+
+		PortletURL portletURL = portletURLFactory.create(
+			httpServletRequest, LayoutReportsPortletKeys.LAYOUT_REPORTS,
+			RenderRequest.RENDER_PHASE);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "/layout_reports/layout_reports_panel");
+		portletURL.setParameter(
+			"redirect", portal.getCurrentCompleteURL(httpServletRequest));
+		portletURL.setWindowState(LiferayWindowState.EXCLUSIVE);
+
+		return portletURL.toString();
+	}
+
+	private boolean _hasEditPermission(
+			Layout layout, PermissionChecker permissionChecker)
+		throws PortalException {
+
+		if (!LayoutPermissionUtil.contains(
+				permissionChecker, layout, ActionKeys.UPDATE)) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean _isEmbeddedPersonalApplicationLayout(Layout layout) {
+		if (layout.isTypeControlPanel()) {
+			return false;
+		}
+
+		String layoutFriendlyURL = layout.getFriendlyURL();
+
+		if (layout.isSystem() &&
+			layoutFriendlyURL.equals(
+				PropsUtil.get(PropsKeys.CONTROL_PANEL_LAYOUT_FRIENDLY_URL))) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isPanelStateOpen(HttpServletRequest httpServletRequest) {
+		String layoutReportsPanelState = SessionClicks.get(
+			httpServletRequest,
+			"com.liferay.layout.reports.web_layoutReportsPanelState", "closed");
+
+		if (Objects.equals(layoutReportsPanelState, "open")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isShow(long plid) {
+		return Optional.ofNullable(
+			_layoutLocalService.fetchLayout(plid)
+		).filter(
+			layout -> layout.isTypePortlet() || layout.isTypeAssetDisplay()
+		).filter(
+			layout -> !_isEmbeddedPersonalApplicationLayout(layout)
+		).filter(
+			layout -> {
+				try {
+					return _hasEditPermission(
+						layout, PermissionThreadLocal.getPermissionChecker());
+				}
+				catch (PortalException portalException) {
+					_log.error(portalException, portalException);
+
+					return false;
+				}
+			}
+		).isPresent();
+	}
+
+	private boolean _isShowPanel(HttpServletRequest httpServletRequest) {
+		String layoutMode = ParamUtil.getString(
+			httpServletRequest, "p_l_mode", Constants.VIEW);
+
+		if (layoutMode.equals(Constants.EDIT)) {
+			return false;
+		}
+
+		PortalPreferences portalPreferences =
+			PortletPreferencesFactoryUtil.getPortalPreferences(
+				httpServletRequest);
+
+		boolean hidePanel = GetterUtil.getBoolean(
+			portalPreferences.getValue(
+				LayoutReportsPortletKeys.LAYOUT_REPORTS, "hide-panel"));
+
+		if (hidePanel) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private void _processBodyBottomTagBody(PageContext pageContext)
+		throws IOException, JspException {
+
+		HttpServletRequest httpServletRequest =
+			(HttpServletRequest)pageContext.getRequest();
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			_portal.getLocale(httpServletRequest), getClass());
+
+		pageContext.setAttribute("resourceBundle", resourceBundle);
+
+		JspWriter jspWriter = pageContext.getOut();
+
+		jspWriter.write("<div class=\"");
+
+		if (_isPanelStateOpen(httpServletRequest)) {
+			jspWriter.write("lfr-has-layout-reports-panel open-admin-panel ");
+		}
+
+		jspWriter.write(
+			StringBundler.concat(
+				"d-print-none lfr-admin-panel lfr-product-menu-panel ",
+				"lfr-layout-reports-panel sidenav-fixed sidenav-menu-slider ",
+				"sidenav-right\" id=\""));
+
+		jspWriter.write(_portletNamespace);
+
+		jspWriter.write("layoutReportsPanelId\">");
+		jspWriter.write(
+			"<div class=\"sidebar sidebar-light sidenav-menu sidebar-sm\">");
+
+		RuntimeTag runtimeTag = new RuntimeTag();
+
+		runtimeTag.setPortletName(LayoutReportsPortletKeys.LAYOUT_REPORTS);
+
+		runtimeTag.doTag(pageContext);
+
+		jspWriter.write("</div></div>");
+	}
+
+	private static final String _ICON_TMPL_CONTENT = StringUtil.read(
+		LayoutReportsProductNavigationControlMenuEntry.class, "icon.tmpl");
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutReportsProductNavigationControlMenuEntry.class);
+
+	@Reference
+	private Html _html;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	private String _portletNamespace;
+
+	@Reference
+	private PortletURLFactory _portletURLFactory;
+
+}
--- modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/view.jsp
+++ modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/view.jsp
@@ -15,3 +15,60 @@
 --%>
 
 <%@ include file="/init.jsp" %>
+
+<div class="lfr-layouts-reports-sidebar" id="layoutsReportsSidebar">
+	<div class="sidebar-header">
+		<clay:content-row
+			cssClass="sidebar-section"
+		>
+			<clay:content-col
+				expand="<%= true %>"
+			>
+				<h1 class="sr-only"><liferay-ui:message key="page-audit" /></h1>
+
+				<span><liferay-ui:message key="page-audit" /></span>
+			</clay:content-col>
+
+			<clay:content-col>
+				<clay:button
+					cssClass="sidenav-close"
+					displayType="unstyled"
+					icon="times-small"
+					monospaced="<%= true %>"
+				/>
+			</clay:content-col>
+		</clay:content-row>
+	</div>
+
+	<div class="sidebar-body">
+		<liferay-util:include page="/layout_reports_panel.jsp" servletContext="<%= application %>" />
+	</div>
+</div>
+
+<aui:script>
+	var layoutReportsPanelToggle = document.getElementById(
+		'<portlet:namespace />layoutReportsPanelToggleId'
+	);
+
+	var sidenavInstance = Liferay.SideNavigation.initialize(
+		layoutReportsPanelToggle
+	);
+
+	sidenavInstance.on('open.lexicon.sidenav', (event) => {
+		Liferay.Util.Session.set(
+			'com.liferay.layout.reports.web_layoutReportsPanelState',
+			'open'
+		);
+	});
+
+	sidenavInstance.on('closed.lexicon.sidenav', (event) => {
+		Liferay.Util.Session.set(
+			'com.liferay.layout.reports.web_layoutReportsPanelState',
+			'closed'
+		);
+	});
+
+	Liferay.once('screenLoad', () => {
+		Liferay.SideNavigation.destroy(layoutReportsPanelToggle);
+	});
+</aui:script>
