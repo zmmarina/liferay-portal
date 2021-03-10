d828b9ce23a3bc088cfb666c41ae4feea693a1e1
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-03-10 10:10:15 +0100

    LPS-128888 Adding javadocs for APIs meant to be implemented by users

--- modules/apps/document-library/document-library-api/src/main/java/com/liferay/document/library/video/external/shortcut/DLVideoExternalShortcut.java
+++ modules/apps/document-library/document-library-api/src/main/java/com/liferay/document/library/video/external/shortcut/DLVideoExternalShortcut.java
@@ -17,24 +17,84 @@ package com.liferay.document.library.video.external.shortcut;
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * Provides an external video shortcut to be used by the document library to
+ * link to and render videos hosted in external services.
+ *
+ * <p>
+ * Implementations of this interface will be returned by an implementation of
+ * {@link
+ * com.liferay.document.library.video.external.shortcut.provider.DLVideoExternalShortcutProvider}
+ * </p>
+ *
  * @author Alejandro Tardín
+ * @review
  */
 public interface DLVideoExternalShortcut {
 
+	/**
+	 * Returns the description of the external video (if any).
+	 *
+	 * <p>
+	 * It will be used to fill the document's description field when saving it
+	 * to the document library.
+	 * </p>
+	 *
+	 * @return the description
+	 * @review
+	 */
 	public default String getDescription() {
 		return null;
 	}
 
+	/**
+	 * Returns the thumbnail URL of the external video (if any).
+	 *
+	 * <p>
+	 * It will be used inside the Documents and Media UI (cards, lists, etc...).
+	 * </p>
+	 *
+	 * @return the thumbnail URL.
+	 * @review
+	 */
 	public default String getThumbnailURL() {
 		return null;
 	}
 
+	/**
+	 * Returns the title of the external video (if any).
+	 *
+	 * <p>
+	 * It will be used to fill the document's title field when saving it to the
+	 * document library.
+	 * </p>
+	 *
+	 * @return the title
+	 * @review
+	 */
 	public default String getTitle() {
 		return null;
 	}
 
+	/**
+	 * Returns the URL of the external video.
+	 *
+	 * @return the URL
+	 * @review
+	 */
 	public String getURL();
 
+	/**
+	 * Returns a snippet of HTML that will be used to embed the video inside a
+	 * specific content.
+	 *
+	 * <p>
+	 * This will typically render an <code>iframe</code> or a <code>video</code>
+	 * element.
+	 * </p>
+	 *
+	 * @return the HTML
+	 * @review
+	 */
 	public String renderHTML(HttpServletRequest httpServletRequest);
 
 }
--- modules/apps/document-library/document-library-api/src/main/java/com/liferay/document/library/video/external/shortcut/provider/DLVideoExternalShortcutProvider.java
+++ modules/apps/document-library/document-library-api/src/main/java/com/liferay/document/library/video/external/shortcut/provider/DLVideoExternalShortcutProvider.java
@@ -17,10 +17,38 @@ package com.liferay.document.library.video.external.shortcut.provider;
 import com.liferay.document.library.video.external.shortcut.DLVideoExternalShortcut;
 
 /**
+ * Provides a way to retrieve a {@link DLVideoExternalShortcut} from a URL.
+ *
+ * <p>
+ * Implement this interface to add support for custom external video providers.
+ * The implementation should:
+ * <ol>
+ * <li>
+ * Decide whether or not to process the URL (based on a regex for example).
+ * </li>
+ * <li>
+ * Return <code>null</code> if the URL does not match to allow matching other
+ * providers.
+ * </li>
+ * <li>
+ * Fetch extra information from the external service and return a {@link
+ * DLVideoExternalShortcut} with the video details.
+ * </li>
+ * </ol>
+ * </p>
+ *
  * @author Alejandro Tardín
+ * @review
  */
 public interface DLVideoExternalShortcutProvider {
 
+	/**
+	 * Returns a {@link DLVideoExternalShortcut} from a given URL, or
+	 * <code>null</code> if the URL is not recognized.
+	 *
+	 * @return the DLVideoExternalShortcut or <code>null</code>
+	 * @review
+	 */
 	public DLVideoExternalShortcut getDLVideoExternalShortcut(String url);
 
 }
