54ecaa39308f8f4733107ea17a876ab254772099
arthurchan35 <arthurchan35@gmail.com>
2021-03-10 11:09:53 +0800

    LPS-111200 Add factory for BCrypt hash provider

--- modules/apps/portal-crypto-hash/portal-crypto-hash-provider/portal-crypto-hash-provider-bcrypt/src/main/java/com/liferay/portal/crypto/hash/provider/bcrypt/internal/BCryptCryptoHashProvider.java
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-provider/portal-crypto-hash-provider-bcrypt/src/main/java/com/liferay/portal/crypto/hash/provider/bcrypt/internal/BCryptCryptoHashProvider.java
@@ -16,10 +16,11 @@ package com.liferay.portal.crypto.hash.provider.bcrypt.internal;
 
 import com.liferay.portal.crypto.hash.spi.CryptoHashProvider;
 import com.liferay.portal.crypto.hash.spi.CryptoHashProviderResponse;
+import com.liferay.portal.kernel.util.MapUtil;
 
 import java.nio.charset.StandardCharsets;
 
-import java.util.HashMap;
+import java.util.Map;
 
 import jodd.crypt.BCrypt;
 
@@ -28,6 +29,23 @@ import jodd.crypt.BCrypt;
  */
 public class BCryptCryptoHashProvider implements CryptoHashProvider {
 
+	public static final String ROUNDS = "rounds";
+
+	public BCryptCryptoHashProvider(String cryptoHashProviderFactoryName) {
+		_cryptoHashProviderFactoryName = cryptoHashProviderFactoryName;
+
+		_rounds = _DEFAULT_ROUNDS;
+	}
+
+	public BCryptCryptoHashProvider(
+		String cryptoHashProviderFactoryName,
+		Map<String, ?> cryptoHashProviderProperties) {
+
+		_cryptoHashProviderFactoryName = cryptoHashProviderFactoryName;
+
+		_rounds = MapUtil.getInteger(cryptoHashProviderProperties, ROUNDS);
+	}
+
 	@Override
 	public CryptoHashProviderResponse generate(byte[] salt, byte[] input) {
 		String hashedPassword = BCrypt.hashpw(
@@ -35,22 +53,20 @@ public class BCryptCryptoHashProvider implements CryptoHashProvider {
 			new String(salt, StandardCharsets.US_ASCII));
 
 		return new CryptoHashProviderResponse(
-			_CRYPTO_HASH_PROVIDER_NAME, new HashMap<>(),
+			_cryptoHashProviderFactoryName, null,
 			hashedPassword.getBytes(StandardCharsets.US_ASCII));
 	}
 
 	@Override
 	public byte[] generateSalt() {
-		String salt = BCrypt.gensalt();
+		String salt = BCrypt.gensalt(_rounds);
 
 		return salt.getBytes(StandardCharsets.US_ASCII);
 	}
 
-	@Override
-	public String getCryptoHashProviderName() {
-		return _CRYPTO_HASH_PROVIDER_NAME;
-	}
+	private static final int _DEFAULT_ROUNDS = 10;
 
-	private static final String _CRYPTO_HASH_PROVIDER_NAME = "BCrypt";
+	private final String _cryptoHashProviderFactoryName;
+	private final int _rounds;
 
 }
---
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-provider/portal-crypto-hash-provider-bcrypt/src/main/java/com/liferay/portal/crypto/hash/provider/bcrypt/internal/BCryptCryptoHashProviderFactory.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.crypto.hash.provider.bcrypt.internal;
+
+import com.liferay.portal.crypto.hash.spi.CryptoHashProvider;
+import com.liferay.portal.crypto.hash.spi.CryptoHashProviderFactory;
+
+import java.util.Map;
+
+/**
+ * @author Arthur Chan
+ */
+public class BCryptCryptoHashProviderFactory
+	implements CryptoHashProviderFactory {
+
+	@Override
+	public CryptoHashProvider create(
+			Map<String, ?> cryptoHashProviderProperties)
+		throws Exception {
+
+		if ((cryptoHashProviderProperties == null) ||
+			cryptoHashProviderProperties.isEmpty()) {
+
+			return new BCryptCryptoHashProvider(
+				getCryptoHashProviderFactoryName());
+		}
+
+		return new BCryptCryptoHashProvider(
+			getCryptoHashProviderFactoryName(), cryptoHashProviderProperties);
+	}
+
+	@Override
+	public String getCryptoHashProviderFactoryName() {
+		return _CRYPTO_HASH_PROVIDER_FACTORY_NAME;
+	}
+
+	private static final String _CRYPTO_HASH_PROVIDER_FACTORY_NAME = "BCrypt";
+
+}
