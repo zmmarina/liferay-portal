d5e72ff2109da0296f1491b8dc2163ec3f87a43d
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-03-10 17:40:22 +0100

    LPS-128746 Add tests.

---
+++ modules/dxp/apps/analytics/analytics-reports-layout-display-page-test/src/testIntegration/java/com/liferay/analytics/reports/layout/display/page/internal/info/item/test/LayoutDisplayPageObjectProviderAnalyticsReportsInfoItemTest.java
@@ -0,0 +1,503 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.analytics.reports.layout.display.page.internal.info.item.test;
+
+import com.liferay.analytics.reports.layout.display.page.info.item.LayoutDisplayPageObjectProviderAnalyticsReportsInfoItem;
+import com.liferay.analytics.reports.layout.display.page.internal.test.MockObject;
+import com.liferay.analytics.reports.layout.display.page.internal.test.layout.display.page.MockObjectLayoutDisplayPageObjectProvider;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.display.page.constants.AssetDisplayPageConstants;
+import com.liferay.asset.display.page.model.AssetDisplayPageEntry;
+import com.liferay.asset.display.page.service.AssetDisplayPageEntryLocalService;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.DateInfoFieldType;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.info.item.InfoItemReference;
+import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
+import com.liferay.layout.display.page.LayoutDisplayPageProvider;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Cristina González
+ */
+@RunWith(Arquillian.class)
+public class LayoutDisplayPageObjectProviderAnalyticsReportsInfoItemTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_className = _classNameLocalService.addClassName(
+			MockObject.class.getName());
+	}
+
+	@Test
+	public void testGetAuthorName() throws Exception {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId());
+
+		Assert.assertEquals(
+			StringPool.BLANK,
+			_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+				getAuthorName(mockObjectLayoutDisplayPageObjectProvider));
+	}
+
+	@Test
+	public void testGetAuthorUserId() throws Exception {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId());
+
+		Assert.assertEquals(
+			0L,
+			_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+				getAuthorUserId(mockObjectLayoutDisplayPageObjectProvider));
+	}
+
+	@Test
+	public void testGetAvailableLocales() throws Exception {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId(), _group.getGroupId());
+
+		GroupTestUtil.updateDisplaySettings(
+			_group.getGroupId(),
+			Arrays.asList(LocaleUtil.BRAZIL, LocaleUtil.SPAIN),
+			LocaleUtil.SPAIN);
+
+		Assert.assertEquals(
+			Arrays.asList(LocaleUtil.BRAZIL, LocaleUtil.SPAIN),
+			_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+				getAvailableLocales(mockObjectLayoutDisplayPageObjectProvider));
+	}
+
+	@Test
+	public void testGetCanonicalURL() throws Exception {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId(), _group.getGroupId());
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			LayoutDisplayPageObjectProviderAnalyticsReportsInfoItemTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		ServiceRegistration<LayoutDisplayPageProvider<?>> serviceRegistration =
+			bundleContext.registerService(
+				(Class<LayoutDisplayPageProvider<?>>)
+					(Class<?>)LayoutDisplayPageProvider.class,
+				new LayoutDisplayPageProvider<MockObject>() {
+
+					@Override
+					public String getClassName() {
+						return MockObject.class.getName();
+					}
+
+					@Override
+					public LayoutDisplayPageObjectProvider
+						getLayoutDisplayPageObjectProvider(
+							InfoItemReference infoItemReference) {
+
+						return mockObjectLayoutDisplayPageObjectProvider;
+					}
+
+					@Override
+					public LayoutDisplayPageObjectProvider
+						getLayoutDisplayPageObjectProvider(
+							long groupId, String urlTitle) {
+
+						return mockObjectLayoutDisplayPageObjectProvider;
+					}
+
+					@Override
+					public String getURLSeparator() {
+						return "||separator||";
+					}
+
+				},
+				new HashMapDictionary<String, String>());
+
+		try {
+			LayoutPageTemplateEntry layoutPageTemplateEntry =
+				_layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
+					_group.getCreatorUserId(), _group.getGroupId(), 0,
+					_className.getClassNameId(), 0,
+					RandomTestUtil.randomString(),
+					LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE, 0,
+					true, 0, 0, 0, 0,
+					ServiceContextTestUtil.getServiceContext(
+						_group.getGroupId()));
+
+			_assetDisplayPageEntryLocalService.addAssetDisplayPageEntry(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				_className.getClassNameId(), 0,
+				layoutPageTemplateEntry.getLayoutPageTemplateEntryId(),
+				AssetDisplayPageConstants.TYPE_SPECIFIC,
+				ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+			ServiceContext serviceContext =
+				ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+			MockHttpServletRequest mockHttpServletRequest =
+				new MockHttpServletRequest();
+
+			mockHttpServletRequest.setAttribute(
+				WebKeys.CURRENT_COMPLETE_URL, StringPool.BLANK);
+
+			ThemeDisplay themeDisplay = new ThemeDisplay();
+
+			themeDisplay.setCompany(
+				_companyLocalService.fetchCompany(
+					TestPropsValues.getCompanyId()));
+
+			Layout layout = _layoutLocalService.addLayout(
+				TestPropsValues.getUserId(), _group.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				StringPool.BLANK, LayoutConstants.TYPE_CONTENT, false,
+				StringPool.BLANK, serviceContext);
+
+			themeDisplay.setLayoutSet(layout.getLayoutSet());
+
+			themeDisplay.setRequest(mockHttpServletRequest);
+			themeDisplay.setSiteGroupId(_group.getGroupId());
+
+			mockHttpServletRequest.setAttribute(
+				WebKeys.THEME_DISPLAY, themeDisplay);
+
+			serviceContext.setRequest(mockHttpServletRequest);
+
+			ServiceContextThreadLocal.pushServiceContext(serviceContext);
+
+			String canonicalURL =
+				_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+					getCanonicalURL(
+						mockObjectLayoutDisplayPageObjectProvider,
+						LocaleUtil.US);
+
+			Assert.assertTrue(canonicalURL.contains("||separator||"));
+		}
+		finally {
+			ServiceContextThreadLocal.popServiceContext();
+
+			serviceRegistration.unregister();
+		}
+	}
+
+	@Test
+	public void testGetDefaultLocale() throws Exception {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId(), _group.getGroupId());
+
+		GroupTestUtil.updateDisplaySettings(
+			_group.getGroupId(),
+			Arrays.asList(LocaleUtil.BRAZIL, LocaleUtil.SPAIN),
+			LocaleUtil.SPAIN);
+
+		Assert.assertEquals(
+			LocaleUtil.SPAIN,
+			_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+				getDefaultLocale(mockObjectLayoutDisplayPageObjectProvider));
+	}
+
+	@Test
+	public void testGetPublishDate() {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId(), _group.getGroupId());
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			LayoutDisplayPageObjectProviderAnalyticsReportsInfoItemTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		Date createDate = new Date();
+
+		ServiceRegistration<InfoItemFieldValuesProvider<?>>
+			serviceRegistration = bundleContext.registerService(
+				(Class<InfoItemFieldValuesProvider<?>>)
+					(Class<?>)InfoItemFieldValuesProvider.class,
+				MockInfoItemFieldValuesProvider.builder(
+				).createDate(
+					createDate
+				).build(),
+				new HashMapDictionary<String, String>());
+
+		try {
+			Assert.assertEquals(
+				createDate,
+				_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+					getPublishDate(mockObjectLayoutDisplayPageObjectProvider));
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+	}
+
+	@Test
+	public void testGetPublishDateWithDisplayPage() throws Exception {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId(), _group.getGroupId());
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			LayoutDisplayPageObjectProviderAnalyticsReportsInfoItemTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		ServiceRegistration<InfoItemFieldValuesProvider<?>>
+			serviceRegistration = bundleContext.registerService(
+				(Class<InfoItemFieldValuesProvider<?>>)
+					(Class<?>)InfoItemFieldValuesProvider.class,
+				MockInfoItemFieldValuesProvider.builder(
+				).createDate(
+					new Date()
+				).build(),
+				new HashMapDictionary<String, String>());
+
+		try {
+			LayoutPageTemplateEntry layoutPageTemplateEntry =
+				_layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
+					_group.getCreatorUserId(), _group.getGroupId(), 0,
+					_className.getClassNameId(), 0,
+					RandomTestUtil.randomString(),
+					LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE, 0,
+					true, 0, 0, 0, 0,
+					ServiceContextTestUtil.getServiceContext(
+						_group.getGroupId()));
+
+			AssetDisplayPageEntry assetDisplayPageEntry =
+				_assetDisplayPageEntryLocalService.addAssetDisplayPageEntry(
+					TestPropsValues.getUserId(), _group.getGroupId(),
+					_className.getClassNameId(), 0,
+					layoutPageTemplateEntry.getLayoutPageTemplateEntryId(),
+					AssetDisplayPageConstants.TYPE_SPECIFIC,
+					ServiceContextTestUtil.getServiceContext(
+						_group.getGroupId()));
+
+			Assert.assertEquals(
+				assetDisplayPageEntry.getCreateDate(),
+				_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+					getPublishDate(mockObjectLayoutDisplayPageObjectProvider));
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+	}
+
+	@Test
+	public void testGetTitle() throws Exception {
+		MockObjectLayoutDisplayPageObjectProvider
+			mockObjectLayoutDisplayPageObjectProvider =
+				new MockObjectLayoutDisplayPageObjectProvider(
+					_className.getClassNameId(), _group.getGroupId());
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			LayoutDisplayPageObjectProviderAnalyticsReportsInfoItemTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		String title = RandomTestUtil.randomString();
+
+		ServiceRegistration<InfoItemFieldValuesProvider<?>>
+			serviceRegistration = bundleContext.registerService(
+				(Class<InfoItemFieldValuesProvider<?>>)
+					(Class<?>)InfoItemFieldValuesProvider.class,
+				MockInfoItemFieldValuesProvider.builder(
+				).title(
+					title
+				).build(),
+				new HashMapDictionary<String, String>());
+
+		try {
+			Assert.assertEquals(
+				title,
+				_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem.
+					getTitle(
+						mockObjectLayoutDisplayPageObjectProvider,
+						LocaleUtil.ENGLISH));
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+	}
+
+	@Inject
+	private AssetDisplayPageEntryLocalService
+		_assetDisplayPageEntryLocalService;
+
+	@DeleteAfterTestRun
+	private ClassName _className;
+
+	@Inject
+	private ClassNameLocalService _classNameLocalService;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private LayoutDisplayPageObjectProviderAnalyticsReportsInfoItem
+		_layoutDisplayPageObjectProviderAnalyticsReportsInfoItem;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
+
+	@Inject
+	private Portal _portal;
+
+	@Inject
+	private UserLocalService _userLocalService;
+
+	private static class MockInfoItemFieldValuesProvider
+		implements InfoItemFieldValuesProvider<MockObject> {
+
+		public static Builder builder() {
+			return new Builder();
+		}
+
+		public MockInfoItemFieldValuesProvider(Date createDate, String title) {
+			_createDate = createDate;
+			_title = title;
+		}
+
+		@Override
+		public InfoItemFieldValues getInfoItemFieldValues(
+			MockObject mockObject) {
+
+			return InfoItemFieldValues.builder(
+			).infoFieldValue(
+				new InfoFieldValue<>(
+					InfoField.builder(
+					).infoFieldType(
+						DateInfoFieldType.INSTANCE
+					).name(
+						"createDate"
+					).labelInfoLocalizedValue(
+						null
+					).build(),
+					_createDate)
+			).infoFieldValue(
+				new InfoFieldValue<>(
+					InfoField.builder(
+					).infoFieldType(
+						TextInfoFieldType.INSTANCE
+					).name(
+						"title"
+					).labelInfoLocalizedValue(
+						null
+					).build(),
+					_title)
+			).build();
+		}
+
+		public static class Builder {
+
+			public MockInfoItemFieldValuesProvider build() {
+				return new MockInfoItemFieldValuesProvider(_createDate, _title);
+			}
+
+			public Builder createDate(Date createDate) {
+				_createDate = createDate;
+
+				return this;
+			}
+
+			public Builder title(String title) {
+				_title = title;
+
+				return this;
+			}
+
+			private Date _createDate;
+			private String _title;
+
+		}
+
+		private final Date _createDate;
+		private final String _title;
+
+	}
+
+}
