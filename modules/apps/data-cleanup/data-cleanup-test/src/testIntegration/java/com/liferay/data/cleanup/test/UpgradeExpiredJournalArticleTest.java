a85aa59ef4f83c7233c42bbbb68b7b49a65ad0bf
Kevin Lee <kevin.lee@liferay.com>
2021-03-10 19:51:41 -0800

    LPS-124518 Add integration test

---
+++ modules/apps/data-cleanup/data-cleanup-test/src/testIntegration/java/com/liferay/data/cleanup/test/UpgradeExpiredJournalArticleTest.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.cleanup.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.constants.JournalFolderConstants;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Dictionary;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Kevin Lee
+ */
+@RunWith(Arquillian.class)
+public class UpgradeExpiredJournalArticleTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testUpgradeExpiredJournalArticle() throws Exception {
+		JournalArticle expiredJournalArticleA = JournalTestUtil.addArticle(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		JournalArticle expiredJournalArticleB = JournalTestUtil.updateArticle(
+			expiredJournalArticleA);
+
+		JournalArticle unexpiredJournalArticle = JournalTestUtil.addArticle(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		JournalTestUtil.expireArticle(
+			expiredJournalArticleB.getGroupId(), expiredJournalArticleB);
+
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("cleanUpExpiredJournalArticleModuleData", true);
+
+		try (ConfigurationTemporarySwapper configurationTemporarySwapper =
+				new ConfigurationTemporarySwapper(
+					_CONFIGURATION_PID, properties)) {
+
+			FinderCacheUtil.clearLocalCache();
+
+			Assert.assertNull(
+				_journalArticleLocalService.fetchArticle(
+					expiredJournalArticleA.getId()));
+
+			Assert.assertNull(
+				_journalArticleLocalService.fetchArticle(
+					expiredJournalArticleB.getId()));
+
+			Assert.assertNotNull(
+				_journalArticleLocalService.fetchArticle(
+					unexpiredJournalArticle.getId()));
+		}
+	}
+
+	private static final String _CONFIGURATION_PID =
+		"com.liferay.data.cleanup.internal.configuration." +
+			"DataCleanupConfiguration";
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private JournalArticleLocalService _journalArticleLocalService;
+
+}
