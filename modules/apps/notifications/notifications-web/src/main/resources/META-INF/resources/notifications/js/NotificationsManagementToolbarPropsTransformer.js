1cbe342840671e504308662df3611ac42c82a8b2
Jonathan Mak <jonathan.mak@liferay.com>
2021-03-12 00:37:16 -0800

    LPS-121893 - Add option chaining to object lookups

--- modules/apps/notifications/notifications-web/src/main/resources/META-INF/resources/notifications/js/NotificationsManagementToolbarPropsTransformer.js
+++ modules/apps/notifications/notifications-web/src/main/resources/META-INF/resources/notifications/js/NotificationsManagementToolbarPropsTransformer.js
@@ -62,7 +62,7 @@ export default function propsTransformer({
 	return {
 		...otherProps,
 		onActionButtonClick: (event, {item}) => {
-			const action = item.data?.action;
+			const action = item?.data?.action;
 
 			if (action === 'deleteNotifications') {
 				deleteNotifications();
