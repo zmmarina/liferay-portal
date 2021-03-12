12436b06f1667b412aea9cb570598f07f486ca31
Igor Beslic <igor.beslic@liferay.com>
2021-03-12 13:30:41 +0100

    COMMERCE-6072 talend-runtime - Source formatted :0

--- modules/etl/talend/talend-runtime/src/main/java/com/liferay/talend/runtime/reader/LiferayBatchFileReader.java
+++ modules/etl/talend/talend-runtime/src/main/java/com/liferay/talend/runtime/reader/LiferayBatchFileReader.java
@@ -120,9 +120,6 @@ public class LiferayBatchFileReader implements Reader<IndexedRecord> {
 		try (FileWriter fileWriter = new FileWriter(_batchSegmentFile)) {
 			String line = _bufferedReader.readLine();
 
-			if (line == null) {
-			}
-
 			while ((line != null) && ((++_current % _BATCH_SIZE) != 0)) {
 				fileWriter.write(line);
 				fileWriter.write(System.lineSeparator());
