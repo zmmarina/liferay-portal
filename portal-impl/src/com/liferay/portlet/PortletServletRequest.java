6f8897e18443bb387fb2165abf7d370a644fdb59
Dante Wang <dante.wang@liferay.com>
2021-03-16 10:44:27 +0800

    LPS-120742 Remove Jetty related classes as they are deprecated as of 7.2.x

--- portal-impl/src/com/liferay/portlet/PortletServletRequest.java
+++ portal-impl/src/com/liferay/portlet/PortletServletRequest.java
@@ -19,15 +19,12 @@ import com.liferay.portal.kernel.servlet.HttpMethods;
 import com.liferay.portal.kernel.servlet.ServletInputStreamAdapter;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.JavaConstants;
-import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portlet.internal.PortletRequestDispatcherImpl;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-
-import java.lang.reflect.Constructor;
 
 import java.security.Principal;
 
@@ -484,28 +481,6 @@ public class PortletServletRequest extends HttpServletRequestWrapper {
 
 			clientDataRequest.setCharacterEncoding(encoding);
 		}
-	}
-
-	/**
-	 * @deprecated As of Mueller (7.2.x), with no direct replacement
-	 */
-	@Deprecated
-	protected HttpSession wrapJettySession(HttpSession session)
-		throws Exception {
-
-		// This must be called through reflection because Resin tries to load
-		// org/mortbay/jetty/servlet/AbstractSessionManager$SessionIf
-
-		ClassLoader classLoader = PortalClassLoaderUtil.getClassLoader();
-
-		Class<?> jettyHttpSessionWrapperClass = classLoader.loadClass(
-			"com.liferay.portal.servlet.JettyHttpSessionWrapper");
-
-		Constructor<?> constructor =
-			jettyHttpSessionWrapperClass.getConstructor(
-				new Class<?>[] {HttpSession.class});
-
-		return (HttpSession)constructor.newInstance(new Object[] {session});
 	}
 
 	private ClientDataRequest _getClientDataRequest() {
