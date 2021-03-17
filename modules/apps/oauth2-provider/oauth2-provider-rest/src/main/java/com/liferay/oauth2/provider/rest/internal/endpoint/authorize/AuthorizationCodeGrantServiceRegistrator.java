b868a863dee75259a8e0ae549a6d9e143cf64af7
Brian Chan <brian.chan@liferay.com>
2021-03-17 09:16:45 -0300

    LPS-126597 inconsistent name

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/authorize/AuthorizationCodeGrantServiceRegistrator.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/authorize/AuthorizationCodeGrantServiceRegistrator.java
@@ -158,9 +158,9 @@ public class AuthorizationCodeGrantServiceRegistrator {
 					permissions);
 			}
 
-			String rememberDeviceCookieContent = _getRememberDeviceContent();
+			String rememberDeviceContent = _getRememberDeviceContent();
 
-			if (rememberDeviceCookieContent == null) {
+			if (rememberDeviceContent == null) {
 				return super.canAuthorizationBeSkipped(
 					params, client, userSubject, requestedScopesList,
 					permissions);
@@ -174,10 +174,10 @@ public class AuthorizationCodeGrantServiceRegistrator {
 			OAuth2Authorization oAuth2Authorization =
 				liferayOAuthDataProvider.
 					getOAuth2AuthorizationByRememberDeviceContent(
-						client, rememberDeviceCookieContent, userId);
+						client, rememberDeviceContent, userId);
 
 			if ((oAuth2Authorization != null) &&
-				rememberDeviceCookieContent.equals(
+				rememberDeviceContent.equals(
 					oAuth2Authorization.getRememberDeviceContent())) {
 
 				RefreshToken refreshToken =
