6856b1092031db65ac8440453642bb1bcf07c1a2
Jorge González <jorge.gonzalez@liferay.com>
2021-03-17 16:55:41 +0100

    LPS-129242 Add Input, Textarea, Editor

--- modules/apps/translation/translation-web/src/main/resources/META-INF/resources/js/translate/Translate.js
+++ modules/apps/translation/translation-web/src/main/resources/META-INF/resources/js/translate/Translate.js
@@ -13,23 +13,81 @@
  */
 
 import ClayAlert from '@clayui/alert';
+import ClayForm, {ClayInput} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import ClayLayout from '@clayui/layout';
 import {ClassicEditor} from 'frontend-editor-ckeditor-web';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const TranslationHeader = ({sourceLanguageIdTitle, targetLanguageIdTitle}) => (
+const TranslationFieldEditor = ({
+	editorConfiguration,
+	id,
+	label,
+	sourceContent,
+	sourceContentDir,
+	targetContent,
+}) => (
 	<ClayLayout.Row>
 		<ClayLayout.Col md={6}>
-			<ClayIcon symbol={sourceLanguageIdTitle.toLowerCase()} />
-			<span className="ml-1">{sourceLanguageIdTitle}</span>
-			<div className="separator" />
+			<ClayForm.Group>
+				<label className="control-label">{label}</label>
+				<div
+					className="translation-editor-preview"
+					dangerouslySetInnerHTML={{__html: sourceContent}}
+					dir={sourceContentDir}
+				/>
+			</ClayForm.Group>
 		</ClayLayout.Col>
 		<ClayLayout.Col md={6}>
-			<ClayIcon symbol={targetLanguageIdTitle.toLowerCase()} />
-			<span className="ml-1">{targetLanguageIdTitle}</span>
-			<div className="separator" />
+			<ClayForm.Group>
+				<label className="control-label">{label}</label>
+				<ClassicEditor
+					contents={targetContent}
+					editorConfig={editorConfiguration.editorConfig}
+					name={id}
+				/>
+			</ClayForm.Group>
+		</ClayLayout.Col>
+	</ClayLayout.Row>
+);
+
+const TranslationFieldInput = ({
+	id,
+	label,
+	multiline,
+	sourceContent,
+	sourceContentDir,
+	targetContent,
+	targetContentDir,
+}) => (
+	<ClayLayout.Row>
+		<ClayLayout.Col md={6}>
+			<ClayForm.Group>
+				<label className="control-label">{label}</label>
+				<ClayInput
+					component={multiline ? 'textarea' : undefined}
+					defaultValue={sourceContent}
+					dir={sourceContentDir}
+					readOnly
+					type="text"
+				/>
+			</ClayForm.Group>
+		</ClayLayout.Col>
+		<ClayLayout.Col md={6}>
+			<ClayForm.Group>
+				<label className="control-label" htmlFor={id}>
+					{label}
+				</label>
+				<ClayInput
+					component={multiline ? 'textarea' : undefined}
+					defaultValue={targetContent}
+					dir={targetContentDir}
+					id={id}
+					name={id}
+					type="text"
+				/>
+			</ClayForm.Group>
 		</ClayLayout.Col>
 	</ClayLayout.Row>
 );
@@ -45,39 +103,30 @@ const TranslationFieldSetEntries = ({infoFieldSetEntries}) =>
 					<div className="fieldset-title">{legend}</div>
 				</ClayLayout.Col>
 			</ClayLayout.Row>
-			{fields.map(
-				({
-					editorConfiguration,
-					id,
-					label,
-					sourceContent,
-					targetContent,
-				}) => (
-					<ClayLayout.Row key={label}>
-						<ClayLayout.Col md={6}>
-							<label className="control-label">{label}</label>
-
-							<div
-								className="translation-editor-preview"
-								dir="<%= sourceContentDir %>"
-							>
-								{sourceContent}
-							</div>
-						</ClayLayout.Col>
-						<ClayLayout.Col md={6}>
-							<label className="control-label">{label}</label>
-
-							<ClassicEditor
-								contents={targetContent}
-								editorConfig={editorConfiguration.editorConfig}
-								name={id}
-							/>
-						</ClayLayout.Col>
-					</ClayLayout.Row>
+			{fields.map((field) =>
+				field.html ? (
+					<TranslationFieldEditor key={field.id} {...field} />
+				) : (
+					<TranslationFieldInput key={field.id} {...field} />
 				)
 			)}
 		</React.Fragment>
 	));
+
+const TranslationHeader = ({sourceLanguageIdTitle, targetLanguageIdTitle}) => (
+	<ClayLayout.Row>
+		<ClayLayout.Col md={6}>
+			<ClayIcon symbol={sourceLanguageIdTitle.toLowerCase()} />
+			<span className="ml-1">{sourceLanguageIdTitle}</span>
+			<div className="separator" />
+		</ClayLayout.Col>
+		<ClayLayout.Col md={6}>
+			<ClayIcon symbol={targetLanguageIdTitle.toLowerCase()} />
+			<span className="ml-1">{targetLanguageIdTitle}</span>
+			<div className="separator" />
+		</ClayLayout.Col>
+	</ClayLayout.Row>
+);
 
 const Translate = ({
 	infoFieldSetEntries,
@@ -113,12 +162,15 @@ Translate.propTypes = {
 		PropTypes.shape({
 			fields: PropTypes.arrayOf(
 				PropTypes.shape({
+					editorConfiguration: PropTypes.object,
 					html: PropTypes.bool,
 					id: PropTypes.string.isRequired,
 					label: PropTypes.string.isRequired,
 					multiline: PropTypes.bool,
-					sourceContent: PropTypes.string,
-					sourceContentDir: PropTypes.string,
+					sourceContent: PropTypes.string.isRequired,
+					sourceContentDir: PropTypes.string.isRequired,
+					targetContent: PropTypes.string,
+					targetContentDir: PropTypes.string,
 				})
 			),
 			legend: PropTypes.string,
