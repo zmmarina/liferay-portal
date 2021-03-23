953ed60a141fd6dca3b8a25f30c54f7fde45a804
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2021-03-23 11:57:22 +0100

    COMMERCE-6179 headless-commerce-machine-learning-api Add sku-level forecasting headless model

---
+++ modules/dxp/apps/commerce/headless/headless-commerce-machine-learning-api/src/main/java/com/liferay/headless/commerce/machine/learning/dto/v1_0/SkuForecast.java
@@ -0,0 +1,458 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.headless.commerce.machine.learning.dto.v1_0;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.liferay.petra.function.UnsafeSupplier;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLField;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLName;
+import com.liferay.portal.vulcan.util.ObjectMapperUtil;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.io.Serializable;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.annotation.Generated;
+
+import javax.validation.Valid;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Riccardo Ferrari
+ * @generated
+ */
+@Generated("")
+@GraphQLName("SkuForecast")
+@JsonFilter("Liferay.Vulcan")
+@XmlRootElement(name = "SkuForecast")
+public class SkuForecast implements Serializable {
+
+	public static SkuForecast toDTO(String json) {
+		return ObjectMapperUtil.readValue(SkuForecast.class, json);
+	}
+
+	@Schema
+	@Valid
+	public Float getActual() {
+		return actual;
+	}
+
+	public void setActual(Float actual) {
+		this.actual = actual;
+	}
+
+	@JsonIgnore
+	public void setActual(
+		UnsafeSupplier<Float, Exception> actualUnsafeSupplier) {
+
+		try {
+			actual = actualUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Float actual;
+
+	@Schema
+	@Valid
+	public Float getForecast() {
+		return forecast;
+	}
+
+	public void setForecast(Float forecast) {
+		this.forecast = forecast;
+	}
+
+	@JsonIgnore
+	public void setForecast(
+		UnsafeSupplier<Float, Exception> forecastUnsafeSupplier) {
+
+		try {
+			forecast = forecastUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Float forecast;
+
+	@Schema
+	@Valid
+	public Float getForecastLowerBound() {
+		return forecastLowerBound;
+	}
+
+	public void setForecastLowerBound(Float forecastLowerBound) {
+		this.forecastLowerBound = forecastLowerBound;
+	}
+
+	@JsonIgnore
+	public void setForecastLowerBound(
+		UnsafeSupplier<Float, Exception> forecastLowerBoundUnsafeSupplier) {
+
+		try {
+			forecastLowerBound = forecastLowerBoundUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Float forecastLowerBound;
+
+	@Schema
+	@Valid
+	public Float getForecastUpperBound() {
+		return forecastUpperBound;
+	}
+
+	public void setForecastUpperBound(Float forecastUpperBound) {
+		this.forecastUpperBound = forecastUpperBound;
+	}
+
+	@JsonIgnore
+	public void setForecastUpperBound(
+		UnsafeSupplier<Float, Exception> forecastUpperBoundUnsafeSupplier) {
+
+		try {
+			forecastUpperBound = forecastUpperBoundUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Float forecastUpperBound;
+
+	@Schema
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	@JsonIgnore
+	public void setSku(UnsafeSupplier<String, Exception> skuUnsafeSupplier) {
+		try {
+			sku = skuUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected String sku;
+
+	@Schema
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	@JsonIgnore
+	public void setTimestamp(
+		UnsafeSupplier<Date, Exception> timestampUnsafeSupplier) {
+
+		try {
+			timestamp = timestampUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Date timestamp;
+
+	@Schema
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	@JsonIgnore
+	public void setUnit(UnsafeSupplier<String, Exception> unitUnsafeSupplier) {
+		try {
+			unit = unitUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected String unit;
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof SkuForecast)) {
+			return false;
+		}
+
+		SkuForecast skuForecast = (SkuForecast)object;
+
+		return Objects.equals(toString(), skuForecast.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		String string = toString();
+
+		return string.hashCode();
+	}
+
+	public String toString() {
+		StringBundler sb = new StringBundler();
+
+		sb.append("{");
+
+		DateFormat liferayToJSONDateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+		if (actual != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"actual\": ");
+
+			sb.append(actual);
+		}
+
+		if (forecast != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"forecast\": ");
+
+			sb.append(forecast);
+		}
+
+		if (forecastLowerBound != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"forecastLowerBound\": ");
+
+			sb.append(forecastLowerBound);
+		}
+
+		if (forecastUpperBound != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"forecastUpperBound\": ");
+
+			sb.append(forecastUpperBound);
+		}
+
+		if (sku != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"sku\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(sku));
+
+			sb.append("\"");
+		}
+
+		if (timestamp != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"timestamp\": ");
+
+			sb.append("\"");
+
+			sb.append(liferayToJSONDateFormat.format(timestamp));
+
+			sb.append("\"");
+		}
+
+		if (unit != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"unit\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(unit));
+
+			sb.append("\"");
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Schema(
+		accessMode = Schema.AccessMode.READ_ONLY,
+		defaultValue = "com.liferay.headless.commerce.machine.learning.dto.v1_0.SkuForecast",
+		name = "x-class-name"
+	)
+	public String xClassName;
+
+	private static String _escape(Object object) {
+		String string = String.valueOf(object);
+
+		return string.replaceAll("\"", "\\\\\"");
+	}
+
+	private static boolean _isArray(Object value) {
+		if (value == null) {
+			return false;
+		}
+
+		Class<?> clazz = value.getClass();
+
+		return clazz.isArray();
+	}
+
+	private static String _toJSON(Map<String, ?> map) {
+		StringBuilder sb = new StringBuilder("{");
+
+		@SuppressWarnings("unchecked")
+		Set set = map.entrySet();
+
+		@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, ?>> iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			Map.Entry<String, ?> entry = iterator.next();
+
+			sb.append("\"");
+			sb.append(entry.getKey());
+			sb.append("\": ");
+
+			Object value = entry.getValue();
+
+			if (_isArray(value)) {
+				sb.append("[");
+
+				Object[] valueArray = (Object[])value;
+
+				for (int i = 0; i < valueArray.length; i++) {
+					if (valueArray[i] instanceof String) {
+						sb.append("\"");
+						sb.append(valueArray[i]);
+						sb.append("\"");
+					}
+					else {
+						sb.append(valueArray[i]);
+					}
+
+					if ((i + 1) < valueArray.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
+			}
+			else if (value instanceof Map) {
+				sb.append(_toJSON((Map<String, ?>)value));
+			}
+			else if (value instanceof String) {
+				sb.append("\"");
+				sb.append(value);
+				sb.append("\"");
+			}
+			else {
+				sb.append(value);
+			}
+
+			if (iterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+}
