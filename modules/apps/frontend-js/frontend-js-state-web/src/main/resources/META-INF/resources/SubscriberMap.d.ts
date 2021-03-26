34c2bb12befaca6b5f5e2b0a709cc664826a7b60
Greg Hurrell <greg.hurrell@liferay.com>
2021-03-26 15:19:11 +0100

    LPS-129801 Commit ".d.ts" files and other type-related artifacts
    To make this happen:
    
    - Get rid of `outDir` (implying default of outputting in-place).
    - Commit the result of `yarn run tsc --emitDeclarationOnly` (this will
      get baked into `npm-scripts`).
    - Tweak `build.gradle` to keep junk out of JAR; as noted in the code
      comment, I've asked in Slack if there is a way to do this globally.
    
    What is the junk, specifically? A single file like "State.ts" can
    produce the following:
    
    - State.d.js
    - State.d.js.map
    - State.d.ts
    - State.js
    - State.js.map
    - State.ts
    
    Of those, the only two that are actually interesting are the ".js" and
    ".js.map" files. ".ts" is totally redundant with what's already in
    `src`, ".d.ts" is useful but intended for consumption by editors (and
    `tsc` itself) and not app servers, and ".d.js"/".d.js.map" are just
    totally pointless (corresponding as they do to type-info-only files with
    no implementations).
    
    Having said all that, this is just one possible way of doing this. We
    could also commit the type-related artifacts to a `types` directory
    alongside `src` and `test`. One think I like about that is it keeps
    `src` and `test` decluttered for people browsing the directory
    structure in their editors or on the command-line. The other is it
    doesn't require any messing with Gradle in order to fine-tune what goes
    into the JARs.

---
+++ modules/apps/frontend-js/frontend-js-state-web/src/main/resources/META-INF/resources/SubscriberMap.d.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+import type { Atom, Selector } from './State';
+import type { Immutable } from './types';
+/**
+ * `Map` wrapper for type safety. While a vanilla `Map` is totally adequate for
+ * our needs in terms of implementation, at the type-system level the declared
+ * type of `Map<K, V>` isn't expressive enough to capture what we want (which
+ * would be something like the following -- sadly, not valid TypeScript -- where
+ * "T" would be any arbitrary subtype of "unknown", and we don't care what the
+ * concrete type is, only that it is the same within each key/value pair).
+ */
+export default class SubscriberMap {
+    _id: number;
+    _subscribers: Map<Atom<unknown> | Selector<unknown>, Map<number, (value: unknown) => void>>;
+    constructor();
+    clear(): void;
+    getCallbacks<T>(atomOrSelector: Atom<T> | Selector<T>): Map<number, (value: Immutable<T>) => void>;
+    addCallback<T extends unknown>(atomOrSelector: Atom<T> | Selector<T>, callback: (value: Immutable<T>) => void): () => void;
+}
---
+++ modules/apps/frontend-js/frontend-js-state-web/src/main/resources/META-INF/resources/types.d.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+declare type Primitive = bigint | boolean | null | number | string | symbol | undefined;
+declare type Builtin = Date | Error | Function | Primitive | RegExp;
+/**
+ * A local "DeepReadonly" until TypeScript bundles one out of the box.
+ *
+ * See: https://github.com/microsoft/TypeScript/issues/13923
+ */
+export declare type Immutable<T> = T extends Builtin ? T : T extends Map<infer K, infer V> ? ReadonlyMap<Immutable<K>, Immutable<V>> : T extends ReadonlyMap<infer K, infer V> ? ReadonlyMap<Immutable<K>, Immutable<V>> : T extends WeakMap<infer K, infer V> ? WeakMap<Immutable<K>, Immutable<V>> : T extends Set<infer U> ? ReadonlySet<Immutable<U>> : T extends ReadonlySet<infer U> ? ReadonlySet<Immutable<U>> : T extends WeakSet<infer U> ? WeakSet<Immutable<U>> : T extends Promise<infer U> ? Promise<Immutable<U>> : T extends {} ? {
+    readonly [K in keyof T]: Immutable<T[K]>;
+} : Readonly<T>;
+export {};
