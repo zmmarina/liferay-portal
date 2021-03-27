da8b117e457d1e690991d91692d5073c52102a18
Javier Gamarra <nhpatt@gmail.com>
2021-03-27 19:47:31 +0100

    LPS-128484 pass siteId and implement the other tests

--- modules/apps/headless/headless-admin-content/headless-admin-content-test/src/testIntegration/java/com/liferay/headless/admin/content/resource/v1_0/test/DisplayPageTemplateResourceTest.java
+++ modules/apps/headless/headless-admin-content/headless-admin-content-test/src/testIntegration/java/com/liferay/headless/admin/content/resource/v1_0/test/DisplayPageTemplateResourceTest.java
@@ -20,12 +20,19 @@ import com.liferay.headless.admin.content.client.dto.v1_0.DisplayPageTemplate;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.test.rule.Inject;
+
+import java.util.Collection;
+import java.util.Collections;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -42,7 +49,7 @@ public class DisplayPageTemplateResourceTest
 	@Test
 	public void testGetSiteDisplayPageTemplate() throws Exception {
 		LayoutPageTemplateEntry layoutPageTemplateEntry =
-			_getLayoutPageTemplateEntry();
+			_getLayoutPageTemplateEntry(testGroup.getGroupId());
 
 		Assert.assertNotNull(
 			displayPageTemplateResource.getSiteDisplayPageTemplate(
@@ -57,7 +64,7 @@ public class DisplayPageTemplateResourceTest
 		throws Exception {
 
 		LayoutPageTemplateEntry layoutPageTemplateEntry =
-			_getLayoutPageTemplateEntry();
+			_getLayoutPageTemplateEntry(siteId);
 
 		displayPageTemplate.setDateCreated(
 			layoutPageTemplateEntry.getCreateDate());
@@ -68,17 +75,60 @@ public class DisplayPageTemplateResourceTest
 		displayPageTemplate.setMarkedAsDefault(
 			layoutPageTemplateEntry.isDefaultTemplate());
 
-		displayPageTemplate.setSiteId(testGroup.getGroupId());
+		displayPageTemplate.setSiteId(siteId);
 		displayPageTemplate.setTitle(layoutPageTemplateEntry.getName());
 
 		return displayPageTemplate;
 	}
 
-	private LayoutPageTemplateEntry _getLayoutPageTemplateEntry()
+	@Override
+	@Test
+	public void testGraphQLGetSiteDisplayPageTemplatesPage() throws Exception {
+		Long siteId = testGetSiteDisplayPageTemplatesPage_getSiteId();
+
+		GraphQLField graphQLField = new GraphQLField(
+			"admin",
+			new GraphQLField(
+				"displayPageTemplates",
+				HashMapBuilder.<String, Object>put(
+					"page", 1
+				).put(
+					"pageSize", 2
+				).put(
+					"siteKey", "\"" + siteId + "\""
+				).build(),
+				new GraphQLField("items", getGraphQLFields()),
+				new GraphQLField("page"), new GraphQLField("totalCount")));
+
+		JSONObject contentStructuresJSONObject = JSONUtil.getValueAsJSONObject(
+			invokeGraphQLQuery(graphQLField), "JSONObject/data",
+			"JSONObject/admin", "JSONObject/displayPageTemplates");
+
+		Assert.assertEquals(0, contentStructuresJSONObject.get("totalCount"));
+
+		testGetSiteDisplayPageTemplatesPage_addDisplayPageTemplate(
+			siteId, randomDisplayPageTemplate());
+
+		contentStructuresJSONObject = JSONUtil.getValueAsJSONObject(
+			invokeGraphQLQuery(graphQLField), "JSONObject/data",
+			"JSONObject/admin", "JSONObject/displayPageTemplates");
+
+		Assert.assertEquals(1, contentStructuresJSONObject.get("totalCount"));
+	}
+
+	@Override
+	protected Collection<EntityField> getEntityFields() {
+		return Collections.singleton(
+			new EntityField(
+				"title", EntityField.Type.STRING, o -> null, o -> null,
+				o -> null));
+	}
+
+	private LayoutPageTemplateEntry _getLayoutPageTemplateEntry(Long siteId)
 		throws Exception {
 
 		return _layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
-			TestPropsValues.getUserId(), testGroup.getGroupId(), 0,
+			TestPropsValues.getUserId(), siteId, 0,
 			_portal.getClassNameId(BlogsEntry.class.getName()), 0,
 			RandomTestUtil.randomString(),
 			LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE, 0, true, 0,
