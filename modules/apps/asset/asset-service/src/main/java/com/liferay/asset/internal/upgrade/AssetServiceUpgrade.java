daf831c0231a7abfdd5eb69696930654021f283f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-03-29 09:34:35 +0100

    LPS-125952 Uses UpgradeModules instead of DummyUpgradeStep, since we want customers coming from old versions where this module did not exist execute the whole list of upgrade processes

--- modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/upgrade/AssetServiceUpgrade.java
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/upgrade/AssetServiceUpgrade.java
@@ -35,8 +35,6 @@ public class AssetServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
-		registry.register("0.0.0", "0.0.1", new DummyUpgradeStep());
-
 		registry.register(
 			"0.0.1", "1.0.0", new AssetEntryUsageUpgradeProcess());
 
