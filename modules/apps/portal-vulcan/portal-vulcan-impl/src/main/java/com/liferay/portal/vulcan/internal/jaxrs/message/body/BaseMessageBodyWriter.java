cbe699273f8d59322ed899f751b900d753692a78
Javier Gamarra <nhpatt@gmail.com>
2021-03-30 10:27:21 +0200

    LPS-129710 add filter just for the request instead of mutating the ObjectMapper

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/message/body/BaseMessageBodyWriter.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/message/body/BaseMessageBodyWriter.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.vulcan.internal.jaxrs.message.body;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.PropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -93,38 +92,13 @@ public abstract class BaseMessageBodyWriter
 
 		objectMapper.registerModule(simpleModule);
 
-		ObjectWriter objectWriter = objectMapper.writerFor(
-			objectMapper.constructType(genericType));
-
-		objectWriter.writeValue(outputStream, object);
+		objectMapper.writer(
+			_getSimpleFilterProvider()
+		).writeValue(
+			outputStream, object
+		);
 
 		outputStream.flush();
-	}
-
-	private ObjectMapper _addFilter(ObjectMapper objectMapper) {
-		objectMapper.setFilterProvider(
-			new SimpleFilterProvider() {
-				{
-					PropertyFilter propertyFilter = null;
-
-					Set<String> fieldNames = _fieldsQueryParam.getFieldNames();
-					Set<String> restrictFieldNames =
-						_restrictFieldsQueryParam.getRestrictFieldNames();
-
-					if ((fieldNames == null) && (restrictFieldNames == null)) {
-						propertyFilter =
-							SimpleBeanPropertyFilter.serializeAll();
-					}
-					else {
-						propertyFilter = VulcanPropertyFilter.of(
-							fieldNames, restrictFieldNames);
-					}
-
-					addFilter("Liferay.Vulcan", propertyFilter);
-				}
-			});
-
-		return objectMapper;
 	}
 
 	private ObjectMapper _getObjectMapper(Class<?> clazz) {
@@ -132,12 +106,32 @@ public abstract class BaseMessageBodyWriter
 			_providers.getContextResolver(_contextType, _mediaType)
 		).map(
 			contextResolver -> contextResolver.getContext(clazz)
-		).map(
-			this::_addFilter
 		).orElseThrow(
 			() -> new InternalServerErrorException(
 				"Unable to generate object mapper for class " + clazz)
 		);
+	}
+
+	private SimpleFilterProvider _getSimpleFilterProvider() {
+		return new SimpleFilterProvider() {
+			{
+				PropertyFilter propertyFilter = null;
+
+				Set<String> fieldNames = _fieldsQueryParam.getFieldNames();
+				Set<String> restrictFieldNames =
+					_restrictFieldsQueryParam.getRestrictFieldNames();
+
+				if ((fieldNames == null) && (restrictFieldNames == null)) {
+					propertyFilter = SimpleBeanPropertyFilter.serializeAll();
+				}
+				else {
+					propertyFilter = VulcanPropertyFilter.of(
+						fieldNames, restrictFieldNames);
+				}
+
+				addFilter("Liferay.Vulcan", propertyFilter);
+			}
+		};
 	}
 
 	private final Class<? extends ObjectMapper> _contextType;
