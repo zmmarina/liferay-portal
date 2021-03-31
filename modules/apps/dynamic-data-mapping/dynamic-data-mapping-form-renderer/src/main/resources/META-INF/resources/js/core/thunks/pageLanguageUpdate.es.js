297c64e9db3c1cb93bb2b8d12fcae236840dd15c
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2021-03-31 12:43:05 -0300

    LPS-129877 Assign default values to localizedValueEdited in case it is undefined

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/core/thunks/pageLanguageUpdate.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/core/thunks/pageLanguageUpdate.es.js
@@ -56,9 +56,11 @@ const getDataRecordValues = ({
 const getFieldProperties = (fieldName, pages) => {
 	const visitor = new PagesVisitor(pages);
 
-	const {itemSelectorURL, localizedValueEdited} = visitor.findField(
-		(field) => field.fieldName === fieldName
-	);
+	const {
+		itemSelectorURL,
+		locale,
+		localizedValueEdited = {[locale]: true},
+	} = visitor.findField((field) => field.fieldName === fieldName);
 
 	return {itemSelectorURL, localizedValueEdited};
 };
