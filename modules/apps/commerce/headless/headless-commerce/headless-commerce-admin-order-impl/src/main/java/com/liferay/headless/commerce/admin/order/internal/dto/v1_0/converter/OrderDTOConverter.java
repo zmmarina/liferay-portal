e8efda4f5c036ea2aedd144187447783594fccc6
Riccardo <riccardo.alberti@liferay.com>
2021-04-01 14:14:53 +0200

    COMMERCE-6398 Get orderStatus label when no mapping is available

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/OrderDTOConverter.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/OrderDTOConverter.java
@@ -21,6 +21,8 @@ import com.liferay.commerce.currency.model.CommerceMoney;
 import com.liferay.commerce.currency.util.CommercePriceFormatter;
 import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.model.CommerceShippingMethod;
+import com.liferay.commerce.order.status.CommerceOrderStatus;
+import com.liferay.commerce.order.status.CommerceOrderStatusRegistry;
 import com.liferay.commerce.product.model.CommerceChannel;
 import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.commerce.service.CommerceOrderService;
@@ -28,6 +30,7 @@ import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.Order;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.Status;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.language.LanguageResources;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -74,14 +77,11 @@ public class OrderDTOConverter implements DTOConverter<CommerceOrder, Order> {
 		ResourceBundle resourceBundle = LanguageResources.getResourceBundle(
 			locale);
 
-		String commerceOrderStatusLabel =
-			CommerceOrderConstants.getOrderStatusLabel(
-				commerceOrder.getOrderStatus());
+		String commerceOrderStatusLabel = _getCommerceOrderStatusLabel(
+			commerceOrder.getOrderStatus(), locale);
 
-		String commerceOrderStatusLabelI18n = LanguageUtil.get(
-			resourceBundle,
-			CommerceOrderConstants.getOrderStatusLabel(
-				commerceOrder.getOrderStatus()));
+		String commerceOrderStatusLabelI18n = _getCommerceOrderStatusLabelI18n(
+			commerceOrder.getOrderStatus(), locale);
 
 		String commerceOrderWorkflowStatusLabel =
 			WorkflowConstants.getStatusLabel(commerceOrder.getStatus());
@@ -176,6 +176,39 @@ public class OrderDTOConverter implements DTOConverter<CommerceOrder, Order> {
 		}
 
 		return _commercePriceFormatter.format(commerceCurrency, price, locale);
+	}
+
+	private String _getCommerceOrderStatusLabel(
+		int orderStatus, Locale locale) {
+
+		String commerceOrderStatusLabel =
+			CommerceOrderConstants.getOrderStatusLabel(orderStatus);
+
+		if (!Validator.isBlank(commerceOrderStatusLabel)) {
+			return commerceOrderStatusLabel;
+		}
+
+		CommerceOrderStatus commerceOrderStatus =
+			_commerceOrderStatusRegistry.getCommerceOrderStatus(orderStatus);
+
+		if (commerceOrderStatus != null) {
+			return commerceOrderStatus.getLabel(locale);
+		}
+
+		return commerceOrderStatusLabel;
+	}
+
+	private String _getCommerceOrderStatusLabelI18n(
+		int orderStatus, Locale locale) {
+
+		String commerceOrderStatusLabelI18n = LanguageUtil.get(
+			locale, CommerceOrderConstants.getOrderStatusLabel(orderStatus));
+
+		if (!Validator.isBlank(commerceOrderStatusLabelI18n)) {
+			return commerceOrderStatusLabelI18n;
+		}
+
+		return _getCommerceOrderStatusLabel(orderStatus, locale);
 	}
 
 	private double _getDoubleValue(BigDecimal amount) {
@@ -496,6 +529,9 @@ public class OrderDTOConverter implements DTOConverter<CommerceOrder, Order> {
 
 	@Reference
 	private CommerceOrderService _commerceOrderService;
+
+	@Reference
+	private CommerceOrderStatusRegistry _commerceOrderStatusRegistry;
 
 	@Reference
 	private CommercePriceFormatter _commercePriceFormatter;
