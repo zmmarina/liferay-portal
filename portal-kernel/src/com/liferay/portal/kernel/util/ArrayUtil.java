9e6252e77624b57bf0e2cc6ffb2f576c89275de8
tina.tian <tina.tian@liferay.com>
2021-04-02 09:00:40 -0700

    LPS-130041 Add null checking, make it consistent with ListUtil.toString()

--- portal-kernel/src/com/liferay/portal/kernel/util/ArrayUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ArrayUtil.java
@@ -2081,7 +2081,14 @@ public class ArrayUtil {
 		for (int i = 0; i < array.length; i++) {
 			Object bean = array[i];
 
-			Object value = BeanPropertiesUtil.getObject(bean, param);
+			Object value = null;
+
+			if (Validator.isNull(param)) {
+				value = String.valueOf(bean);
+			}
+			else {
+				value = BeanPropertiesUtil.getObject(bean, param);
+			}
 
 			if (value != null) {
 				if (locale != null) {
