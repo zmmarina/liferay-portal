4768a5a4194040614c23d6687faf374f77df3055
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-04-06 18:39:59 +0100

    LPS-102711 Moves documents and media to its own screen

---
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portal/settings/configuration/admin/display/DocumentsAndMediaSiteSettingsConfigurationScreen.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.admin.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.configuration.admin.display.ConfigurationScreen;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = ConfigurationScreen.class)
+public class DocumentsAndMediaSiteSettingsConfigurationScreen
+	implements ConfigurationScreen {
+
+	@Override
+	public String getCategoryKey() {
+		return "documents-and-media";
+	}
+
+	@Override
+	public String getKey() {
+		return "site-configuration-documents-and-media";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return LanguageUtil.get(locale, "documents-and-media");
+	}
+
+	@Override
+	public String getScope() {
+		return ExtendedObjectClassDefinition.Scope.GROUP.getValue();
+	}
+
+	@Override
+	public boolean isVisible() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		Group siteGroup = themeDisplay.getSiteGroup();
+
+		if ((siteGroup == null) || siteGroup.isCompany()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		try {
+			RequestDispatcher requestDispatcher =
+				_servletContext.getRequestDispatcher(
+					"/edit_documents_and_media.jsp");
+
+			requestDispatcher.include(httpServletRequest, httpServletResponse);
+		}
+		catch (Exception exception) {
+			throw new IOException(
+				"Unable to render edit_documents_and_media.jsp", exception);
+		}
+	}
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.site.admin.web)")
+	private ServletContext _servletContext;
+
+}
---
+++ modules/apps/site/site-admin-web/src/main/resources/META-INF/resources/edit_documents_and_media.jsp
@@ -0,0 +1,53 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+Group siteGroup = themeDisplay.getSiteGroup();
+
+Group liveGroup = null;
+
+if (siteGroup.isStagingGroup()) {
+	liveGroup = siteGroup.getLiveGroup();
+}
+else {
+	liveGroup = siteGroup;
+}
+%>
+
+<portlet:actionURL name="/site_admin/edit_documents_and_media" var="editDocumentsAndMediaURL">
+	<portlet:param name="mvcPath" value="/edit_documents_and_media.jsp" />
+</portlet:actionURL>
+
+<liferay-frontend:edit-form
+	action="<%= editDocumentsAndMediaURL %>"
+	method="post"
+	name="fm"
+>
+	<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+	<aui:input name="liveGroupId" type="hidden" value="<%= liveGroup.getGroupId() %>" />
+
+	<liferay-frontend:edit-form-body>
+		<aui:input helpMessage='<%= LanguageUtil.format(request, "directory-indexing-help", new Object[] {HtmlUtil.escape(siteGroup.getDescriptiveName(themeDisplay.getLocale())), themeDisplay.getPortalURL() + "/documents" + siteGroup.getFriendlyURL()}, false) %>' inlineLabel="right" label="enable-directory-indexing" labelCssClass="simple-toggle-switch" name="TypeSettingsProperties--directoryIndexingEnabled--" type="toggle-switch" value='<%= PropertiesParamUtil.getBoolean(liveGroup.getTypeSettingsProperties(), request, "directoryIndexingEnabled") %>' />
+	</liferay-frontend:edit-form-body>
+
+	<liferay-frontend:edit-form-footer>
+		<aui:button type="submit" />
+
+		<aui:button href='<%= ParamUtil.getString(request, "redirect") %>' type="cancel" />
+	</liferay-frontend:edit-form-footer>
+</liferay-frontend:edit-form>
