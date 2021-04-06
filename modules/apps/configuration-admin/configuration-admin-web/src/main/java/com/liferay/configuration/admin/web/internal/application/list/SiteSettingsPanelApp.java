eee65ede11b1f8c4d8b31d604c910601db33b3d9
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-04-06 08:55:03 +0100

    LPS-129860 Put portlet at the beginning of the list

--- modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/application/list/SiteSettingsPanelApp.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/application/list/SiteSettingsPanelApp.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.app.order:Integer=210",
+		"panel.app.order:Integer=100",
 		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONFIGURATION
 	},
 	service = PanelApp.class
