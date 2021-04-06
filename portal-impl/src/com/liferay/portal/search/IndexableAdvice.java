368a8e1b72b083bb59c4c0fbdb39827015d73ce9
drewbrokke <drew.brokke@liferay.com>
2021-04-06 17:05:58 -0500

    LPS-130001 liferay-portal: IndexableAdvice: revert change from 89a1b0dce665

--- portal-impl/src/com/liferay/portal/search/IndexableAdvice.java
+++ portal-impl/src/com/liferay/portal/search/IndexableAdvice.java
@@ -14,16 +14,13 @@
 
 package com.liferay.portal.search;
 
-import com.liferay.petra.lang.SafeClosable;
 import com.liferay.portal.kernel.aop.AopMethodInvocation;
 import com.liferay.portal.kernel.aop.ChainableMethodAdvice;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.proxy.ProxyModeThreadLocal;
 import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
 import com.liferay.portal.kernel.search.Indexable;
-import com.liferay.portal.kernel.search.IndexableThreadLocal;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
@@ -126,16 +123,11 @@ public class IndexableAdvice extends ChainableMethodAdvice {
 			}
 		}
 
-		try (SafeClosable safeClosable =
-				ProxyModeThreadLocal.setWithSafeClosable(
-					IndexableThreadLocal.isForceSync())) {
-
-			if (indexableContext._indexableType == IndexableType.DELETE) {
-				indexer.delete(result);
-			}
-			else {
-				indexer.reindex(result);
-			}
+		if (indexableContext._indexableType == IndexableType.DELETE) {
+			indexer.delete(result);
+		}
+		else {
+			indexer.reindex(result);
 		}
 	}
 
