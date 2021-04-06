d273531a34592a304c01a55336b0f7ce33290773
Eduardo Garcia <eduardo.garcia@liferay.com>
2021-04-06 11:51:50 +0200

    LPS-129316 Add a fragment collection for cookie consent banners, with a first simple banner

---
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-cookie-banners/src/main/java/com/liferay/fragment/collection/contributor/cookie/banners/CookieBannersFragmentCollectionContributor.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.collection.contributor.cookie.banners;
+
+import com.liferay.fragment.contributor.BaseFragmentCollectionContributor;
+import com.liferay.fragment.contributor.FragmentCollectionContributor;
+import com.liferay.fragment.contributor.PortletAliasRegistration;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eduardo García
+ */
+@Component(
+	property = "fragment.collection.key=COOKIE_BANNERS",
+	service = FragmentCollectionContributor.class
+)
+public class CookieBannersFragmentCollectionContributor
+	extends BaseFragmentCollectionContributor {
+
+	@Override
+	public String getFragmentCollectionKey() {
+		return "COOKIE_BANNERS";
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(
+		target = "(com.liferay.fragment.entry.processor.portlet.alias=nav)"
+	)
+	private PortletAliasRegistration _portletAliasRegistration;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.fragment.collection.contributor.cookie.banners)"
+	)
+	private ServletContext _servletContext;
+
+}
