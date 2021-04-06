843b7b9e14fc7c832755d355fc5bc5c492aac7fd
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-04-06 14:32:18 +0200

    LPS-130097 New method that avoid uses InfoItemFieldValues instead of a map. This will improve performance since the values will not be calculated at once, only when needed

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-api/src/main/java/com/liferay/fragment/entry/processor/helper/FragmentEntryProcessorHelper.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-api/src/main/java/com/liferay/fragment/entry/processor/helper/FragmentEntryProcessorHelper.java
@@ -17,10 +17,12 @@ package com.liferay.fragment.entry.processor.helper;
 import aQute.bnd.annotation.ProviderType;
 
 import com.liferay.fragment.processor.FragmentEntryProcessorContext;
+import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.type.WebImage;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -60,11 +62,64 @@ public interface FragmentEntryProcessorHelper {
 			FragmentEntryProcessorContext fragmentEntryProcessorContext)
 		throws PortalException;
 
+	public default Object getMappedInfoItemFieldValue(
+			JSONObject jsonObject,
+			Map<Long, InfoItemFieldValues> infoItemFieldValuesMap,
+			FragmentEntryProcessorContext fragmentEntryProcessorContext)
+		throws PortalException {
+
+		Map<Long, Map<String, Object>> infoDisplaysFieldValuesMap =
+			new HashMap<>();
+
+		for (Map.Entry<Long, InfoItemFieldValues> entry :
+				infoItemFieldValuesMap.entrySet()) {
+
+			InfoItemFieldValues infoItemFieldValues = entry.getValue();
+
+			infoDisplaysFieldValuesMap.put(
+				entry.getKey(),
+				infoItemFieldValues.getMap(
+					fragmentEntryProcessorContext.getLocale()));
+		}
+
+		return getMappedValue(
+			jsonObject, infoDisplaysFieldValuesMap,
+			fragmentEntryProcessorContext);
+	}
+
+	public default Object getMappedInfoItemFieldValue(
+			JSONObject jsonObject,
+			Map<Long, InfoItemFieldValues> infoItemFieldValuesMap, String mode,
+			Locale locale, long previewClassPK, long previewClassNameId,
+			int previewType)
+		throws PortalException {
+
+		Map<Long, Map<String, Object>> infoDisplaysFieldValuesMap =
+			new HashMap<>();
+
+		for (Map.Entry<Long, InfoItemFieldValues> entry :
+				infoItemFieldValuesMap.entrySet()) {
+
+			InfoItemFieldValues infoItemFieldValues = entry.getValue();
+
+			infoDisplaysFieldValuesMap.put(
+				entry.getKey(), infoItemFieldValues.getMap(locale));
+		}
+
+		return getMappedValue(
+			jsonObject, infoDisplaysFieldValuesMap, mode, locale,
+			previewClassPK, previewClassNameId, previewType);
+	}
+
 	public Object getMappedLayoutValue(
 			JSONObject jsonObject,
 			FragmentEntryProcessorContext fragmentEntryProcessorContext)
 		throws PortalException;
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link #getMappedInfoItemFieldValue(JSONObject, Map, FragmentEntryProcessorContext)}
+	 */
+	@Deprecated
 	public Object getMappedValue(
 			JSONObject jsonObject,
 			Map<Long, Map<String, Object>> infoDisplaysFieldValues,
@@ -82,6 +137,10 @@ public interface FragmentEntryProcessorHelper {
 			0, previewType);
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link #getMappedInfoItemFieldValue(JSONObject, Map, String, Locale, long, long, int)}
+	 */
+	@Deprecated
 	public Object getMappedValue(
 			JSONObject jsonObject,
 			Map<Long, Map<String, Object>> infoDisplaysFieldValues, String mode,
