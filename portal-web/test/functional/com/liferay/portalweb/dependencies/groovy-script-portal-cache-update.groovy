0d2e959c5fdf3e2e9f4aaa0a381b6e54d4b963ea
Koor Gai <koor.gai@liferay.com>
2021-04-06 13:33:09 +0300

    LRQA-42945 Groovy scrips

---
+++ portal-web/test/functional/com/liferay/portalweb/dependencies/groovy-script-portal-cache-update.groovy
@@ -0,0 +1,7 @@
+import com.liferay.portal.kernel.cache.PortalCache;
+import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
+import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
+
+PortalCache<String, String> testPortalCache = PortalCacheHelperUtil.getPortalCache(PortalCacheManagerNames.MULTI_VM, "test.cache");
+
+testPortalCache.put("test.key", "test.value.update");
