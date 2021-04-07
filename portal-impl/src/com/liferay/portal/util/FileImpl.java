49b10c845e39dba596eac7a816522459ebab1107
Jorge Díaz <jorge.diaz@liferay.com>
2021-04-07 19:41:20 +0200

    LPS-130154 Handle the org.apache.tika.sax.WriteOutContentHandler exception. We have to get the parsed text in that case.

--- portal-impl/src/com/liferay/portal/util/FileImpl.java
+++ portal-impl/src/com/liferay/portal/util/FileImpl.java
@@ -1157,7 +1157,7 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 
 	private static String _parseToString(
 			Tika tika, TikaInputStream tikaInputStream)
-		throws IOException, SAXException, TikaException {
+		throws IOException, TikaException {
 
 		UniversalEncodingDetector universalEncodingDetector =
 			new UniversalEncodingDetector();
@@ -1214,6 +1214,12 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 			parser.parse(
 				tikaInputStream, new BodyContentHandler(writeOutContentHandler),
 				metadata, parseContext);
+		}
+		catch (SAXException saxException) {
+			if (!writeOutContentHandler.isWriteLimitReached(saxException)) {
+				throw new TikaException(
+					"Unexpected SAX processing failure", saxException);
+			}
 		}
 		finally {
 			tikaInputStream.close();
