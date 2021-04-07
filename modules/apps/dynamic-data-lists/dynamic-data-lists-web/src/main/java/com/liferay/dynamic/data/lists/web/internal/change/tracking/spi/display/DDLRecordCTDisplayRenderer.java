2f3b1a95c8d39fcb6161d2943421b85ddaa3c291
Preston-Crary <preston.crary@liferay.com>
2021-04-07 11:42:51 -0700

    LPS-130075 Show content for DDLRecords, see view_record.jsp.

--- modules/apps/dynamic-data-lists/dynamic-data-lists-web/src/main/java/com/liferay/dynamic/data/lists/web/internal/change/tracking/spi/display/DDLRecordCTDisplayRenderer.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-web/src/main/java/com/liferay/dynamic/data/lists/web/internal/change/tracking/spi/display/DDLRecordCTDisplayRenderer.java
@@ -16,15 +16,23 @@ package com.liferay.dynamic.data.lists.web.internal.change.tracking.spi.display;
 
 import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
 import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
+import com.liferay.change.tracking.spi.display.context.DisplayContext;
 import com.liferay.dynamic.data.lists.model.DDLRecord;
-import com.liferay.dynamic.data.mapping.io.DDMFormSerializer;
-import com.liferay.dynamic.data.mapping.io.DDMFormSerializerSerializeRequest;
-import com.liferay.dynamic.data.mapping.io.DDMFormSerializerSerializeResponse;
-import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.taglib.servlet.taglib.HTMLTag;
+import com.liferay.petra.io.unsync.UnsyncStringWriter;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.taglib.servlet.PipingServletResponse;
 
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -42,9 +50,7 @@ public class DDLRecordCTDisplayRenderer
 	}
 
 	@Override
-	public String getTitle(Locale locale, DDLRecord ddlRecord)
-		throws PortalException {
-
+	public String getTitle(Locale locale, DDLRecord ddlRecord) {
 		return String.valueOf(ddlRecord.getPrimaryKey());
 	}
 
@@ -53,6 +59,8 @@ public class DDLRecordCTDisplayRenderer
 		throws PortalException {
 
 		DDLRecord ddlRecord = displayBuilder.getModel();
+
+		DisplayContext<?> displayContext = displayBuilder.getDisplayContext();
 
 		displayBuilder.display(
 			"created-by",
@@ -66,41 +74,56 @@ public class DDLRecordCTDisplayRenderer
 				return null;
 			}
 		).display(
-			"create-date", ddlRecord.getCreateDate()
-		).display(
-			"last-modified", ddlRecord.getModifiedDate()
-		).display(
 			"version", ddlRecord.getVersion()
 		).display(
-			"cached", ddlRecord.isCachedModel()
-		).display(
-			"ddl-record-content-json", _serializeJSONDDLRecord(ddlRecord), false
+			"content",
+			_getContent(
+				displayContext.getHttpServletRequest(),
+				displayContext.getHttpServletResponse(), ddlRecord,
+				displayBuilder.getLocale()),
+			false
 		);
 	}
 
-	/**
-	 * Serializes a DDLRecord into a JSON String representation of its contents.
-	 *
-	 * @param ddlRecord the DDLRecord to be serialized
-	 * @return JSON String of the given DDLRecord's contents
-	 * @throws PortalException if the DDMFormValues couldn't be retrieved from the DDLRecord
-	 */
-	private String _serializeJSONDDLRecord(DDLRecord ddlRecord)
+	private String _getContent(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, DDLRecord ddlRecord,
+			Locale locale)
 		throws PortalException {
 
-		DDMFormValues ddmFormValues = ddlRecord.getDDMFormValues();
+		DDLRecordSet ddlRecordSet = ddlRecord.getRecordSet();
 
-		DDMFormSerializerSerializeRequest.Builder builder =
-			DDMFormSerializerSerializeRequest.Builder.newBuilder(
-				ddmFormValues.getDDMForm());
+		HTMLTag htmlTag = new HTMLTag();
 
-		DDMFormSerializerSerializeResponse ddmFormSerializerSerializeResponse =
-			_ddmFormSerializer.serialize(builder.build());
+		htmlTag.setClassNameId(
+			_classNameLocalService.getClassNameId(DDMStructure.class));
+		htmlTag.setClassPK(ddlRecordSet.getDDMStructureId());
+		htmlTag.setDdmFormValues(ddlRecord.getDDMFormValues());
+		htmlTag.setGroupId(ddlRecord.getGroupId());
+		htmlTag.setReadOnly(true);
+		htmlTag.setRequestedLocale(locale);
 
-		return ddmFormSerializerSerializeResponse.getContent();
+		try (UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter()) {
+			htmlTag.doTag(
+				httpServletRequest,
+				new PipingServletResponse(
+					httpServletResponse, unsyncStringWriter));
+
+			return unsyncStringWriter.toString();
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(exception, exception);
+			}
+		}
+
+		return null;
 	}
 
-	@Reference(target = "(ddm.form.serializer.type=json)")
-	private DDMFormSerializer _ddmFormSerializer;
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDLRecordCTDisplayRenderer.class);
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
 
 }
