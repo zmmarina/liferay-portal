cd48577dc67ff9188e66dab0b77686848c951bde
Brian Chan <brian.chan@liferay.com>
2021-04-09 06:54:20 -0300

    LRQA-42945 rename

--- portal-web/test/functional/com/liferay/portalweb/dependencies/groovy-script-portal-cache-put.groovy
+++ portal-web/test/functional/com/liferay/portalweb/dependencies/groovy-script-portal-cache-put.groovy
@@ -2,6 +2,6 @@ import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
 import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
 
-PortalCache<String, String> testPortalCache = PortalCacheHelperUtil.getPortalCache(PortalCacheManagerNames.MULTI_VM, "test.cache");
+PortalCache<String, String> portalCache = PortalCacheHelperUtil.getPortalCache(PortalCacheManagerNames.MULTI_VM, "test.cache");
 
-testPortalCache.put("test.key", "test.value");
+portalCache.put("test.key", "test.value");
--- portal-web/test/functional/com/liferay/portalweb/dependencies/groovy-script-portal-cache-remove-all.groovy
+++ portal-web/test/functional/com/liferay/portalweb/dependencies/groovy-script-portal-cache-remove-all.groovy
@@ -2,6 +2,6 @@ import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
 import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
 
-PortalCache<String, String> testPortalCache = PortalCacheHelperUtil.getPortalCache(PortalCacheManagerNames.MULTI_VM, "test.cache");
+PortalCache<String, String> portalCache = PortalCacheHelperUtil.getPortalCache(PortalCacheManagerNames.MULTI_VM, "test.cache");
 
-testPortalCache.removeAll();
+portalCache.removeAll();
