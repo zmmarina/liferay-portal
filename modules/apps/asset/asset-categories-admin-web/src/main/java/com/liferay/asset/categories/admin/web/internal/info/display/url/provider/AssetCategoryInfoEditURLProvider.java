96cde4ac029fad1a6078889bdd0e76ac4cd7f1c7
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-04-09 11:20:25 +0200

    LPS-130143 Adds edit URL provider, so that the user can edit the category from the display page template

---
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/display/url/provider/AssetCategoryInfoEditURLProvider.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.info.display.url.provider;
+
+import com.liferay.asset.categories.admin.web.constants.AssetCategoriesAdminPortletKeys;
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.info.display.url.provider.InfoEditURLProvider;
+import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	property = "model.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = InfoEditURLProvider.class
+)
+public class AssetCategoryInfoEditURLProvider
+	implements InfoEditURLProvider<AssetCategory> {
+
+	@Override
+	public String getURL(
+		AssetCategory assetCategory, HttpServletRequest httpServletRequest) {
+
+		Group group = _groupLocalService.fetchGroup(assetCategory.getGroupId());
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		if (group.isCompany()) {
+			group = themeDisplay.getScopeGroup();
+		}
+
+		PortletURL portletURL = PortletURLBuilder.create(
+			_portal.getControlPanelPortletURL(
+				httpServletRequest, group,
+				AssetCategoriesAdminPortletKeys.ASSET_CATEGORIES_ADMIN, 0, 0,
+				PortletRequest.RENDER_PHASE)
+		).setMVCPath(
+			"/edit_category.jsp"
+		).setParameter(
+			"redirect",
+			() -> {
+				String redirect = ParamUtil.getString(
+					httpServletRequest, "redirect");
+
+				if (Validator.isNotNull(redirect)) {
+					return redirect;
+				}
+
+				return themeDisplay.getURLCurrent();
+			}
+		).setParameter(
+			"categoryId", String.valueOf(assetCategory.getCategoryId())
+		).setParameter(
+			"vocabularyId", String.valueOf(assetCategory.getVocabularyId())
+		).build();
+
+		return portletURL.toString();
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
