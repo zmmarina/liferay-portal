75aff230f2b12cb78d2cb8601aa19bd13f05439e
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-04-09 09:21:42 +0200

    LPS-130143 Describe the fields of the entity that will be available for mapping to the user

---
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/item/AssetCategoryInfoItemFields.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.info.item;
+
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.localized.InfoLocalizedValue;
+
+/**
+ * @author Jürgen Kappler
+ */
+public interface AssetCategoryInfoItemFields {
+
+	public static final InfoField<TextInfoFieldType> descriptionInfoField =
+		InfoField.builder(
+		).infoFieldType(
+			TextInfoFieldType.INSTANCE
+		).name(
+			"description"
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(
+				AssetCategoryInfoItemFields.class, "description")
+		).build();
+	public static final InfoField<TextInfoFieldType> nameInfoField =
+		InfoField.builder(
+		).infoFieldType(
+			TextInfoFieldType.INSTANCE
+		).name(
+			"name"
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(
+				AssetCategoryInfoItemFields.class, "name")
+		).build();
+	public static final InfoField<TextInfoFieldType> vocabularyInfoField =
+		InfoField.builder(
+		).infoFieldType(
+			TextInfoFieldType.INSTANCE
+		).name(
+			"vocabulary"
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(
+				AssetCategoryInfoItemFields.class, "vocabulary")
+		).build();
+
+}
---
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/item/provider/AssetCategoryInfoItemFormProvider.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.info.item.provider;
+
+import com.liferay.asset.categories.admin.web.internal.info.item.AssetCategoryInfoItemFields;
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.info.field.InfoFieldSet;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.field.reader.InfoItemFieldReaderFieldSetProvider;
+import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+
+import java.util.Locale;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = InfoItemFormProvider.class)
+public class AssetCategoryInfoItemFormProvider
+	implements InfoItemFormProvider<AssetCategory> {
+
+	@Override
+	public InfoForm getInfoForm() {
+		Set<Locale> availableLocales = LanguageUtil.getAvailableLocales();
+
+		InfoLocalizedValue.Builder infoLocalizedValueBuilder =
+			InfoLocalizedValue.builder();
+
+		for (Locale locale : availableLocales) {
+			infoLocalizedValueBuilder.value(
+				locale,
+				ResourceActionsUtil.getModelResource(
+					locale, AssetCategory.class.getName()));
+		}
+
+		return InfoForm.builder(
+		).infoFieldSetEntry(
+			_getBasicInformationInfoFieldSet()
+		).infoFieldSetEntry(
+			_infoItemFieldReaderFieldSetProvider.getInfoFieldSet(
+				AssetCategory.class.getName())
+		).labelInfoLocalizedValue(
+			infoLocalizedValueBuilder.build()
+		).name(
+			AssetCategory.class.getName()
+		).build();
+	}
+
+	private InfoFieldSet _getBasicInformationInfoFieldSet() {
+		return InfoFieldSet.builder(
+		).infoFieldSetEntry(
+			AssetCategoryInfoItemFields.nameInfoField
+		).infoFieldSetEntry(
+			AssetCategoryInfoItemFields.descriptionInfoField
+		).infoFieldSetEntry(
+			AssetCategoryInfoItemFields.vocabularyInfoField
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(getClass(), "basic-information")
+		).name(
+			"basic-information"
+		).build();
+	}
+
+	@Reference
+	private InfoItemFieldReaderFieldSetProvider
+		_infoItemFieldReaderFieldSetProvider;
+
+}
