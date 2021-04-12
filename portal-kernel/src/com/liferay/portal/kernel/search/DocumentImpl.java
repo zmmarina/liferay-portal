df49a98fa9217ad10c4ca4e3c45fe16e00d2192b
Joshua Cords <joshua.cords@liferay.com>
2021-04-12 15:49:12 -0700

    LPS-130126 portal-kernel: all Inherited methods are now null safe
    https://issues.liferay.com/browse/LPS-130126

--- portal-kernel/src/com/liferay/portal/kernel/search/DocumentImpl.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/DocumentImpl.java
@@ -367,11 +367,7 @@ public class DocumentImpl implements Document {
 
 	@Override
 	public void addKeywordSortable(String name, Boolean value) {
-		String valueString = String.valueOf(value);
-
-		createKeywordField(name, valueString, false);
-
-		_createSortableTextField(name, true, valueString);
+		addKeywordSortable(name, String.valueOf(value));
 	}
 
 	@Override
@@ -389,6 +385,10 @@ public class DocumentImpl implements Document {
 
 	@Override
 	public void addKeywordSortable(String name, String value) {
+		if (Validator.isNull(value)) {
+			return;
+		}
+
 		createKeywordField(name, value, false);
 
 		_createSortableTextField(name, true, value);
@@ -396,6 +396,10 @@ public class DocumentImpl implements Document {
 
 	@Override
 	public void addKeywordSortable(String name, String[] values) {
+		if (values == null) {
+			return;
+		}
+
 		createField(name, values);
 
 		_createSortableTextField(name, true, values);
