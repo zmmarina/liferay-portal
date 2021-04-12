425f17ec895c057522cb715e3e2bce57ba6cd7a6
Julien Castelain <julien.castelain@liferay.com>
2021-04-12 15:50:58 +0200

    LPS-130399 Revert "LPS-127012 Like this?"
    This reverts commit da91d94aadf4bb2b0e09625054abb301ff3cdefd.

--- modules/apps/frontend-editor/frontend-editor-ckeditor-sample-web/src/main/java/com/liferay/frontend/editor/ckeditor/sample/web/internal/constants/CKEditorSamplePortletKeys.java
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-sample-web/src/main/java/com/liferay/frontend/editor/ckeditor/sample/web/internal/constants/CKEditorSamplePortletKeys.java
@@ -20,7 +20,6 @@ package com.liferay.frontend.editor.ckeditor.sample.web.internal.constants;
 public class CKEditorSamplePortletKeys {
 
 	public static final String CKEDITOR_SAMPLE =
-		"com_liferay_editor_ckeditor_sample_web_internal_portlet_" +
-			"CKEditorSamplePortlet";
+		"com_liferay_editor_ckeditor_sample_web_portlet_CKEditorSamplePortlet";
 
 }
