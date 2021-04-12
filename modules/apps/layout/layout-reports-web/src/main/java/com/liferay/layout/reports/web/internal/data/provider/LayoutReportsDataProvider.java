fc1fe8df0c038cc82a1445f16c044a4e8d3e29cc
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-04-12 10:28:06 +0200

    LPS-119066 Call pagespeed inside getLayoutReportsIssues in LayoutReportsDataProvider:
    This method will return a list of LayoutReportsIssues obtained directly from the API of PageSpeed

--- modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/data/provider/LayoutReportsDataProvider.java
+++ modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/data/provider/LayoutReportsDataProvider.java
@@ -14,12 +14,22 @@
 
 package com.liferay.layout.reports.web.internal.data.provider;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.pagespeedonline.v5.PagespeedInsights;
+import com.google.api.services.pagespeedonline.v5.model.LighthouseAuditResultV5;
+import com.google.api.services.pagespeedonline.v5.model.LighthouseResultV5;
+import com.google.api.services.pagespeedonline.v5.model.PagespeedApiPagespeedResponseV5;
+
 import com.liferay.layout.reports.web.internal.model.LayoutReportsIssue;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 /**
  * @author Cristina González
@@ -33,9 +43,70 @@ public class LayoutReportsDataProvider {
 	public List<LayoutReportsIssue> getLayoutReportsIssues(String url)
 		throws LayoutReportsDataProviderException {
 
-		return Arrays.asList(
-			new LayoutReportsIssue("accessibility", 0),
-			new LayoutReportsIssue("seo", 0));
+		try {
+			if (!isValidConnection()) {
+				throw new LayoutReportsDataProviderException(
+					"Invalid Connection");
+			}
+
+			PagespeedInsights pagespeedInsights = new PagespeedInsights.Builder(
+				GoogleNetHttpTransport.newTrustedTransport(),
+				JacksonFactory.getDefaultInstance(),
+				request -> request.setConnectTimeout(_TIMEOUT)
+			).build();
+
+			PagespeedInsights.Pagespeedapi pagespeedapi =
+				pagespeedInsights.pagespeedapi();
+
+			PagespeedInsights.Pagespeedapi.Runpagespeed runpagespeed =
+				pagespeedapi.runpagespeed(url);
+
+			runpagespeed.setCategory(
+				Arrays.asList("accessibility", "best-practices", "seo"));
+			runpagespeed.setKey(_apiKey);
+
+			PagespeedApiPagespeedResponseV5 pagespeedApiPagespeedResponseV5 =
+				runpagespeed.execute();
+
+			LighthouseResultV5 lighthouseResultV5 =
+				pagespeedApiPagespeedResponseV5.getLighthouseResult();
+
+			Map<String, LighthouseAuditResultV5> lighthouseAuditResultV5s =
+				lighthouseResultV5.getAudits();
+
+			return Arrays.asList(
+				new LayoutReportsIssue(
+					"accessibility",
+					IntStream.of(
+						_getCount(
+							lighthouseAuditResultV5s.get("color-contrast")),
+						_getCount(lighthouseAuditResultV5s.get("image-alt")),
+						_getCount(
+							lighthouseAuditResultV5s.get("input-image-alt")),
+						_getCount(lighthouseAuditResultV5s.get("video-caption"))
+					).sum()),
+				new LayoutReportsIssue(
+					"seo",
+					IntStream.of(
+						_getCount(lighthouseAuditResultV5s.get("canonical")),
+						_getCount(
+							lighthouseAuditResultV5s.get("crawlable-anchors")),
+						_getCount(
+							lighthouseAuditResultV5s.get("document-title")),
+						_getCount(lighthouseAuditResultV5s.get("font-size")),
+						_getCount(lighthouseAuditResultV5s.get("hreflang")),
+						_getCount(
+							lighthouseAuditResultV5s.get("image-aspect-ratio")),
+						_getCount(lighthouseAuditResultV5s.get("is-crawlable")),
+						_getCount(lighthouseAuditResultV5s.get("link-text")),
+						_getCount(
+							lighthouseAuditResultV5s.get("meta-description")),
+						_getCount(lighthouseAuditResultV5s.get("tap-targets"))
+					).sum()));
+		}
+		catch (Exception exception) {
+			throw new LayoutReportsDataProviderException(exception);
+		}
 	}
 
 	public boolean isValidConnection() {
@@ -54,6 +125,30 @@ public class LayoutReportsDataProvider {
 		}
 
 	}
+
+	private int _getCount(LighthouseAuditResultV5 lighthouseAuditResultV5) {
+		Map<String, Object> details = lighthouseAuditResultV5.getDetails();
+
+		if (details != null) {
+			Object items = details.get("items");
+
+			if (items instanceof List) {
+				List<?> itemsList = (List)items;
+
+				return itemsList.size();
+			}
+		}
+
+		float score = GetterUtil.getFloat(lighthouseAuditResultV5.getScore());
+
+		if (score == 0) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	private static final int _TIMEOUT = 30000;
 
 	private final String _apiKey;
 
