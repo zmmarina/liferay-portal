562d4e4e0fc66c27711bdbfa4a3b6ae874b6db4e
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2021-04-13 15:18:41 +0200

    LPS-130263 Add the styles/format combo to the toolbar

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/editor/configuration/CKEditorBalloonEditorConfigContributor.java
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/editor/configuration/CKEditorBalloonEditorConfigContributor.java
@@ -18,13 +18,24 @@ import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.Validator;
 
+import java.util.Locale;
 import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Ambrín Chaudhary
@@ -48,13 +59,16 @@ public class CKEditorBalloonEditorConfigContributor
 
 		jsonObject.put("extraAllowedContent", Boolean.TRUE);
 
+		String balloonToolbarPlugins =
+			"balloontoolbar,floatingspace,stylescombo";
+
 		String extraPlugins = jsonObject.getString("extraPlugins");
 
 		if (Validator.isNotNull(extraPlugins)) {
-			extraPlugins = extraPlugins + ",balloontoolbar,floatingspace";
+			extraPlugins += "," + balloonToolbarPlugins;
 		}
 		else {
-			extraPlugins = "balloontoolbar,floatingspace";
+			extraPlugins = balloonToolbarPlugins;
 		}
 
 		jsonObject.put("extraPlugins", extraPlugins);
@@ -63,7 +77,50 @@ public class CKEditorBalloonEditorConfigContributor
 			getToolbarImageJSONObject(), getToolbarLinkJSONObject(),
 			getToolbarTextJSONObject());
 
-		jsonObject.put("toolbars", toolbarsJSONArray);
+		jsonObject.put(
+			"stylesSet", getStyleFormatsJSONArray(themeDisplay.getLocale())
+		).put(
+			"toolbars", toolbarsJSONArray
+		);
+	}
+
+	protected JSONObject getStyleFormatJSONObject(
+		String styleFormatName, String element) {
+
+		return JSONUtil.put(
+			"element", element
+		).put(
+			"name", styleFormatName
+		);
+	}
+
+	protected JSONArray getStyleFormatsJSONArray(Locale locale) {
+		ResourceBundle resourceBundle = null;
+
+		try {
+			resourceBundle = _resourceBundleLoader.loadResourceBundle(locale);
+		}
+		catch (MissingResourceException missingResourceException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(missingResourceException, missingResourceException);
+			}
+
+			resourceBundle = ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE;
+		}
+
+		return JSONUtil.putAll(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "normal"), "p"),
+			getStyleFormatJSONObject(
+				LanguageUtil.format(resourceBundle, "heading-x", "1"), "h1"),
+			getStyleFormatJSONObject(
+				LanguageUtil.format(resourceBundle, "heading-x", "2"), "h2"),
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "preformatted-text"), "pre"),
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "cited-work"), "cite"),
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "computer-code"), "code"));
 	}
 
 	protected JSONObject getToolbarImageJSONObject() {
@@ -89,12 +146,22 @@ public class CKEditorBalloonEditorConfigContributor
 	protected JSONObject getToolbarTextJSONObject() {
 		return JSONUtil.put(
 			"buttons",
-			"Bold,Italic,Underline,BulletedList,NumberedList,Link," +
+			"Styles,Bold,Italic,Underline,BulletedList,NumberedList,Link," +
 				"JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock," +
-				"RemoveFormat"
+					"RemoveFormat"
 		).put(
-			"cssSelector", "*"
+			"cssSelector", "cite,code,h1,h2,h3,p,pre,span,strong,li"
 		);
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CKEditorBalloonEditorConfigContributor.class);
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(bundle.symbolic.name=com.liferay.frontend.editor.lang)"
+	)
+	private volatile ResourceBundleLoader _resourceBundleLoader;
 
 }
