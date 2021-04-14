14ac9c3deb9f9d46dfaf646245c7dd9afcd106a8
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-04-14 15:58:49 +0200

    LPS-119066 Add tests for LayoutReportsDataProvider

---
+++ modules/apps/layout/layout-reports-web/src/test/java/com/liferay/layout/reports/web/internal/data/provider/LayoutReportsDataProviderTest.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.reports.web.internal.data.provider;
+
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Cristina González
+ */
+public class LayoutReportsDataProviderTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testIsValidConnection() {
+		LayoutReportsDataProvider layoutReportsDataProvider =
+			new LayoutReportsDataProvider("apiKey");
+
+		Assert.assertTrue(layoutReportsDataProvider.isValidConnection());
+	}
+
+	@Test
+	public void testIsValidConnectionWithoutApiKey() {
+		LayoutReportsDataProvider layoutReportsDataProvider =
+			new LayoutReportsDataProvider(null);
+
+		Assert.assertFalse(layoutReportsDataProvider.isValidConnection());
+	}
+
+}
