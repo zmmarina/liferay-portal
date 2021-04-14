f77f0908e63c57848ae18406084f5ae5607c1d84
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-04-14 16:56:41 +0200

    LPS-130305 SF - Remove comments and add qualifiers to config interface

--- modules/apps/portal-store/portal-store-azure/src/main/java/com/liferay/portal/store/azure/configuration/AzureBlobStorageStoreConfiguration.java
+++ modules/apps/portal-store/portal-store-azure/src/main/java/com/liferay/portal/store/azure/configuration/AzureBlobStorageStoreConfiguration.java
@@ -19,8 +19,6 @@ import aQute.bnd.annotation.metatype.Meta;
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
 
 /**
- * The configuration for the DL Store backed by Azure Blob Storage.
- *
  * @author Josef Sustacek
  */
 @ExtendedObjectClassDefinition(category = "file-storage")
@@ -31,51 +29,26 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface AzureBlobStorageStoreConfiguration {
 
-	// Example values:
-
-	//	* via Storage Account -> Access Keys:
-	//
-	//		DefaultEndpointsProtocol=https;AccountName=liferayportal;AccountKey=iq3R6pupSRXSnNwZkSWEXdghiLiMr7jq3G17RyZrchj9/lOYV1X9JY/1xiu2gh7Y8ElaJfQRA9+52PPqhpckMQ==;EndpointSuffix=core.windows.net
-	//
-	//	* via Storage Account -> Shared Access Signature:
-	//
-	//		BlobEndpoint=https://liferayportal.blob.core.windows.net/;QueueEndpoint=https://liferayportal.queue.core.windows.net/;FileEndpoint=https://liferayportal.file.core.windows.net/;TableEndpoint=https://liferayportal.table.core.windows.net/;SharedAccessSignature=sv=2020-02-10&ss=b&srt=sc&sp=rwdlacx&se=2021-03-23T17:44:46Z&st=2021-03-23T09:44:46Z&spr=https&sig=k4fMgC%2Bzkjc7Xu3%2Fv9HBmD6F%2BIjhQHW6qPjR7Xa5AW8%3D
-	//
-	//	* via Storage Account -> Container -> Shared access signature
-	//	(manually build based on "Blob SAS URL" and "Blob SAS token")
-	//
-	//		BlobEndpoint=https://liferayportal.blob.core.windows.net/;SharedAccessSignature=sp=r&st=2021-03-23T09:53:28Z&se=2021-03-23T17:53:28Z&spr=https&sv=2020-02-10&sr=c&sig=8GdYBOQ0%2BTlEhJlyP1bkr%2BOzdQC6X7Bm0G145MMDMz0%3D
-
 	@Meta.AD(
-		description = "connection-string-help", name = "connection-string",
-		required = true
+		description = "connection-string-help", name = "connection-string"
 	)
-	String connectionString();
+	public String connectionString();
 
-	/**
-	 * Kind of like "S3 bucket" in AWS.
-	 * @return
-	 */
 	@Meta.AD(
 		description = "container-name-help", name = "container-name",
-		required = true
 	)
-	String containerName();
+	public String containerName();
 
-	/**
-	 * Must be pre-created in Azure in given storage account.
-	 * @return
-	 */
 	@Meta.AD(
 		description = "encryption-scope-help", name = "encryption-scope",
 		required = false
 	)
-	String encryptionScope();
+	public String encryptionScope();
 
 	@Meta.AD(
 		description = "http-logging-enabled-help",
 		name = "http-logging-enabled", required = false
 	)
-	boolean httpLoggingEnabled();
+	public boolean httpLoggingEnabled();
 
 }
