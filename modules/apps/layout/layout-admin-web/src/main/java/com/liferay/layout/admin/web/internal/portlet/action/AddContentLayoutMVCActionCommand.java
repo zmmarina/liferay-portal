f32f466fdb4b2d7c75334021c1e7a784916e8cd2
Attila Bakay <attila.bakay@liferay.com>
2021-04-15 15:54:00 +0200

    LPS-130685 Passing typeSettingsProperties on layout creation from template.

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddContentLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddContentLayoutMVCActionCommand.java
@@ -39,6 +39,8 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.sites.kernel.util.SitesUtil;
 
@@ -88,6 +90,10 @@ public class AddContentLayoutMVCActionCommand
 
 		Layout layout = null;
 
+		UnicodeProperties typeSettingsUnicodeProperties =
+			PropertiesParamUtil.getProperties(
+				actionRequest, "TypeSettingsProperties--");
+
 		try {
 			LayoutPageTemplateEntry layoutPageTemplateEntry =
 				_layoutPageTemplateEntryLocalService.
@@ -106,7 +112,8 @@ public class AddContentLayoutMVCActionCommand
 				layout = _layoutService.addLayout(
 					groupId, privateLayout, parentLayoutId, nameMap,
 					new HashMap<>(), new HashMap<>(), new HashMap<>(),
-					new HashMap<>(), LayoutConstants.TYPE_PORTLET, null, false,
+					new HashMap<>(), LayoutConstants.TYPE_PORTLET,
+					typeSettingsUnicodeProperties.toString(), false,
 					new HashMap<>(), serviceContext);
 
 				// Force propagation from page template to page. See LPS-48430.
@@ -142,7 +149,8 @@ public class AddContentLayoutMVCActionCommand
 					portal.getClassNameId(LayoutPageTemplateEntry.class),
 					layoutPageTemplateEntryId, nameMap, new HashMap<>(),
 					new HashMap<>(), new HashMap<>(), new HashMap<>(),
-					LayoutConstants.TYPE_CONTENT, null, false, false,
+					LayoutConstants.TYPE_CONTENT,
+					typeSettingsUnicodeProperties.toString(), false, false,
 					new HashMap<>(), masterLayoutPlid, serviceContext);
 			}
 
