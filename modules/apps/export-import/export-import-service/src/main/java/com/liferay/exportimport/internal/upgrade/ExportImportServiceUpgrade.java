35cccfce6f7f45715d6a7c1a0fff903657da6915
Tamas Molnar <tamas.molnar@liferay.com>
2021-04-16 14:31:12 +0200

    LPS-126769 Upgrade process

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/upgrade/ExportImportServiceUpgrade.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/upgrade/ExportImportServiceUpgrade.java
@@ -15,14 +15,17 @@
 package com.liferay.exportimport.internal.upgrade;
 
 import com.liferay.exportimport.internal.upgrade.v1_0_0.PublisherRequestUpgradeProcess;
+import com.liferay.exportimport.internal.upgrade.v1_0_2.UpgradeExportImportServiceConfiguration;
 import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalService;
 import com.liferay.portal.kernel.model.Release;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -51,6 +54,11 @@ public class ExportImportServiceUpgrade implements UpgradeStepRegistrator {
 			"1.0.0", "1.0.1",
 			new com.liferay.exportimport.internal.upgrade.v1_0_1.
 				UpgradeBackgroundTaskExecutorClassNames());
+
+		registry.register(
+			"1.0.1", "1.0.2",
+			new UpgradeExportImportServiceConfiguration(
+				_configurationAdmin, _configurationProvider));
 	}
 
 	@Reference(unbind = "-")
@@ -85,6 +93,12 @@ public class ExportImportServiceUpgrade implements UpgradeStepRegistrator {
 	protected void setUserLocalService(UserLocalService userLocalService) {
 		_userLocalService = userLocalService;
 	}
+
+	@Reference
+	private ConfigurationAdmin _configurationAdmin;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
 
 	private ExportImportConfigurationLocalService
 		_exportImportConfigurationLocalService;
---
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/upgrade/v1_0_2/UpgradeExportImportServiceConfiguration.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.internal.upgrade.v1_0_2;
+
+import com.liferay.exportimport.configuration.ExportImportServiceConfiguration;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.staging.configuration.StagingConfiguration;
+
+import java.util.Dictionary;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Tamas Molnar
+ */
+public class UpgradeExportImportServiceConfiguration extends UpgradeProcess {
+
+	public UpgradeExportImportServiceConfiguration(
+		ConfigurationAdmin configurationAdmin,
+		ConfigurationProvider configurationProvider) {
+
+		_configurationAdmin = configurationAdmin;
+		_configurationProvider = configurationProvider;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		upgradeExportImportServiceConfiguration();
+	}
+
+	protected void upgradeExportImportServiceConfiguration() throws Exception {
+		Configuration[] exportImportServiceConfigurations =
+			_configurationAdmin.listConfigurations(
+				"(service.pid=" +
+					ExportImportServiceConfiguration.class.getName() + "*)");
+
+		if (exportImportServiceConfigurations == null) {
+			return;
+		}
+
+		for (Configuration exportImportServiceConfiguration :
+				exportImportServiceConfigurations) {
+
+			Dictionary<String, Object> exportImportProperties =
+				exportImportServiceConfiguration.getProperties();
+
+			if (exportImportProperties == null) {
+				continue;
+			}
+
+			Dictionary<String, Object> stagingProperties =
+				new HashMapDictionary<>();
+
+			stagingProperties.put(
+				"publishParentLayoutsByDefault",
+				GetterUtil.getBoolean(
+					exportImportProperties.remove(
+						"publishParentLayoutsByDefault"),
+					true));
+			stagingProperties.put(
+				"stagingDeleteTempLarOnFailure",
+				GetterUtil.getBoolean(
+					exportImportProperties.remove(
+						"stagingDeleteTempLarOnFailure"),
+					true));
+			stagingProperties.put(
+				"stagingDeleteTempLarOnSuccess",
+				GetterUtil.getBoolean(
+					exportImportProperties.remove(
+						"stagingDeleteTempLarOnSuccess"),
+					true));
+			stagingProperties.put(
+				"stagingUseVirtualHostForRemoteSite",
+				GetterUtil.getBoolean(
+					exportImportProperties.remove(
+						"stagingUseVirtualHostForRemoteSite")));
+
+			exportImportServiceConfiguration.update(exportImportProperties);
+
+			long companyId = GetterUtil.getLong(
+				exportImportProperties.get("companyId"));
+
+			if (companyId != 0) {
+				stagingProperties.put("companyId", companyId);
+
+				_configurationProvider.saveCompanyConfiguration(
+					StagingConfiguration.class, companyId, stagingProperties);
+			}
+			else {
+				_configurationProvider.saveSystemConfiguration(
+					StagingConfiguration.class, stagingProperties);
+			}
+		}
+	}
+
+	private final ConfigurationAdmin _configurationAdmin;
+	private final ConfigurationProvider _configurationProvider;
+
+}
