a169acf4686e371455421bc5e6c2f237080ae089
Alan Huang <alan.huang@liferay.com>
2021-04-19 12:41:02 +0800

    LPS-130505 SF, remove unsed method

--- modules/apps/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/react/FlagsTag.java
+++ modules/apps/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/react/FlagsTag.java
@@ -251,10 +251,6 @@ public class FlagsTag extends IncludeTag {
 		return dataJSONObject;
 	}
 
-	private String _getElementClasses() {
-		return _elementClasses;
-	}
-
 	private String _getMessage() {
 		ResourceBundle resourceBundle = new AggregateResourceBundle(
 			TagResourceBundleUtil.getResourceBundle(pageContext),
