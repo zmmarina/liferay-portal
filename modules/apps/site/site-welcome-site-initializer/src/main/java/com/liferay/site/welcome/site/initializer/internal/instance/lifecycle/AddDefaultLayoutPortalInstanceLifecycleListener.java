93f861d864b6c24ace7bdc6f7b16a855d2e07b6d
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-04-22 09:47:39 +0200

    LPS-124483 Move LifecycleListener and remove files and code that is not needed. We now call the site initializer, creating the home page

---
+++ modules/apps/site/site-welcome-site-initializer/src/main/java/com/liferay/site/welcome/site/initializer/internal/instance/lifecycle/AddDefaultLayoutPortalInstanceLifecycleListener.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.welcome.site.initializer.internal.instance.lifecycle;
+
+import com.liferay.fragment.contributor.FragmentCollectionContributorRegistration;
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.site.initializer.SiteInitializer;
+import com.liferay.site.welcome.site.initializer.internal.WelcomeSiteInitializer;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
+public class AddDefaultLayoutPortalInstanceLifecycleListener
+	extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+		Group group = _groupLocalService.getGroup(
+			company.getCompanyId(), GroupConstants.GUEST);
+
+		Layout defaultLayout = _layoutLocalService.fetchFirstLayout(
+			group.getGroupId(), false, LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+			false);
+
+		if (defaultLayout == null) {
+			_siteInitializer.initialize(group.getGroupId());
+		}
+	}
+
+	@Reference(
+		target = "(fragment.collection.key=BASIC_COMPONENT)", unbind = "-"
+	)
+	protected void setFragmentCollectionContributorRegistration(
+		FragmentCollectionContributorRegistration
+			fragmentCollectionContributorRegistration) {
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference(
+		target = "(site.initializer.key=" + WelcomeSiteInitializer.KEY + ")"
+	)
+	private SiteInitializer _siteInitializer;
+
+}
