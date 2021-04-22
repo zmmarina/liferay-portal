7891966f691e4627c295e1183cc2dfcbd50ed25c
Marco Leo <marco.leo@liferay.com>
2021-04-23 00:54:38 +0200

    COMMERCE-6502 Expose delete method in remote services

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionSpecificationOptionValueServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionSpecificationOptionValueServiceImpl.java
@@ -74,6 +74,16 @@ public class CPDefinitionSpecificationOptionValueServiceImpl
 	}
 
 	@Override
+	public void deleteCPDefinitionSpecificationOptionValues(long cpDefinitionId)
+		throws PortalException {
+
+		_checkCommerceCatalog(cpDefinitionId, ActionKeys.UPDATE);
+
+		cpDefinitionSpecificationOptionValueLocalService.
+			deleteCPDefinitionSpecificationOptionValues(cpDefinitionId);
+	}
+
+	@Override
 	public CPDefinitionSpecificationOptionValue
 			fetchCPDefinitionSpecificationOptionValue(
 				long cpDefinitionSpecificationOptionValueId)
