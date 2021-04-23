55db17873787c74bb90c151b77f5072af31cf9ed
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-04-23 12:39:27 +0200

    LPS-131049 Add integration test for GCS store

---
+++ modules/apps/portal-store/portal-store-gcs-test/src/testIntegration/java/com/liferay/portal/store/gcs/test/GCSStoreTest.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.store.gcs.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.store.Store;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.AssumeTestRule;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.store.test.util.BaseStoreTestCase;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Dictionary;
+
+import org.junit.AfterClass;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Adolfo Pérez
+ */
+@RunWith(Arquillian.class)
+public class GCSStoreTest extends BaseStoreTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new AssumeTestRule("assume"), new LiferayIntegrationTestRule());
+
+	public static void assume() {
+		String dlStoreImpl = PropsUtil.get(PropsKeys.DL_STORE_IMPL);
+
+		String gcsStoreClassName = "com.liferay.portal.store.gcs.GCSStore";
+
+		Assume.assumeTrue(
+			StringBundler.concat(
+				"Property \"", PropsKeys.DL_STORE_IMPL, "\" is not set to \"",
+				gcsStoreClassName, "\""),
+			dlStoreImpl.equals(gcsStoreClassName));
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_configuration = _configurationAdmin.getConfiguration(
+			"com.liferay.portal.store.gcs.configuration.GCSStoreConfiguration",
+			StringPool.QUESTION);
+
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("serviceAccountKey", "");
+		properties.put("bucketName", "test");
+		properties.put("maxRetryAttempts", "5");
+		properties.put("initialRetryDelay", "400");
+		properties.put("maxRetryDelay", "10000");
+		properties.put("retryDelayMultiplier", "1.5");
+		properties.put("initialRPCTimeout", "120000");
+		properties.put("maxRPCTimeout", "600000");
+		properties.put("rpcTimeoutMultiplier", "1.0");
+		properties.put("retryJitter", "false");
+		properties.put("aes256Key", "");
+
+		ConfigurationTestUtil.saveConfiguration(_configuration, properties);
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		ConfigurationTestUtil.deleteConfiguration(_configuration);
+	}
+
+	@Override
+	protected Store getStore() {
+		return _store;
+	}
+
+	private static Configuration _configuration;
+
+	@Inject
+	private static ConfigurationAdmin _configurationAdmin;
+
+	@Inject(
+		filter = "store.type=com.liferay.portal.store.gcs.GCSStore",
+		type = Store.class
+	)
+	private Store _store;
+
+}
