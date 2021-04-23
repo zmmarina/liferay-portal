44c964bd37786ee72d47ae44c03a2efb3ac5a0ff
Brian Chan <brian.chan@liferay.com>
2021-04-23 17:18:34 -0300

    Revert "LPS-130505 SF, remove unused method"
    This reverts commit 88fca30b6d7b986d3af0c732d5b9e575c909ab4d.

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/results/builder/RankingJSONBuilder.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/results/builder/RankingJSONBuilder.java
@@ -316,6 +316,10 @@ public class RankingJSONBuilder {
 		return _resourceActions.getModelResource(_locale, entryClassName);
 	}
 
+	private String _getViewURL() {
+		return _viewURL;
+	}
+
 	private boolean _isFileEntry() {
 		String entryClassName = _document.getString(Field.ENTRY_CLASS_NAME);
 
