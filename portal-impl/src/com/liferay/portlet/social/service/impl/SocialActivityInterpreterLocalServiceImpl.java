f728fb2726036ca829e8d81e3962e1d2d19d6e85
Brian Chan <brian.chan@liferay.com>
2021-04-25 19:19:02 -0300

    Hugo SF, like the one before this commit

--- portal-impl/src/com/liferay/portlet/social/service/impl/SocialActivityInterpreterLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/social/service/impl/SocialActivityInterpreterLocalServiceImpl.java
@@ -93,7 +93,7 @@ public class SocialActivityInterpreterLocalServiceImpl
 				SocialActivityInterpreter.class, activityInterpreter,
 				properties);
 
-		_serviceRegistrations.put(activityInterpreter, serviceRegistration);
+		_serviceRegistrationMap.put(activityInterpreter, serviceRegistration);
 	}
 
 	@Override
@@ -122,7 +122,7 @@ public class SocialActivityInterpreterLocalServiceImpl
 		SocialActivityInterpreter activityInterpreter) {
 
 		ServiceRegistration<SocialActivityInterpreter> serviceRegistration =
-			_serviceRegistrations.remove(activityInterpreter);
+			_serviceRegistrationMap.remove(activityInterpreter);
 
 		if (serviceRegistration != null) {
 			serviceRegistration.unregister();
@@ -331,7 +331,7 @@ public class SocialActivityInterpreterLocalServiceImpl
 	private final Map<String, List<SocialActivityInterpreter>>
 		_activityInterpreters = new HashMap<>();
 	private final ServiceRegistrationMap<SocialActivityInterpreter>
-		_serviceRegistrations = new ServiceRegistrationMapImpl<>();
+		_serviceRegistrationMap = new ServiceRegistrationMapImpl<>();
 	private ServiceTracker<SocialActivityInterpreter, SocialActivityInterpreter>
 		_serviceTracker;
 
