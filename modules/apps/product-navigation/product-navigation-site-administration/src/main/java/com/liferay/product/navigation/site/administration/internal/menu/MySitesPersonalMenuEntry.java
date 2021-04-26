c4c731285388d615b0631d9b2d7ead8b2555c486
Brian Chan <brian.chan@liferay.com>
2021-04-26 12:33:03 -0300

    LPS-129701 Hugo SF

--- modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/internal/menu/MySitesPersonalMenuEntry.java
+++ modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/internal/menu/MySitesPersonalMenuEntry.java
@@ -85,8 +85,7 @@ public class MySitesPersonalMenuEntry implements PersonalMenuEntry {
 
 		StringBuilder sb = new StringBuilder(12);
 
-		sb.append("javascript:Liferay.Util.openSelectionModal({");
-		sb.append("id: '");
+		sb.append("javascript:Liferay.Util.openSelectionModal({id: '");
 		sb.append(namespace);
 		sb.append("selectSite', onSelect: function(selectedItem) ");
 		sb.append("{Liferay.Util.navigate(selectedItem.url);}");
