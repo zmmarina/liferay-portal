fb3d832946dd47a92ea09f517aead1a33a71038c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-04-26 17:41:27 +0100

    LPS-131040 Allow select categories on preview

---
+++ modules/apps/asset/asset-categories-item-selector-web/src/main/java/com/liferay/asset/categories/item/selector/web/internal/AssetCategoryInfoItemItemSelectorView.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.item.selector.web.internal;
+
+import com.liferay.asset.categories.item.selector.web.internal.constants.AssetCategoryTreeNodeItemSelectorWebKeys;
+import com.liferay.asset.categories.item.selector.web.internal.display.context.SelectAssetCategoryTreeNodeDisplayContext;
+import com.liferay.asset.categories.item.selector.web.internal.display.context.SelectAssetVocabularyDisplayContext;
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.info.item.selector.InfoItemSelectorView;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.InfoItemItemSelectorReturnType;
+import com.liferay.item.selector.criteria.info.item.criterion.InfoItemItemSelectorCriterion;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = "item.selector.view.order:Integer=400",
+	service = ItemSelectorView.class
+)
+public class AssetCategoryInfoItemItemSelectorView
+	implements InfoItemSelectorView,
+			   ItemSelectorView<InfoItemItemSelectorCriterion> {
+
+	@Override
+	public String getClassName() {
+		return AssetCategory.class.getName();
+	}
+
+	@Override
+	public Class<InfoItemItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return InfoItemItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, AssetCategoryInfoItemItemSelectorView.class);
+
+		return ResourceBundleUtil.getString(resourceBundle, "categories");
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			InfoItemItemSelectorCriterion infoItemItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		SelectAssetCategoryTreeNodeDisplayContext
+			selectAssetCategoryLevelDisplayContext =
+				new SelectAssetCategoryTreeNodeDisplayContext(
+					(HttpServletRequest)servletRequest, itemSelectedEventName,
+					portletURL);
+
+		servletRequest.setAttribute(
+			AssetCategoryTreeNodeItemSelectorWebKeys.
+				SELECT_ASSET_CATEGORY_TREE_NODE_ITEM_SELECTOR_DISPLAY_CONTEXT,
+			selectAssetCategoryLevelDisplayContext);
+
+		SelectAssetVocabularyDisplayContext
+			selectAssetVocabularyDisplayContext =
+				new SelectAssetVocabularyDisplayContext(
+					(HttpServletRequest)servletRequest, portletURL);
+
+		servletRequest.setAttribute(
+			AssetCategoryTreeNodeItemSelectorWebKeys.
+				SELECT_ASSET_VOCABULARY_DISPLAY_CONTEXT,
+			selectAssetVocabularyDisplayContext);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/select_asset_vocabulary.jsp");
+
+		requestDispatcher.include(servletRequest, servletResponse);
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.singletonList(
+			new InfoItemItemSelectorReturnType());
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.categories.item.selector.web)"
+	)
+	private ServletContext _servletContext;
+
+}
