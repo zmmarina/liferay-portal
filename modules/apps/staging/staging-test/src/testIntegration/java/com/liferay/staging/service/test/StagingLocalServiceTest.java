e996e93d6a71fcf48965372ed5901a640766002d
Brian Chan <brian.chan@liferay.com>
2021-04-27 15:04:08 -0300

    LPS-127658 auto SF

--- modules/apps/staging/staging-test/src/testIntegration/java/com/liferay/staging/service/test/StagingLocalServiceTest.java
+++ modules/apps/staging/staging-test/src/testIntegration/java/com/liferay/staging/service/test/StagingLocalServiceTest.java
@@ -71,197 +71,6 @@ public class StagingLocalServiceTest {
 	}
 
 	@Test
-	public void testDisableStaging() throws Exception {
-		Group group = GroupTestUtil.addGroup();
-
-		try {
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), group, false, false, new ServiceContext());
-
-			StagingLocalServiceUtil.disableStaging(group, new ServiceContext());
-
-			group = GroupLocalServiceUtil.getGroup(group.getGroupId());
-
-			Assert.assertFalse(group.hasStagingGroup());
-		}
-		finally {
-			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
-		}
-	}
-
-	@Test
-	public void testDisableStagingWithParentGroup() throws Exception {
-		Group parentGroup = GroupTestUtil.addGroup();
-
-		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
-
-		try {
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), childGroup, false, false,
-				new ServiceContext());
-
-			StagingLocalServiceUtil.disableStaging(
-				childGroup, new ServiceContext());
-
-			childGroup = GroupLocalServiceUtil.getGroup(
-				childGroup.getGroupId());
-
-			Assert.assertFalse(childGroup.hasStagingGroup());
-		}
-		finally {
-			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
-
-			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
-		}
-	}
-
-	@Test
-	public void testDisableStagingWithStagedChildGroup() throws Exception {
-		Group parentGroup = GroupTestUtil.addGroup();
-
-		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
-
-		try {
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), parentGroup, false, false,
-				new ServiceContext());
-
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), childGroup, false, false,
-				new ServiceContext());
-
-			StagingLocalServiceUtil.disableStaging(
-				parentGroup, new ServiceContext());
-
-			childGroup = GroupLocalServiceUtil.getGroup(
-				childGroup.getGroupId());
-
-			Group childGroupStagingGroup = childGroup.getStagingGroup();
-
-			Assert.assertEquals(
-				childGroupStagingGroup.getParentGroupId(),
-				childGroup.getParentGroupId());
-		}
-		finally {
-			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
-
-			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
-		}
-	}
-
-	@Test
-	public void testEnableLocalStaging() throws Exception {
-		Group group = GroupTestUtil.addGroup();
-
-		try {
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), group, false, false, new ServiceContext());
-
-			group = GroupLocalServiceUtil.getGroup(group.getGroupId());
-
-			Assert.assertTrue(group.hasStagingGroup());
-		}
-		finally {
-			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
-		}
-	}
-
-	@Test
-	public void testEnableLocalStagingWithParentGroup() throws Exception {
-		Group parentGroup = GroupTestUtil.addGroup();
-
-		Group group = GroupTestUtil.addGroup(parentGroup.getGroupId());
-
-		try {
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), group, false, false, new ServiceContext());
-
-			group = GroupLocalServiceUtil.getGroup(group.getGroupId());
-
-			Group groupStagingGroup = group.getStagingGroup();
-
-			Assert.assertEquals(
-				parentGroup.getGroupId(), group.getParentGroupId());
-
-			Assert.assertEquals(
-				parentGroup.getGroupId(), groupStagingGroup.getParentGroupId());
-		}
-		finally {
-			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
-
-			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
-		}
-	}
-
-	@Test
-	public void testEnableLocalStagingWithStagedChildGroup() throws Exception {
-		Group parentGroup = GroupTestUtil.addGroup();
-
-		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
-
-		try {
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), childGroup, false, false,
-				new ServiceContext());
-
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), parentGroup, false, false,
-				new ServiceContext());
-
-			childGroup = GroupLocalServiceUtil.getGroup(
-				childGroup.getGroupId());
-
-			Group childGroupStagingGroup = childGroup.getStagingGroup();
-
-			Assert.assertEquals(
-				parentGroup.getGroupId(), childGroup.getParentGroupId());
-
-			Assert.assertEquals(
-				parentGroup.getGroupId(),
-				childGroupStagingGroup.getParentGroupId());
-		}
-		finally {
-			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
-
-			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
-		}
-	}
-
-	@Test
-	public void testEnableLocalStagingWithStagedParentGroup() throws Exception {
-		Group parentGroup = GroupTestUtil.addGroup();
-
-		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
-
-		try {
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), parentGroup, false, false,
-				new ServiceContext());
-
-			StagingLocalServiceUtil.enableLocalStaging(
-				_user.getUserId(), childGroup, false, false,
-				new ServiceContext());
-
-			childGroup = GroupLocalServiceUtil.getGroup(
-				childGroup.getGroupId());
-
-			Group childGroupStagingGroup = childGroup.getStagingGroup();
-
-			Assert.assertEquals(
-				parentGroup.getGroupId(), childGroup.getParentGroupId());
-
-			Assert.assertEquals(
-				parentGroup.getGroupId(),
-				childGroupStagingGroup.getParentGroupId());
-		}
-		finally {
-			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
-
-			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
-		}
-	}
-
-	@Test
 	public void testBranchingLayoutPortletRemoval() throws Exception {
 		Group group = GroupTestUtil.addGroup();
 
@@ -466,6 +275,197 @@ public class StagingLocalServiceTest {
 		}
 		finally {
 			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
+		}
+	}
+
+	@Test
+	public void testDisableStaging() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), group, false, false, new ServiceContext());
+
+			StagingLocalServiceUtil.disableStaging(group, new ServiceContext());
+
+			group = GroupLocalServiceUtil.getGroup(group.getGroupId());
+
+			Assert.assertFalse(group.hasStagingGroup());
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
+		}
+	}
+
+	@Test
+	public void testDisableStagingWithParentGroup() throws Exception {
+		Group parentGroup = GroupTestUtil.addGroup();
+
+		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), childGroup, false, false,
+				new ServiceContext());
+
+			StagingLocalServiceUtil.disableStaging(
+				childGroup, new ServiceContext());
+
+			childGroup = GroupLocalServiceUtil.getGroup(
+				childGroup.getGroupId());
+
+			Assert.assertFalse(childGroup.hasStagingGroup());
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
+
+			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
+		}
+	}
+
+	@Test
+	public void testDisableStagingWithStagedChildGroup() throws Exception {
+		Group parentGroup = GroupTestUtil.addGroup();
+
+		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), parentGroup, false, false,
+				new ServiceContext());
+
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), childGroup, false, false,
+				new ServiceContext());
+
+			StagingLocalServiceUtil.disableStaging(
+				parentGroup, new ServiceContext());
+
+			childGroup = GroupLocalServiceUtil.getGroup(
+				childGroup.getGroupId());
+
+			Group childGroupStagingGroup = childGroup.getStagingGroup();
+
+			Assert.assertEquals(
+				childGroupStagingGroup.getParentGroupId(),
+				childGroup.getParentGroupId());
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
+
+			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
+		}
+	}
+
+	@Test
+	public void testEnableLocalStaging() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), group, false, false, new ServiceContext());
+
+			group = GroupLocalServiceUtil.getGroup(group.getGroupId());
+
+			Assert.assertTrue(group.hasStagingGroup());
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
+		}
+	}
+
+	@Test
+	public void testEnableLocalStagingWithParentGroup() throws Exception {
+		Group parentGroup = GroupTestUtil.addGroup();
+
+		Group group = GroupTestUtil.addGroup(parentGroup.getGroupId());
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), group, false, false, new ServiceContext());
+
+			group = GroupLocalServiceUtil.getGroup(group.getGroupId());
+
+			Group groupStagingGroup = group.getStagingGroup();
+
+			Assert.assertEquals(
+				parentGroup.getGroupId(), group.getParentGroupId());
+
+			Assert.assertEquals(
+				parentGroup.getGroupId(), groupStagingGroup.getParentGroupId());
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
+
+			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
+		}
+	}
+
+	@Test
+	public void testEnableLocalStagingWithStagedChildGroup() throws Exception {
+		Group parentGroup = GroupTestUtil.addGroup();
+
+		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), childGroup, false, false,
+				new ServiceContext());
+
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), parentGroup, false, false,
+				new ServiceContext());
+
+			childGroup = GroupLocalServiceUtil.getGroup(
+				childGroup.getGroupId());
+
+			Group childGroupStagingGroup = childGroup.getStagingGroup();
+
+			Assert.assertEquals(
+				parentGroup.getGroupId(), childGroup.getParentGroupId());
+
+			Assert.assertEquals(
+				parentGroup.getGroupId(),
+				childGroupStagingGroup.getParentGroupId());
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
+
+			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
+		}
+	}
+
+	@Test
+	public void testEnableLocalStagingWithStagedParentGroup() throws Exception {
+		Group parentGroup = GroupTestUtil.addGroup();
+
+		Group childGroup = GroupTestUtil.addGroup(parentGroup.getGroupId());
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), parentGroup, false, false,
+				new ServiceContext());
+
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), childGroup, false, false,
+				new ServiceContext());
+
+			childGroup = GroupLocalServiceUtil.getGroup(
+				childGroup.getGroupId());
+
+			Group childGroupStagingGroup = childGroup.getStagingGroup();
+
+			Assert.assertEquals(
+				parentGroup.getGroupId(), childGroup.getParentGroupId());
+
+			Assert.assertEquals(
+				parentGroup.getGroupId(),
+				childGroupStagingGroup.getParentGroupId());
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
+
+			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
 		}
 	}
 
