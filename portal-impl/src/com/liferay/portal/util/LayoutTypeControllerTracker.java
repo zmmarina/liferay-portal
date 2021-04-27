2ee2a6f6e9f1a235789019220349fe8095119e7a
Victor Galan <victor.galan@liferay.com>
2021-04-27 17:16:59 +0200

    LPS-131141 Use a serviceTrackerMap to leverage service ranking to avoid default type controller to override custom one

--- portal-impl/src/com/liferay/portal/util/LayoutTypeControllerTracker.java
+++ portal-impl/src/com/liferay/portal/util/LayoutTypeControllerTracker.java
@@ -19,18 +19,14 @@ import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutTypeController;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.model.impl.LayoutTypeControllerImpl;
-import com.liferay.registry.Filter;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
-import com.liferay.registry.ServiceReference;
-import com.liferay.registry.ServiceTracker;
-import com.liferay.registry.ServiceTrackerCustomizer;
+import com.liferay.registry.collections.ServiceTrackerCollections;
+import com.liferay.registry.collections.ServiceTrackerMap;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author Raymond Augé
@@ -42,120 +38,55 @@ public class LayoutTypeControllerTracker {
 	}
 
 	public static LayoutTypeController getLayoutTypeController(String type) {
-		LayoutTypeController layoutTypeController = _layoutTypeControllers.get(
-			type);
+		LayoutTypeController layoutTypeController =
+			_serviceTrackerMap.getService(type);
 
 		if (layoutTypeController != null) {
 			return layoutTypeController;
 		}
 
-		return _layoutTypeControllers.get(LayoutConstants.TYPE_PORTLET);
+		return _serviceTrackerMap.getService(LayoutConstants.TYPE_PORTLET);
 	}
 
 	public static Map<String, LayoutTypeController> getLayoutTypeControllers() {
-		return Collections.unmodifiableMap(_layoutTypeControllers);
+		HashMap<String, LayoutTypeController> layoutTypeControllers =
+			new HashMap<>();
+
+		for (String type : _serviceTrackerMap.keySet()) {
+
+			layoutTypeControllers.put(
+				type, _serviceTrackerMap.getService(type));
+		}
+
+		return layoutTypeControllers;
 	}
 
 	public static String[] getTypes() {
-		Set<String> types = _layoutTypeControllers.keySet();
+		Set<String> types = _serviceTrackerMap.keySet();
 
 		return types.toArray(new String[0]);
-	}
-
-	private static void _registerDefaults(Registry registry) {
-		Set<Map.Entry<String, LayoutTypeController>> entries =
-			_defaultLayoutTypeControllers.entrySet();
-
-		for (Map.Entry<String, LayoutTypeController> entry : entries) {
-			registry.registerService(
-				LayoutTypeController.class, entry.getValue(),
-				HashMapBuilder.<String, Object>put(
-					"layout.type", entry.getKey()
-				).build());
-		}
 	}
 
 	private static final String[] _LAYOUT_TYPES = {
 		LayoutConstants.TYPE_PORTLET
 	};
 
-	private static final Map<String, LayoutTypeController>
-		_defaultLayoutTypeControllers = new ConcurrentHashMap<>();
-	private static final ConcurrentMap<String, LayoutTypeController>
-		_layoutTypeControllers = new ConcurrentHashMap<>();
-	private static final ServiceTracker
-		<LayoutTypeController, LayoutTypeController> _serviceTracker;
-
-	private static class LayoutTypeControllerServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer
-			<LayoutTypeController, LayoutTypeController> {
-
-		@Override
-		@SuppressWarnings("unchecked")
-		public LayoutTypeController addingService(
-			ServiceReference<LayoutTypeController> serviceReference) {
-
-			Registry registry = RegistryUtil.getRegistry();
-
-			LayoutTypeController layoutTypeController = registry.getService(
-				serviceReference);
-
-			String type = (String)serviceReference.getProperty("layout.type");
-
-			_layoutTypeControllers.put(type, layoutTypeController);
-
-			return layoutTypeController;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<LayoutTypeController> serviceReference,
-			LayoutTypeController layoutTypeController) {
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<LayoutTypeController> serviceReference,
-			LayoutTypeController layoutTypeController) {
-
-			Registry registry = RegistryUtil.getRegistry();
-
-			registry.ungetService(serviceReference);
-
-			String type = (String)serviceReference.getProperty("layout.type");
-
-			LayoutTypeController defaultLayoutTypeController =
-				_defaultLayoutTypeControllers.get(type);
-
-			if (defaultLayoutTypeController == null) {
-				_layoutTypeControllers.remove(type);
-			}
-			else {
-				_layoutTypeControllers.replace(
-					type, layoutTypeController, defaultLayoutTypeController);
-			}
-		}
-
-	}
+	private static final ServiceTrackerMap<String, LayoutTypeController>
+		_serviceTrackerMap;
 
 	static {
-		for (String type : _LAYOUT_TYPES) {
-			_defaultLayoutTypeControllers.put(
-				type, new LayoutTypeControllerImpl(type));
-		}
-
 		Registry registry = RegistryUtil.getRegistry();
 
-		_registerDefaults(registry);
+		for (String type : _LAYOUT_TYPES) {
+			registry.registerService(
+				LayoutTypeController.class, new LayoutTypeControllerImpl(type),
+				HashMapBuilder.<String, Object>put(
+					"layout.type", type
+				).build());
+		}
 
-		Filter filter = registry.getFilter(
-			"(&(layout.type=*)(objectClass=" +
-				LayoutTypeController.class.getName() + "))");
-
-		_serviceTracker = registry.trackServices(
-			filter, new LayoutTypeControllerServiceTrackerCustomizer());
-
-		_serviceTracker.open();
+		_serviceTrackerMap = ServiceTrackerCollections.openSingleValueMap(
+			LayoutTypeController.class, "layout.type");
 	}
 
 }
