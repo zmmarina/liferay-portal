41f1cb170244fc18f5568b7ddaca6b0d4d812198
Matthew Tambara <matthew.tambara@liferay.com>
2021-04-28 15:11:33 -0700

    LPS-130644 Add test

--- portal-impl/test/unit/com/liferay/portal/test/rule/InitializeKernelUtilTestRuleTest.java
+++ portal-impl/test/unit/com/liferay/portal/test/rule/InitializeKernelUtilTestRuleTest.java
@@ -14,6 +14,11 @@
 
 package com.liferay.portal.test.rule;
 
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.PropsUtil;
 
 import org.junit.Assert;
@@ -33,6 +38,31 @@ public class InitializeKernelUtilTestRuleTest {
 		LiferayUnitTestRule.INSTANCE;
 
 	@Test
+	public void testCalendarFactoryUtil() {
+		Assert.assertNotNull(CalendarFactoryUtil.getCalendar());
+	}
+
+	@Test
+	public void testFileUtil() {
+		Assert.assertNotNull(FileUtil.getFile());
+	}
+
+	@Test
+	public void testHtmlUtil() {
+		Assert.assertNotNull(HtmlUtil.getHtml());
+	}
+
+	@Test
+	public void testHttpUtil() {
+		Assert.assertNotNull(HttpUtil.getHttp());
+	}
+
+	@Test
+	public void testJSONFactoryUtil() {
+		Assert.assertNotNull(JSONFactoryUtil.getJSONFactory());
+	}
+
+	@Test
 	public void testProps() {
 		Assert.assertNotNull(PropsUtil.getProps());
 
@@ -47,6 +77,11 @@ public class InitializeKernelUtilTestRuleTest {
 		Assert.assertEquals("testClassValue", PropsUtil.get("testClassKey"));
 
 		Assert.assertEquals("testMethodValue", PropsUtil.get("testMethodKey"));
+	}
+
+	@Test
+	public void testPropsUtil() {
+		Assert.assertNotNull(PropsUtil.getProps());
 	}
 
 }
