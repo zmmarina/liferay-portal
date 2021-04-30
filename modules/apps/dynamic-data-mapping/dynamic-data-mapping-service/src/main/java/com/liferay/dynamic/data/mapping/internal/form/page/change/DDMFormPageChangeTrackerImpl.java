a46c68029c9dfc0b18d0a392ab9f195de61f1c8a
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-04-30 16:09:51 -0300

    LPS-130739 Create DDMFormPageTracker implementation

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/form/page/change/DDMFormPageChangeTrackerImpl.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.internal.form.page.change;
+
+import com.liferay.dynamic.data.mapping.form.page.change.DDMFormPageChange;
+import com.liferay.dynamic.data.mapping.form.page.change.DDMFormPageChangeTracker;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Bruno Oliveira
+ * @author Carolina Barbosa
+ */
+@Component(immediate = true, service = DDMFormPageChangeTracker.class)
+public class DDMFormPageChangeTrackerImpl implements DDMFormPageChangeTracker {
+
+	@Override
+	public DDMFormPageChange getDDMFormPageChangeById(long id) {
+		return _ddmFormPageChangeIdTrackerMap.getService(id);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_ddmFormPageChangeIdTrackerMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext, DDMFormPageChange.class, "ddm.form.instance.id");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_ddmFormPageChangeIdTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<Long, DDMFormPageChange>
+		_ddmFormPageChangeIdTrackerMap;
+
+}
