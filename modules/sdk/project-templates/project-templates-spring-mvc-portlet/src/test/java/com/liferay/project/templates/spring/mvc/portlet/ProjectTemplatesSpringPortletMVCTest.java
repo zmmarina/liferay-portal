6a5562449c07351b1055d74b1d54c4d2efb21c03
Lawrence Lee <lawrence.lee@liferay.com>
2021-04-30 15:02:48 -0500

    LPS-130908 Update test case

--- modules/sdk/project-templates/project-templates-spring-mvc-portlet/src/test/java/com/liferay/project/templates/spring/mvc/portlet/ProjectTemplatesSpringPortletMVCTest.java
+++ modules/sdk/project-templates/project-templates-spring-mvc-portlet/src/test/java/com/liferay/project/templates/spring/mvc/portlet/ProjectTemplatesSpringPortletMVCTest.java
@@ -54,13 +54,16 @@ public class ProjectTemplatesSpringPortletMVCTest
 				{"springportletmvc", "embedded", "jsp", "7.0.6"},
 				{"springportletmvc", "embedded", "jsp", "7.1.3"},
 				{"springportletmvc", "embedded", "jsp", "7.2.1"},
-				{"springportletmvc", "embedded", "jsp", "7.3.5"},
+				{"springportletmvc", "embedded", "jsp", "7.3.6"},
+				{"springportletmvc", "embedded", "jsp", "7.4.0"},
 				{"portletmvc4spring", "embedded", "jsp", "7.1.3"},
 				{"portletmvc4spring", "embedded", "jsp", "7.2.1"},
-				{"portletmvc4spring", "embedded", "jsp", "7.3.5"},
+				{"portletmvc4spring", "embedded", "jsp", "7.3.6"},
+				{"portletmvc4spring", "embedded", "jsp", "7.4.0"},
 				{"portletmvc4spring", "embedded", "thymeleaf", "7.1.3"},
 				{"portletmvc4spring", "embedded", "thymeleaf", "7.2.1"},
-				{"portletmvc4spring", "embedded", "thymeleaf", "7.3.5"}
+				{"portletmvc4spring", "embedded", "thymeleaf", "7.3.6"},
+				{"portletmvc4spring", "embedded", "thymeleaf", "7.4.0"}
 			});
 	}
 
