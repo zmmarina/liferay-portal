6f4606def8a28adb1737224a4c823d0df0a898ba
balazssk <balazs.safrany-kovalik@liferay.com>
2021-05-03 11:23:20 +0200

    LPS-131525 Override equals and hashCode to enable object comparison

--- portal-impl/src/com/liferay/portal/model/impl/UserGroupGroupRoleImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/UserGroupGroupRoleImpl.java
@@ -14,10 +14,12 @@
 
 package com.liferay.portal.model.impl;
 
+import com.liferay.petra.lang.HashUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.model.UserGroupGroupRole;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserGroupLocalServiceUtil;
@@ -27,6 +29,28 @@ import com.liferay.portal.kernel.service.UserGroupLocalServiceUtil;
  * @author Norbert Kocsis
  */
 public class UserGroupGroupRoleImpl extends UserGroupGroupRoleBaseImpl {
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof UserGroupGroupRole)) {
+			return false;
+		}
+
+		UserGroupGroupRole userGroupGroupRole = (UserGroupGroupRole)object;
+
+		if ((getUserGroupId() == userGroupGroupRole.getUserGroupId()) &&
+			(getGroupId() == userGroupGroupRole.getGroupId()) &&
+			(getRoleId() == userGroupGroupRole.getRoleId())) {
+
+			return true;
+		}
+
+		return false;
+	}
 
 	@Override
 	public Group getGroup() throws PortalException {
@@ -41,6 +65,15 @@ public class UserGroupGroupRoleImpl extends UserGroupGroupRoleBaseImpl {
 	@Override
 	public UserGroup getUserGroup() throws PortalException {
 		return UserGroupLocalServiceUtil.getUserGroup(getUserGroupId());
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = HashUtil.hash(0, getUserGroupId());
+
+		hash = HashUtil.hash(hash, getGroupId());
+
+		return HashUtil.hash(hash, getRoleId());
 	}
 
 }
