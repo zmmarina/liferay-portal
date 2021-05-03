548491ac588d691b42456e843d4e3fb1b9da1dfd
Hugo Huijser <hugo.huijser@liferay.com>
2021-05-03 12:39:06 -0600

    LPS-130806 Add HashMapDictionaryBuilderTest

---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/util/HashMapDictionaryBuilderTest.java
@@ -0,0 +1,187 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Hugo Huijser
+ */
+public class HashMapDictionaryBuilderTest {
+
+	@Test
+	public void testHashMapDictionaryBuilder() {
+		Dictionary<String, Integer> dictionary = new HashMapDictionary<>();
+
+		dictionary.put("One", 1);
+		dictionary.put("Three", 3);
+		dictionary.put("Two", 2);
+
+		Assert.assertEquals(
+			dictionary,
+			HashMapDictionaryBuilder.put(
+				"One", 1
+			).put(
+				"Three", 3
+			).put(
+				"Two", 2
+			).build());
+	}
+
+	@Test
+	public void testNullValues() {
+		Dictionary<String, Object> dictionary =
+			HashMapDictionaryBuilder.<String, Object>put(
+				"Hello", null
+			).build();
+
+		Assert.assertEquals(dictionary.toString(), 1, dictionary.size());
+
+		dictionary = HashMapDictionaryBuilder.<String, Object>put(
+			"Hello", () -> null
+		).build();
+
+		Assert.assertEquals(dictionary.toString(), 0, dictionary.size());
+	}
+
+	@Test
+	public void testPutAll() {
+		Map<String, Integer> map = new HashMap<>();
+
+		map.put("One", 1);
+		map.put("Three", 3);
+		map.put("Two", 2);
+
+		HashMapDictionary<String, Integer> dictionary =
+			new HashMapDictionary<>();
+
+		dictionary.putAll(map);
+
+		Assert.assertEquals(
+			dictionary,
+			HashMapDictionaryBuilder.putAll(
+				map
+			).build());
+	}
+
+	@Test
+	public void testUnsafeFunction() {
+		List<String> list = ListUtil.fromArray("hello  ", "  world");
+
+		Dictionary<String, String> dictionary = new HashMapDictionary<>();
+
+		for (String s : list) {
+			dictionary.put(s, StringUtil.trim(s.toLowerCase()));
+		}
+
+		Assert.assertEquals(
+			dictionary,
+			HashMapDictionaryBuilder.put(
+				list, s -> StringUtil.trim(s.toLowerCase())
+			).build());
+	}
+
+	@Test
+	public void testUnsafeSuppliers() {
+		_testUnsafeSupplierKey(false, 2);
+		_testUnsafeSupplierKey(true, 3);
+
+		_testUnsafeSupplierValue(false, 2);
+		_testUnsafeSupplierValue(true, 3);
+
+		Dictionary<String, Integer> dictionary = new HashMapDictionary<>();
+
+		String s1 = "Hello World";
+
+		String[] array1 = StringUtil.split(s1, ' ');
+
+		dictionary.put(s1, array1.length);
+
+		String s2 = "Hello World Hello World";
+
+		String[] array2 = StringUtil.split(s2, ' ');
+
+		dictionary.put(s2, array2.length);
+
+		Assert.assertEquals(
+			dictionary,
+			HashMapDictionaryBuilder.put(
+				s1,
+				() -> {
+					String[] array = StringUtil.split(s1, ' ');
+
+					return array.length;
+				}
+			).put(
+				s2,
+				() -> {
+					String[] array = StringUtil.split(s2, ' ');
+
+					return array.length;
+				}
+			).build());
+	}
+
+	private void _testUnsafeSupplierKey(
+		boolean allowVegatables, int expectedSize) {
+
+		Dictionary<String, String> dictionary = HashMapDictionaryBuilder.put(
+			"Apple", "Fruit"
+		).put(
+			"Banana", "Fruit"
+		).put(
+			() -> {
+				if (allowVegatables) {
+					return "Carrot";
+				}
+
+				return null;
+			},
+			"Vegetable"
+		).build();
+
+		Assert.assertEquals(
+			dictionary.toString(), expectedSize, dictionary.size());
+	}
+
+	private void _testUnsafeSupplierValue(
+		boolean allowVegatables, int expectedSize) {
+
+		Dictionary<String, String> dictionary = HashMapDictionaryBuilder.put(
+			"Apple", "Fruit"
+		).put(
+			"Banana", "Fruit"
+		).put(
+			"Carrot",
+			() -> {
+				if (allowVegatables) {
+					return "Vegetable";
+				}
+
+				return null;
+			}
+		).build();
+
+		Assert.assertEquals(
+			dictionary.toString(), expectedSize, dictionary.size());
+	}
+
+}
