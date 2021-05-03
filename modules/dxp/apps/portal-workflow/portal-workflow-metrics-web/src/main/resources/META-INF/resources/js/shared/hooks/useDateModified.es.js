54d2392e48eff9915e3dc9677c95579a0ce0eba3
ramon <ramon.maciel@liferay.com>
2021-05-03 13:53:13 -0300

    LPS-130968 Add a specific way to fetch metrics calculated

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/hooks/useDateModified.es.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {useCallback, useContext, useState} from 'react';
+
+import {AppContext} from '../../components/AppContext.es';
+
+const useDateModified = ({
+	admin = false,
+	callback = (data) => data,
+	params = {},
+	processId,
+}) => {
+	const {getClient} = useContext(AppContext);
+	const [dateModified, setDateModified] = useState(null);
+
+	const client = getClient(admin);
+	const queryParamsStr = JSON.stringify(params);
+
+	const url = `processes/${processId}/last-sla-result`;
+
+	const fetchData = useCallback(
+		() =>
+			client.get(url, {params}).then(({data, status}) => {
+				if (status === 200) {
+					setDateModified(data.dateModified);
+
+					return callback(data.dateModified);
+				}
+				setDateModified(null);
+
+				return callback(null);
+			}),
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[client, queryParamsStr, url]
+	);
+
+	return {
+		dateModified,
+		fetchData,
+	};
+};
+
+export {useDateModified};
