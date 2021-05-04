3bf82af5ff01c4a7fc37855979e94b9dbdb24cc2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-05-04 11:47:32 +0100

    LPS-102711 Convert sharing to the new site settings configuration pattern

---
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/portal/settings/configuration/admin/display/SharingSiteSettingsConfigurationScreenContributor.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.sharing.configuration.SharingConfiguration;
+import com.liferay.sharing.configuration.SharingConfigurationFactory;
+import com.liferay.sharing.web.internal.constants.SharingWebKeys;
+import com.liferay.site.settings.configuration.admin.display.SiteSettingsConfigurationScreenContributor;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = SiteSettingsConfigurationScreenContributor.class)
+public class SharingSiteSettingsConfigurationScreenContributor
+	implements SiteSettingsConfigurationScreenContributor {
+
+	@Override
+	public String getCategoryKey() {
+		return "sharing";
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/site_settings/sharing.jsp";
+	}
+
+	@Override
+	public String getKey() {
+		return "site-configuration-sharing";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return LanguageUtil.get(locale, "sharing");
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public boolean isVisible() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		SharingConfiguration groupSharingConfiguration =
+			_sharingConfigurationFactory.getGroupSharingConfiguration(
+				themeDisplay.getSiteGroup());
+
+		return groupSharingConfiguration.isAvailable();
+	}
+
+	@Override
+	public void setAttributes(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		httpServletRequest.setAttribute(
+			SharingWebKeys.GROUP_SHARING_CONFIGURATION,
+			_sharingConfigurationFactory.getGroupSharingConfiguration(
+				themeDisplay.getSiteGroup()));
+	}
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.sharing.web)")
+	private ServletContext _servletContext;
+
+	@Reference
+	private SharingConfigurationFactory _sharingConfigurationFactory;
+
+}
