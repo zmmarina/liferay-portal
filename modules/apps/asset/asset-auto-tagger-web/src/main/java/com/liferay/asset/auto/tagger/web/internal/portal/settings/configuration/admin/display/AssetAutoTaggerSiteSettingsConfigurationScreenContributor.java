d2645c0cc87a22943b0f3760afb1364d19616818
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-05-04 13:37:51 +0100

    LPS-102711 Convert asset auto tagger to the new site settings configuration pattern

---
+++ modules/apps/asset/asset-auto-tagger-web/src/main/java/com/liferay/asset/auto/tagger/web/internal/portal/settings/configuration/admin/display/AssetAutoTaggerSiteSettingsConfigurationScreenContributor.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.auto.tagger.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.asset.auto.tagger.configuration.AssetAutoTaggerConfiguration;
+import com.liferay.asset.auto.tagger.configuration.AssetAutoTaggerConfigurationFactory;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.site.settings.configuration.admin.display.SiteSettingsConfigurationScreenContributor;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = SiteSettingsConfigurationScreenContributor.class)
+public class AssetAutoTaggerSiteSettingsConfigurationScreenContributor
+	implements SiteSettingsConfigurationScreenContributor {
+
+	@Override
+	public String getCategoryKey() {
+		return "assets";
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/site_settings/asset_auto_tagger.jsp";
+	}
+
+	@Override
+	public String getKey() {
+		return "site-configuration-asset-auto-tagger";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return LanguageUtil.get(
+			ResourceBundleUtil.getBundle(locale, getClass()),
+			"asset-auto-tagging");
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public boolean isVisible() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		AssetAutoTaggerConfiguration assetAutoTaggerConfiguration =
+			_assetAutoTaggerConfigurationFactory.
+				getGroupAssetAutoTaggerConfiguration(
+					themeDisplay.getSiteGroup());
+
+		return assetAutoTaggerConfiguration.isAvailable();
+	}
+
+	@Override
+	public void setAttributes(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		httpServletRequest.setAttribute(
+			AssetAutoTaggerConfiguration.class.getName(),
+			_assetAutoTaggerConfigurationFactory.
+				getGroupAssetAutoTaggerConfiguration(
+					themeDisplay.getSiteGroup()));
+	}
+
+	@Reference
+	private AssetAutoTaggerConfigurationFactory
+		_assetAutoTaggerConfigurationFactory;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.auto.tagger.web)"
+	)
+	private ServletContext _servletContext;
+
+}
