409b46f90fa66ac33973f8e5f33e648aef2d5312
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-05-05 10:30:30 -0300

    LPS-130976 Add method to get the last process SLA result

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/resource/v1_0/SLAResultResourceImpl.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.internal.resource.v1_0;
+
+import com.liferay.portal.search.engine.adapter.search.SearchRequestExecutor;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.sort.SortOrder;
+import com.liferay.portal.search.sort.Sorts;
+import com.liferay.portal.workflow.metrics.rest.dto.v1_0.SLAResult;
+import com.liferay.portal.workflow.metrics.rest.internal.dto.v1_0.util.SLAResultUtil;
+import com.liferay.portal.workflow.metrics.rest.internal.resource.exception.NoSuchSLAResultException;
+import com.liferay.portal.workflow.metrics.rest.resource.v1_0.SLAResultResource;
+import com.liferay.portal.workflow.metrics.search.index.name.WorkflowMetricsIndexNameBuilder;
+import com.liferay.portal.workflow.metrics.service.WorkflowMetricsSLADefinitionLocalService;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	properties = "OSGI-INF/liferay/rest/v1_0/sla-result.properties",
+	scope = ServiceScope.PROTOTYPE, service = SLAResultResource.class
+)
+public class SLAResultResourceImpl extends BaseSLAResultResourceImpl {
+
+	@Override
+	public SLAResult getProcessLastSLAResult(Long processId) throws Exception {
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		searchSearchRequest.addSorts(
+			_sorts.field("lastCheckDate", SortOrder.DESC));
+
+		searchSearchRequest.setIndexNames(
+			_slaInstanceResultWorkflowMetricsIndexNameBuilder.getIndexName(
+				contextCompany.getCompanyId()));
+
+		BooleanQuery booleanQuery = _queries.booleanQuery();
+
+		BooleanQuery filterBooleanQuery = _queries.booleanQuery();
+
+		searchSearchRequest.setQuery(
+			booleanQuery.addFilterQueryClauses(
+				filterBooleanQuery.addMustQueryClauses(
+					_queries.term("deleted", false),
+					_queries.term("processId", processId)),
+				filterBooleanQuery.addMustNotQueryClauses(
+					_queries.term("instanceId", 0))));
+
+		searchSearchRequest.setQuery(
+			booleanQuery.addFilterQueryClauses(filterBooleanQuery));
+
+		searchSearchRequest.setSize(1);
+
+		return Stream.of(
+			_searchRequestExecutor.executeSearchRequest(searchSearchRequest)
+		).map(
+			SearchSearchResponse::getSearchHits
+		).map(
+			SearchHits::getSearchHits
+		).flatMap(
+			List::stream
+		).map(
+			SearchHit::getDocument
+		).findFirst(
+		).map(
+			document -> SLAResultUtil.toSLAResult(
+				document,
+				_workflowMetricsSLADefinitionLocalService::
+					fetchWorkflowMetricsSLADefinition)
+		).orElseThrow(
+			() -> new NoSuchSLAResultException(
+				"No sla result exists with process ID " + processId)
+		);
+	}
+
+	@Reference
+	private Queries _queries;
+
+	@Reference
+	private SearchRequestExecutor _searchRequestExecutor;
+
+	@Reference(
+		target = "(workflow.metrics.index.entity.name=sla-instance-result)"
+	)
+	private WorkflowMetricsIndexNameBuilder
+		_slaInstanceResultWorkflowMetricsIndexNameBuilder;
+
+	@Reference
+	private Sorts _sorts;
+
+	@Reference
+	private WorkflowMetricsSLADefinitionLocalService
+		_workflowMetricsSLADefinitionLocalService;
+
+}
