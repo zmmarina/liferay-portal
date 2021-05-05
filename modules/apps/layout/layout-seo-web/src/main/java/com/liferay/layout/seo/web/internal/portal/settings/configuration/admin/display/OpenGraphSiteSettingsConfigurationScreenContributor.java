db03d90e69d63f8e700657a3029c70a6ec8177b4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-05-05 17:12:00 +0100

    LPS-102711 Convert open graph to the new site settings configuration pattern

--- modules/apps/layout/layout-seo-web/src/main/java/com/liferay/layout/seo/web/internal/portal/settings/configuration/admin/display/OpenGraphSiteSettingsConfigurationScreenContributor.java
+++ modules/apps/layout/layout-seo-web/src/main/java/com/liferay/layout/seo/web/internal/portal/settings/configuration/admin/display/OpenGraphSiteSettingsConfigurationScreenContributor.java
@@ -12,13 +12,10 @@
  * details.
  */
 
-package com.liferay.layout.seo.web.internal.frontend.taglib.form.navigator;
+package com.liferay.layout.seo.web.internal.portal.settings.configuration.admin.display;
 
 import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.document.library.util.DLURLHelper;
-import com.liferay.frontend.taglib.form.navigator.BaseJSPFormNavigatorEntry;
-import com.liferay.frontend.taglib.form.navigator.FormNavigatorEntry;
-import com.liferay.frontend.taglib.form.navigator.constants.FormNavigatorConstants;
 import com.liferay.item.selector.ItemSelector;
 import com.liferay.layout.seo.open.graph.OpenGraphConfiguration;
 import com.liferay.layout.seo.service.LayoutSEOSiteLocalService;
@@ -28,13 +25,14 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-
-import java.io.IOException;
+import com.liferay.site.settings.configuration.admin.display.SiteSettingsConfigurationScreenContributor;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -52,61 +50,45 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Alicia García
  */
-@Component(
-	property = "form.navigator.entry.order:Integer=10",
-	service = FormNavigatorEntry.class
-)
-public class SiteOpenGraphFormNavigatorEntry
-	extends BaseJSPFormNavigatorEntry<Group> {
+@Component(service = SiteSettingsConfigurationScreenContributor.class)
+public class OpenGraphSiteSettingsConfigurationScreenContributor
+	implements SiteSettingsConfigurationScreenContributor {
 
 	@Override
 	public String getCategoryKey() {
-		return FormNavigatorConstants.CATEGORY_KEY_SITES_GENERAL;
+		return "pages";
 	}
 
 	@Override
-	public String getFormNavigatorId() {
-		return FormNavigatorConstants.FORM_NAVIGATOR_ID_SITES;
+	public String getJspPath() {
+		return "/site_settings/open_graph.jsp";
 	}
 
 	@Override
 	public String getKey() {
-		return "open-graph";
+		return "site-configuration-open-graph";
 	}
 
 	@Override
-	public String getLabel(Locale locale) {
+	public String getName(Locale locale) {
 		return LanguageUtil.get(_getResourceBundle(locale), "open-graph");
 	}
 
 	@Override
-	public void include(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException {
-
-		PortletRequest portletRequest =
-			(PortletRequest)httpServletRequest.getAttribute(
-				JavaConstants.JAVAX_PORTLET_REQUEST);
-		PortletResponse portletResponse =
-			(PortletResponse)httpServletRequest.getAttribute(
-				JavaConstants.JAVAX_PORTLET_RESPONSE);
-
-		httpServletRequest.setAttribute(
-			OpenGraphSettingsDisplayContext.class.getName(),
-			new OpenGraphSettingsDisplayContext(
-				_dlAppService, _dlurlHelper, httpServletRequest, _itemSelector,
-				_layoutSEOSiteLocalService,
-				_portal.getLiferayPortletRequest(portletRequest),
-				_portal.getLiferayPortletResponse(portletResponse),
-				_openGraphConfiguration));
-
-		super.include(httpServletRequest, httpServletResponse);
+	public ServletContext getServletContext() {
+		return _servletContext;
 	}
 
 	@Override
-	public boolean isVisible(User user, Group group) {
+	public boolean isVisible() {
 		try {
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+			Group group = themeDisplay.getSiteGroup();
+
 			if ((group == null) || group.isCompany() ||
 				!_openGraphConfiguration.isOpenGraphEnabled(
 					_companyLocalService.getCompany(group.getCompanyId()))) {
@@ -124,17 +106,28 @@ public class SiteOpenGraphFormNavigatorEntry
 	}
 
 	@Override
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.layout.seo.web)",
-		unbind = "-"
-	)
-	public void setServletContext(ServletContext servletContext) {
-		super.setServletContext(servletContext);
-	}
+	public void setAttributes(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
 
-	@Override
-	protected String getJspPath() {
-		return "/site_settings/open_graph.jsp";
+		SiteSettingsConfigurationScreenContributor.super.setAttributes(
+			httpServletRequest, httpServletResponse);
+
+		PortletRequest portletRequest =
+			(PortletRequest)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST);
+		PortletResponse portletResponse =
+			(PortletResponse)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+		httpServletRequest.setAttribute(
+			OpenGraphSettingsDisplayContext.class.getName(),
+			new OpenGraphSettingsDisplayContext(
+				_dlAppService, _dlurlHelper, httpServletRequest, _itemSelector,
+				_layoutSEOSiteLocalService,
+				_portal.getLiferayPortletRequest(portletRequest),
+				_portal.getLiferayPortletResponse(portletResponse),
+				_openGraphConfiguration));
 	}
 
 	private ResourceBundle _getResourceBundle(Locale locale) {
@@ -143,7 +136,7 @@ public class SiteOpenGraphFormNavigatorEntry
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
-		SiteOpenGraphFormNavigatorEntry.class);
+		OpenGraphSiteSettingsConfigurationScreenContributor.class);
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
@@ -165,5 +158,11 @@ public class SiteOpenGraphFormNavigatorEntry
 
 	@Reference
 	private Portal _portal;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.layout.seo.web)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
 
 }
