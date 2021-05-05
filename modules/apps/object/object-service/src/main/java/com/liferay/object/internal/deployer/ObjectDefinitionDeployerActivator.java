51a4f1d57ffe446c3d8247a3d3c5b69cc2389c7a
tina.tian <tina.tian@liferay.com>
2021-05-05 15:10:21 -0700

    LPS-130662 No longer need initialization here, ObjectDefinition entries in database would be initialized when ObjectDefinitionDeployer is registered

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/deployer/ObjectDefinitionDeployerActivator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/deployer/ObjectDefinitionDeployerActivator.java
@@ -19,9 +19,6 @@ import com.liferay.object.model.ObjectField;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
-import com.liferay.portal.kernel.cluster.ClusterInvokeThreadLocal;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.dependency.manager.DependencyManagerSyncUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
@@ -35,12 +32,10 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.FutureTask;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -52,38 +47,9 @@ public class ObjectDefinitionDeployerActivator {
 
 	@Activate
 	protected void activate(BundleContext bundleContext) throws Exception {
-		List<ObjectDefinition> objectDefinitions =
-			_objectDefinitionLocalService.getObjectDefinitions(
-				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		if (!objectDefinitions.isEmpty()) {
-			FutureTask<Void> futureTask = new FutureTask<>(
-				() -> {
-					ClusterInvokeThreadLocal.setEnabled(false);
-
-					objectDefinitions.forEach(
-						_objectDefinitionLocalService::deployObjectDefinition);
-				},
-				null);
-
-			Thread thread = new Thread(
-				futureTask, "Object Definition Deployer Activator");
-
-			thread.setDaemon(true);
-
-			thread.start();
-
-			DependencyManagerSyncUtil.registerSyncFuture(futureTask);
-		}
-
 		if (false) {
 			_addSampleObjectDefinition();
 		}
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_objectDefinitionLocalService.undeployObjectDefinitions();
 	}
 
 	private void _addSampleObjectDefinition() throws Exception {
