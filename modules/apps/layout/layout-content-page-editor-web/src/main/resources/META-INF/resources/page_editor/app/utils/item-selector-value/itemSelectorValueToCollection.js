b84f10d0779fc797f6b14c0f018ca069a1bd0c05
Victor Galan <victor.galan@liferay.com>
2021-05-06 10:22:00 +0200

    LPS-131454 Separate each collection return type

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/item-selector-value/itemSelectorValueToCollection.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/item-selector-value/itemSelectorValueToCollection.js
@@ -12,6 +12,15 @@
  * details.
  */
 
+const RETURN_TYPES = {
+	assetList:
+		'com.liferay.item.selector.criteria.InfoListItemSelectorReturnType',
+	infoItemRelatedList:
+		'com.liferay.info.list.provider.item.selector.criterion.InfoItemRelatedListProviderItemSelectorReturnType',
+	infoList:
+		'com.liferay.info.list.provider.item.selector.criterion.InfoListProviderItemSelectorReturnType',
+};
+
 export default function itemSelectorValueToCollection(collection) {
 	const {
 		classNameId,
@@ -19,17 +28,40 @@ export default function itemSelectorValueToCollection(collection) {
 		itemSubtype,
 		itemType,
 		key,
-		returnType,
+		returnType: type,
+		sourceItemType,
 		title,
 	} = collection;
 
-	return {
-		classNameId,
-		classPK,
-		itemSubtype,
-		itemType,
-		key,
-		title,
-		type: returnType,
-	};
+	switch (type) {
+		case RETURN_TYPES.assetList:
+			return {
+				classNameId,
+				classPK,
+				itemSubtype,
+				itemType,
+				title,
+				type,
+			};
+
+		case RETURN_TYPES.infoItemRelatedList:
+			return {
+				itemType,
+				key,
+				sourceItemType,
+				title,
+				type,
+			};
+		case RETURN_TYPES.infoList:
+			return {
+				itemSubtype,
+				itemType,
+				key,
+				title,
+				type,
+			};
+
+		default:
+			return {...collection};
+	}
 }
