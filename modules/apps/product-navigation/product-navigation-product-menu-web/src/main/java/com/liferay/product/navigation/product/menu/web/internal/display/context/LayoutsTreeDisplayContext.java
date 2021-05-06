14a0aa6456ed400c6eea23d025dbf055525b8ed7
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-05-06 15:28:05 +0100

    LPS-131773 We don't need to add the parameter using PortletQName.PUBLIC_RENDER_PARAMETER_NAMESPACE, since the selPlid is a public render parameter of GroupPagesPortlet, in this case it should be enough if we set the selPlid when we are creating the url

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/display/context/LayoutsTreeDisplayContext.java
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/display/context/LayoutsTreeDisplayContext.java
@@ -69,22 +69,34 @@ public class LayoutsTreeDisplayContext {
 	}
 
 	public String getAddChildCollectionURLTemplate() throws Exception {
+		PortletURL addChildCollectionURL = getAddCollectionLayoutURL();
+
+		if (addChildCollectionURL == null) {
+			return StringPool.BLANK;
+		}
+
 		return StringBundler.concat(
-			getAddCollectionLayoutURL(), StringPool.AMPERSAND,
+			addChildCollectionURL, StringPool.AMPERSAND,
 			PortletQName.PUBLIC_RENDER_PARAMETER_NAMESPACE, "selPlid={plid}");
 	}
 
 	public String getAddChildURLTemplate() throws Exception {
+		PortletURL addLayoutURL = getAddLayoutURL();
+
+		if (addLayoutURL == null) {
+			return StringPool.BLANK;
+		}
+
 		return StringBundler.concat(
-			getAddLayoutURL(), StringPool.AMPERSAND,
+			addLayoutURL, StringPool.AMPERSAND,
 			PortletQName.PUBLIC_RENDER_PARAMETER_NAMESPACE, "selPlid={plid}");
 	}
 
-	public String getAddCollectionLayoutURL() throws Exception {
+	public PortletURL getAddCollectionLayoutURL() throws Exception {
 		Group scopeGroup = _themeDisplay.getScopeGroup();
 
 		if (scopeGroup.isStaged() && !scopeGroup.isStagingGroup()) {
-			return StringPool.BLANK;
+			return null;
 		}
 
 		Layout layout = _themeDisplay.getLayout();
@@ -103,14 +115,14 @@ public class LayoutsTreeDisplayContext {
 			"groupId", _themeDisplay.getSiteGroupId()
 		).setParameter(
 			"privateLayout", isPrivateLayout()
-		).buildString();
+		).build();
 	}
 
-	public String getAddLayoutURL() throws Exception {
+	public PortletURL getAddLayoutURL() throws Exception {
 		Group scopeGroup = _themeDisplay.getScopeGroup();
 
 		if (scopeGroup.isStaged() && !scopeGroup.isStagingGroup()) {
-			return StringPool.BLANK;
+			return null;
 		}
 
 		Layout layout = _themeDisplay.getLayout();
@@ -129,7 +141,7 @@ public class LayoutsTreeDisplayContext {
 			"groupId", _themeDisplay.getSiteGroupId()
 		).setParameter(
 			"privateLayout", isPrivateLayout()
-		).buildString();
+		).build();
 	}
 
 	public String getAdministrationPortletURL() {
@@ -142,7 +154,7 @@ public class LayoutsTreeDisplayContext {
 		).buildString();
 	}
 
-	public String getConfigureLayoutSetURL() throws PortalException {
+	public PortletURL getConfigureLayoutSetURL() throws PortalException {
 		Layout layout = _themeDisplay.getLayout();
 
 		return PortletURLBuilder.create(
@@ -159,7 +171,7 @@ public class LayoutsTreeDisplayContext {
 			"groupId", _themeDisplay.getScopeGroupId()
 		).setParameter(
 			"privateLayout", isPrivateLayout()
-		).buildString();
+		).build();
 	}
 
 	public String getConfigureLayoutURL() throws PortalException {
@@ -260,12 +272,12 @@ public class LayoutsTreeDisplayContext {
 	public Map<String, Object> getPageTypeSelectorData() throws Exception {
 		return HashMapBuilder.<String, Object>put(
 			"addCollectionLayoutURL",
-			_addDefaultselPlidParam(getAddCollectionLayoutURL())
+			_addDefaultSelPlidParam(getAddCollectionLayoutURL())
 		).put(
-			"addLayoutURL", _addDefaultselPlidParam(getAddLayoutURL())
+			"addLayoutURL", _addDefaultSelPlidParam(getAddLayoutURL())
 		).put(
 			"configureLayoutSetURL",
-			_addDefaultselPlidParam(getConfigureLayoutSetURL())
+			_addDefaultSelPlidParam(getConfigureLayoutSetURL())
 		).put(
 			"namespace", getNamespace()
 		).put(
@@ -359,11 +371,15 @@ public class LayoutsTreeDisplayContext {
 			layout.isPrivateLayout());
 	}
 
-	private String _addDefaultselPlidParam(String portletUrl) {
-		return StringBundler.concat(
-			portletUrl, StringPool.AMPERSAND,
-			PortletQName.PUBLIC_RENDER_PARAMETER_NAMESPACE, "selPlid=",
-			LayoutConstants.DEFAULT_PLID);
+	private String _addDefaultSelPlidParam(PortletURL portletURL) {
+		if (portletURL == null) {
+			return StringPool.BLANK;
+		}
+
+		portletURL.setParameter(
+			"selPlid", String.valueOf(LayoutConstants.DEFAULT_PLID));
+
+		return portletURL.toString();
 	}
 
 	private Long _groupId;
