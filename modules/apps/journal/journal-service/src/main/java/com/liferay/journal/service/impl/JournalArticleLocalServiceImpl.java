533df3f2d9171ee74a2e6ebb68469b2b6eb56d70
jesseyeh-liferay <jesse.yeh@liferay.com>
2021-05-07 16:23:06 -0700

    LPS-131901 Change the Web Content review notification link to go directly to the Web Content itself

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7136,8 +7136,13 @@ public class JournalArticleLocalServiceImpl
 			String portletId = PortletProviderUtil.getPortletId(
 				JournalArticle.class.getName(), PortletProvider.Action.EDIT);
 
+			String namespace = _portal.getPortletNamespace(portletId);
+
 			String articleURL = _portal.getControlPanelFullURL(
 				article.getGroupId(), portletId, null);
+
+			articleURL = _http.addParameter(
+				articleURL, namespace + "mvcPath", "/edit_article.jsp");
 
 			articleURL = buildArticleURL(
 				articleURL, article.getGroupId(), article.getFolderId(),
