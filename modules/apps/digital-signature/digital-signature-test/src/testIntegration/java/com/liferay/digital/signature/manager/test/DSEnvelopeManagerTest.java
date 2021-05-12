9c2196efb1849e86991a5b2520b9ce7daf7424bb
Brian Chan <brian.chan@liferay.com>
2021-05-12 10:10:01 -0700

    LPS-131649 we only have 1 document

--- modules/apps/digital-signature/digital-signature-test/src/testIntegration/java/com/liferay/digital/signature/manager/test/DSEnvelopeManagerTest.java
+++ modules/apps/digital-signature/digital-signature-test/src/testIntegration/java/com/liferay/digital/signature/manager/test/DSEnvelopeManagerTest.java
@@ -59,9 +59,9 @@ public class DSEnvelopeManagerTest {
 					data = Base64.encode(
 						FileUtil.getBytes(
 							clazz.getResourceAsStream(
-								"dependencies/Document_1.pdf")));
+								"dependencies/Document.pdf")));
 					dsDocumentId = "1";
-					name = "Document 1";
+					name = "Document";
 				}
 			});
 
