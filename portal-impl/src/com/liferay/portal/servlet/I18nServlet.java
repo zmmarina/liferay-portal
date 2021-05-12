b296f788737f9139a89d3a06b2ac6ddaf7d7cf4a
Georgel Pop <georgel.pop@liferay.com>
2021-05-12 14:48:10 +0200

    LPS-131709 Fix regression of LPS-130105 decode URL on I18nServlet.java only when not already encoded, because only when forwarding with request dispatcher in VirtualHostFilter.java (check forwardURLString variable) Jboss is returning already an encoded getPathInfo() and we should not encode it again

--- portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -24,10 +24,9 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.portlet.FriendlyURLResolverRegistryUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
-import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -216,35 +215,10 @@ public class I18nServlet extends HttpServlet {
 			}
 		}
 
-		String friendlyURLSeparator = StringPool.BLANK;
-		int friendlyURLSeparatorIndex = -1;
+		String decodePath = HttpUtil.decodePath(path);
 
-		for (String urlSeparator :
-				FriendlyURLResolverRegistryUtil.getURLSeparators()) {
-
-			friendlyURLSeparatorIndex = path.indexOf(urlSeparator);
-
-			if (friendlyURLSeparatorIndex != -1) {
-				friendlyURLSeparator = urlSeparator;
-
-				break;
-			}
-		}
-
-		String redirect = null;
-
-		if (friendlyURLSeparatorIndex == -1) {
-			redirect = FriendlyURLNormalizerUtil.normalizeWithEncoding(path);
-		}
-		else {
-			redirect = StringBundler.concat(
-				path.substring(0, friendlyURLSeparatorIndex),
-				friendlyURLSeparator,
-				FriendlyURLNormalizerUtil.normalizeWithEncoding(
-					path.substring(
-						friendlyURLSeparatorIndex +
-							friendlyURLSeparator.length())));
-		}
+		String redirect =
+			decodePath.equals(path) ? HttpUtil.encodePath(path) : path;
 
 		if (_log.isDebugEnabled()) {
 			_log.debug("Redirect " + redirect);
