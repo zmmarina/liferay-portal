bc4a448088d696fbadd4492a7f85273670fab5d3
shuyangzhou <shuyang.zhou@liferay.com>
2021-05-16 23:31:07 -0700

    LPS-131709 SF

--- portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -215,10 +215,11 @@ public class I18nServlet extends HttpServlet {
 			}
 		}
 
-		String decodePath = HttpUtil.decodePath(path);
+		String redirect = path;
 
-		String redirect =
-			decodePath.equals(path) ? HttpUtil.encodePath(path) : path;
+		if (path.equals(HttpUtil.decodePath(path))) {
+			redirect = HttpUtil.encodePath(path);
+		}
 
 		if (_log.isDebugEnabled()) {
 			_log.debug("Redirect " + redirect);
