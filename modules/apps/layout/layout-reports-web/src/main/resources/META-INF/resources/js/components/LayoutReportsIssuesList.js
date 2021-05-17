eebd14a78f7b63ea763d3ac3ccc87c763b3a2692
Sandro Chinea <sandrochinea@gmail.com>
2021-05-17 12:04:42 +0100

    LPS-132232 Move Section markup to new component

--- modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/js/components/LayoutReportsIssuesList.js
+++ modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/js/components/LayoutReportsIssuesList.js
@@ -52,61 +52,62 @@ export default function LayoutReportsIssuesList() {
 			)}
 
 			<ClayPanel.Group className="panel-group-flush panel-group-sm">
-				{layoutReportsIssues?.map((section) => {
-					let sectionTotal = section.total;
-
-					if (sectionTotal > 100) {
-						sectionTotal = '+100';
-					}
-
-					return (
-						<ClayPanel
-							collapsable
-							collapseClassNames="c-mb-4 c-mt-3"
-							displayTitle={
-								<span className="c-inner" tabIndex="-1">
-									<ClayPanel.Title>
-										<ClayLayout.ContentRow>
-											<ClayLayout.ContentCol
-												className="align-self-center panel-title"
-												expand
-											>
-												{section.title}
-											</ClayLayout.ContentCol>
-											<ClayLayout.ContentCol>
-												<ClayBadge
-													displayType={
-														sectionTotal === 0
-															? 'success'
-															: 'info'
-													}
-													label={sectionTotal}
-												/>
-											</ClayLayout.ContentCol>
-										</ClayLayout.ContentRow>
-									</ClayPanel.Title>
-								</span>
-							}
-							displayType="unstyled"
-							key={section.key}
-							showCollapseIcon={true}
-						>
-							<ClayPanel.Body>
-								{sectionTotal === '0' && (
-									<div className="text-secondary">
-										{Liferay.Util.sub(
-											Liferay.Language.get(
-												'there-are-no-x-related-issues'
-											),
-											section.title
-										)}
-									</div>
-								)}
-							</ClayPanel.Body>
-						</ClayPanel>
-					);
-				})}
+				{layoutReportsIssues?.map((section) => (
+					<Section key={section.key} section={section} />
+				))}
 			</ClayPanel.Group>
 		</div>
 	);
 }
+
+const Section = ({section}) => {
+	let sectionTotal = section.total;
+
+	if (sectionTotal > 100) {
+		sectionTotal = '+100';
+	}
+
+	return (
+		<ClayPanel
+			collapsable
+			collapseClassNames="c-mb-4 c-mt-3"
+			displayTitle={
+				<span className="c-inner" tabIndex="-1">
+					<ClayPanel.Title>
+						<ClayLayout.ContentRow>
+							<ClayLayout.ContentCol
+								className="align-self-center panel-title"
+								expand
+							>
+								{section.title}
+							</ClayLayout.ContentCol>
+							<ClayLayout.ContentCol>
+								<ClayBadge
+									displayType={
+										sectionTotal === 0 ? 'success' : 'info'
+									}
+									label={sectionTotal}
+								/>
+							</ClayLayout.ContentCol>
+						</ClayLayout.ContentRow>
+					</ClayPanel.Title>
+				</span>
+			}
+			displayType="unstyled"
+			showCollapseIcon={true}
+		>
+			<ClayPanel.Body>
+				{sectionTotal === '0' && (
+					<div className="text-secondary">
+						{Liferay.Util.sub(
+							Liferay.Language.get(
+								'there-are-no-x-related-issues'
+							),
+							section.title
+						)}
+					</div>
+				)}
+			</ClayPanel.Body>
+		</ClayPanel>
+	);
+};
