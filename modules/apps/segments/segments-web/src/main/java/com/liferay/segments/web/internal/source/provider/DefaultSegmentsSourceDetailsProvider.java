933090b7991b29387acd94bcfed7b62ef80e8ab4
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-05-18 07:43:39 +0200

    LPS-121105 Adding implementations for AC and CE

---
+++ modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/source/provider/DefaultSegmentsSourceDetailsProvider.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.web.internal.source.provider;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.segments.constants.SegmentsEntryConstants;
+import com.liferay.segments.source.provider.SegmentsSourceDetailsProvider;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	property = "segments.source=" + SegmentsEntryConstants.SOURCE_DEFAULT,
+	service = SegmentsSourceDetailsProvider.class
+)
+public class DefaultSegmentsSourceDetailsProvider
+	implements SegmentsSourceDetailsProvider {
+
+	@Override
+	public String getIconSrc() {
+		return _servletContext.getContextPath() + "/assets/ce-icon.svg";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "source.ce");
+	}
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.segments.web)")
+	private ServletContext _servletContext;
+
+}
