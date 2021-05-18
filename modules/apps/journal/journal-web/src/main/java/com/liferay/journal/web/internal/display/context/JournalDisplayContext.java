1e191156ec73857c0f71cc5ebd7f67eeb3ba6039
Daniel Couso <daniel.couso@liferay.com>
2021-05-18 09:31:56 +0200

    LPS-132376 Fix compile errors and remove redundant code left after reverting

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1182,10 +1182,6 @@ public class JournalDisplayContext {
 			searchContext.setAttribute("showNonindexable", Boolean.TRUE);
 		}
 
-		searchContext.setEnd(end);
-		searchContext.setFolderIds(folderIds);
-		searchContext.setStart(start);
-
 		QueryConfig queryConfig = searchContext.getQueryConfig();
 
 		queryConfig.setHighlightEnabled(false);
