fb4f62a6f472acb2ca1e541ec8ca2b01884d7f91
Guilherme Camacho <guilherme.camacho@liferay.com>
2021-05-18 18:51:01 -0300

    LPS-128822 Write unit tests

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/storage/constants/FieldConstantsTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.storage.constants;
+
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.io.Serializable;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Guilherme Camacho
+ */
+public class FieldConstantsTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testGetSerializableReturnDoubleWithLocaleBR() {
+		Serializable serializable = FieldConstants.getSerializable(
+			LocaleUtil.BRAZIL, LocaleUtil.BRAZIL, FieldConstants.DOUBLE, "3,0");
+
+		Assert.assertTrue(serializable instanceof Double);
+
+		Assert.assertEquals(Double.valueOf(3.0), (Double)serializable);
+	}
+
+	@Test
+	public void testGetSerializableReturnDoubleWithLocaleUS() {
+		Serializable serializable = FieldConstants.getSerializable(
+			LocaleUtil.US, LocaleUtil.US, FieldConstants.DOUBLE, "3.0");
+
+		Assert.assertTrue(serializable instanceof Double);
+
+		Assert.assertEquals(Double.valueOf(3.0), (Double)serializable);
+	}
+
+	@Test
+	public void testGetSerializableReturnIntegerWithLocaleBR() {
+		Serializable serializable = FieldConstants.getSerializable(
+			LocaleUtil.BRAZIL, LocaleUtil.BRAZIL, FieldConstants.INTEGER, "3");
+
+		Assert.assertTrue(serializable instanceof Integer);
+
+		Assert.assertEquals(Integer.valueOf(3), (Integer)serializable);
+	}
+
+	@Test
+	public void testGetSerializableReturnIntegerWithLocaleUS() {
+		Serializable serializable = FieldConstants.getSerializable(
+			LocaleUtil.US, LocaleUtil.US, FieldConstants.INTEGER, "3");
+
+		Assert.assertTrue(serializable instanceof Integer);
+
+		Assert.assertEquals(Integer.valueOf(3), (Integer)serializable);
+	}
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/util/NumberUtilTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.util;
+
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Guilherme Camacho
+ */
+public class NumberUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testGetDecimalSeparatorIndexUsingValueWithComma() {
+		Assert.assertEquals(1, NumberUtil.getDecimalSeparatorIndex("2,0"));
+	}
+
+	@Test
+	public void testGetDecimalSeparatorIndexUsingValueWithDot() {
+		Assert.assertEquals(1, NumberUtil.getDecimalSeparatorIndex("2.0"));
+	}
+
+	@Test
+	public void testGetDecimalSeparatorIndexUsingValueWithNoSeparator() {
+		Assert.assertEquals(-1, NumberUtil.getDecimalSeparatorIndex("2"));
+	}
+
+	@Test
+	public void testGetDecimalSeparatorIndexWithEmptyValue() {
+		Assert.assertEquals(-1, NumberUtil.getDecimalSeparatorIndex(""));
+	}
+
+}
