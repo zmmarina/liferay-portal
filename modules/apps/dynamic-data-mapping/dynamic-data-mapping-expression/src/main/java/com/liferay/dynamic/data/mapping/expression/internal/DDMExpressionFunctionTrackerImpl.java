63501cf9bb9c34f9c2064cfbd827f7ba58ce2114
Brian Chan <brian.chan@liferay.com>
2021-05-18 20:29:49 -0700

    Revert "LPS-132174 Rename"
    This reverts commit 5d6595e7c53276024e5da33e19af5dc66068cc97.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-expression/src/main/java/com/liferay/dynamic/data/mapping/expression/internal/DDMExpressionFunctionTrackerImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-expression/src/main/java/com/liferay/dynamic/data/mapping/expression/internal/DDMExpressionFunctionTrackerImpl.java
@@ -45,7 +45,7 @@ public class DDMExpressionFunctionTrackerImpl
 			new HashMap<>();
 
 		for (DDMExpressionFunctionFactory ddmExpressionFunctionFactory :
-				_ddmExpressionFunctionFactories.values()) {
+				_ddmExpressionFunctionFactoryMap.values()) {
 
 			DDMExpressionFunction ddmExpressionFunction =
 				ddmExpressionFunctionFactory.create();
@@ -70,7 +70,7 @@ public class DDMExpressionFunctionTrackerImpl
 
 		for (String functionName : functionNames) {
 			DDMExpressionFunctionFactory ddmExpressionFunctionFactory =
-				_ddmExpressionFunctionFactories.getService(functionName);
+				_ddmExpressionFunctionFactoryMap.getService(functionName);
 
 			if (ddmExpressionFunctionFactory != null) {
 				ddmExpressionFunctionFactories.put(
@@ -103,17 +103,17 @@ public class DDMExpressionFunctionTrackerImpl
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
-		_ddmExpressionFunctionFactories =
+		_ddmExpressionFunctionFactoryMap =
 			ServiceTrackerMapFactory.openSingleValueMap(
 				bundleContext, DDMExpressionFunctionFactory.class, "name");
 	}
 
 	@Deactivate
 	protected void deactivate() {
-		_ddmExpressionFunctionFactories.close();
+		_ddmExpressionFunctionFactoryMap.close();
 	}
 
 	private ServiceTrackerMap<String, DDMExpressionFunctionFactory>
-		_ddmExpressionFunctionFactories;
+		_ddmExpressionFunctionFactoryMap;
 
 }
