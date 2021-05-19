83b2c0eb5a2027929a647c2f8b00f43a5d322957
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-19 15:24:09 -0300

    LPS-128758 Show all entries when a field group is repeated

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/storage/DDMFormValues.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/storage/DDMFormValues.java
@@ -112,9 +112,26 @@ public class DDMFormValues implements Serializable {
 
 			ddmFormFieldValues.add(ddmFormFieldValue);
 
-			if (includeNestedDDMFormFieldValues) {
-				ddmFormFieldValuesMap.putAll(
-					ddmFormFieldValue.getNestedDDMFormFieldValuesMap());
+			if (!includeNestedDDMFormFieldValues) {
+				continue;
+			}
+
+			Map<String, List<DDMFormFieldValue>> nestedDDMFormFieldValuesMap =
+				ddmFormFieldValue.getNestedDDMFormFieldValuesMap();
+
+			for (Map.Entry<String, List<DDMFormFieldValue>> entry :
+					nestedDDMFormFieldValuesMap.entrySet()) {
+
+				List<DDMFormFieldValue> nestedDDMFormFieldValues =
+					ddmFormFieldValuesMap.get(entry.getKey());
+
+				if (nestedDDMFormFieldValues == null) {
+					nestedDDMFormFieldValues = new ArrayList<>();
+
+					ddmFormFieldValuesMap.put(entry.getKey(), entry.getValue());
+				}
+
+				nestedDDMFormFieldValues.addAll(entry.getValue());
 			}
 		}
 
